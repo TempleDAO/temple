bbe933430f6e3664ee285e9c6cff197d154d663b
butlerji <>
2021-11-01 21:35:29 +1100

    Updated Opening Ceremony Quest contract + tests

--- contracts/OpeningCeremonyQuest.sol
+++ contracts/OpeningCeremonyQuest.sol
@@ -2,25 +2,87 @@ pragma solidity ^0.8.4;
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
- * Temple opening ceremony contract
- */
-contract OpeningCeremonyQuest is Ownable {
+ * Temple opening ceremony contract */ 
+ contract OpeningCeremonyQuest is AccessControl {
+    bytes32 public constant CAN_CHANGE_STATE = keccak256("CAN_CHANGE_STATE");
 
     struct QuestData {
-        // data version so dapps can map the data field
-        uint256 version;
-        // templar data as JSON.stringify
-        string data;
+        bytes32 stepWhenLocked;
+        bytes32 stepWhenUnlocked;
+        uint256 lockedUntil;
+        bytes32 enclave;
+        bool completed;
     }
 
     // opening ceremony data from a templar
     mapping(address => QuestData) public dataOf;
 
-    // use to update data for a templar
-    function setData(address templar, uint256 version, string memory data) external onlyOwner {
-        dataOf[templar].version = version;
-        dataOf[templar].data = data;
+    // keccak256 encoeded step where caller can set the enclave
+    bytes32 public setEnclaveCondition;
+
+    // keccak256 encoeded step where caller can mark the quest as complete
+    bytes32 public setCompletedCondition;
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        grantRole(CAN_CHANGE_STATE, msg.sender);
+    }
+
+    function setConditions(bytes32 _setEnclaveCondition, bytes32 _setCompletedCondition) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        setEnclaveCondition = _setEnclaveCondition;
+        setCompletedCondition = _setCompletedCondition;
+    }
+
+    function setCurrentStep(address templar, bytes32 step) external onlyRole(CAN_CHANGE_STATE) {
+        QuestData storage questData = dataOf[templar];
+
+        require(block.timestamp > questData.lockedUntil, "Locked on current step");
+        dataOf[templar].stepWhenUnlocked = step;
+    }
+
+    function setCurrentStepWithLock(address templar, bytes32 step, uint256 lockDurationSeconds, bytes32 stepWhenUnlocked) external  onlyRole(CAN_CHANGE_STATE)  {
+        QuestData storage questData = dataOf[templar];
+
+        require(block.timestamp > questData.lockedUntil, "Locked on current step");
+
+        dataOf[templar].stepWhenLocked = step;
+        dataOf[templar].stepWhenUnlocked = stepWhenUnlocked;
+        dataOf[templar].lockedUntil = block.timestamp + lockDurationSeconds;
+    }
+
+    function setEnclave(address templar, bytes32 enclave) external onlyRole(CAN_CHANGE_STATE) {
+        require(
+            keccak256(abi.encode(getCurrentStep(templar))) == setEnclaveCondition 
+            && dataOf[templar].enclave == "", "Cannot set enclave at current quest step");
+        dataOf[templar].enclave = enclave;
+    }
+        
+
+    function setCompleted(address templar) external onlyRole(CAN_CHANGE_STATE) {
+        require(keccak256(abi.encode(getCurrentStep(templar))) == setCompletedCondition, "Cannot set quest as completed at current step");
+        dataOf[templar].completed = true;
+    }
+
+    /** Owner only method to udpate any/all user quest data if required by enclave of order to support questers */
+    function overrideUserData(address templar, QuestData memory data) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        dataOf[templar] = data;
+    }
+
+    /** Owner only method to remove a lock for a given user (to speed up testing) */
+    function removeLock(address templar) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        dataOf[templar].lockedUntil = 0;
+    }
+
+    function getCurrentStep(address templar) public view returns(bytes32) {
+        QuestData storage questData = dataOf[templar];
+
+        if (block.timestamp < questData.lockedUntil) {
+            return questData.stepWhenLocked;
+        } else {
+            return questData.stepWhenUnlocked;
+        }
     }
 }
--- test/opening-ceremony-quest-tests.ts
+++ test/opening-ceremony-quest-tests.ts
@@ -1,60 +1,122 @@
+import { arrayify } from '@ethersproject/bytes';
 import { expect } from 'chai';
 import { BigNumber, Signer } from 'ethers';
 import { ethers } from 'hardhat';
 import { OpeningCeremonyQuest, OpeningCeremonyQuest__factory } from '../typechain';
-import { shouldThrow } from './helpers';
+import { mineForwardSeconds, shouldThrow } from './helpers';
 
 describe('Test Opening Ceremony Quest', async () => {
-  const ownableErr = /Ownable: caller is not the owner/;
-  const DEFAULT_DATA = {
-    roles: ['r1', 'r2'],
-    lastCompletedStep: 4,
-  };
-  const templarDataStringify = JSON.stringify(DEFAULT_DATA);
-  let TEMPLE_OPENING_CEREMONY: OpeningCeremonyQuest;
+  const aclErr = /AccessControl: account .* is missing role .*/;
+
+  let quest: OpeningCeremonyQuest;
   let owner: Signer;
+  let stateChanger: Signer;
   let nonOwner: Signer;
   const testVersion = BigNumber.from(1);
 
-
   beforeEach(async () => {
-    [owner, nonOwner] = (await ethers.getSigners()) as Signer[];
-    TEMPLE_OPENING_CEREMONY = await new OpeningCeremonyQuest__factory(owner).deploy();
+    [owner, stateChanger, nonOwner] = (await ethers.getSigners()) as Signer[];
+    quest = await new OpeningCeremonyQuest__factory(owner).deploy();
+
+    await quest.setConditions(
+      ethers.utils.keccak256(ethers.utils.formatBytes32String("1")),
+      ethers.utils.keccak256(ethers.utils.formatBytes32String("2")))
+
+    await quest.grantRole(await quest.CAN_CHANGE_STATE(), await stateChanger.getAddress())
   });
 
-  it('Only owner can set data', async () => {
-    const nonOwnerAddress = await nonOwner.getAddress();
-    const ownerAddress = await owner.getAddress();
+  it('Only admin can set conditions', async () => {
+    const setEnclaveCondition = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["string"], ["a"]));
+    const setCompletedCondition = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["string"], ["b"]));
 
-    // Check that nonOwner can NOT update templar data
-    await shouldThrow(TEMPLE_OPENING_CEREMONY.connect(nonOwner).setData(nonOwnerAddress, testVersion, templarDataStringify), ownableErr);
+    await shouldThrow(quest.connect(stateChanger).setConditions(
+      setEnclaveCondition,
+      setCompletedCondition),
+      aclErr);
+    await shouldThrow(quest.connect(nonOwner).setConditions(
+      setEnclaveCondition,
+      setCompletedCondition),
+      aclErr);
+    await quest.setConditions(
+      setEnclaveCondition,
+      setCompletedCondition,
+    );
 
-
-    // Check that Owner CAN update templar data
-    await TEMPLE_OPENING_CEREMONY.setData(nonOwnerAddress, testVersion, templarDataStringify);
-    const templarData = await TEMPLE_OPENING_CEREMONY.dataOf(nonOwnerAddress);
-    expect(templarData.version).eq(testVersion);
-    expect(templarData.data).eq(templarDataStringify);
+    expect(await quest.setEnclaveCondition()).eq(setEnclaveCondition);
+    expect(await quest.setCompletedCondition()).eq(setCompletedCondition);
   });
 
+  it('Only address with the CAN_CHANGE_STATE role can change a user state', async () => {
+    const questerAddr = await nonOwner.getAddress();
 
-  it('Owner can change templar data', async () => {
-    const nonOwnerAddress = await nonOwner.getAddress();
-    const ownerAddress = await owner.getAddress();
+    const step2 = ethers.utils.formatBytes32String("2")
+    const step3 = ethers.utils.formatBytes32String("3")
 
-    // Set data and chek original values
-    await TEMPLE_OPENING_CEREMONY.setData(nonOwnerAddress, testVersion, templarDataStringify);
-    const templarData = await TEMPLE_OPENING_CEREMONY.dataOf(nonOwnerAddress);
-    expect(templarData.version).eq(testVersion);
-    expect(templarData.data).eq(templarDataStringify);
+    await shouldThrow(quest.connect(nonOwner).setCurrentStep(questerAddr, step2), aclErr);
+    await shouldThrow(quest.connect(nonOwner).setCurrentStepWithLock(questerAddr, step2, 10, step3), aclErr);
 
-    // Update data
-    const newData = JSON.parse(templarData.data);
-    newData.roles.push('r3');
-    let newDataStringify = JSON.stringify(newData);
-    await TEMPLE_OPENING_CEREMONY.setData(nonOwnerAddress, testVersion, newDataStringify);
+    await quest.connect(stateChanger).setCurrentStep(questerAddr, step2);
+    expect(await quest.getCurrentStep(questerAddr)).eq(step2);
+    await quest.connect(stateChanger).setCurrentStepWithLock(await nonOwner.getAddress(), step3, 10, step2);
+    expect(await quest.getCurrentStep(questerAddr)).eq(step3);
+    await mineForwardSeconds(10);
+    expect(await quest.getCurrentStep(questerAddr)).eq(step2);
+  });
 
-    const templarDataUpdated = await TEMPLE_OPENING_CEREMONY.dataOf(nonOwnerAddress);
-    expect(templarDataUpdated.data, newDataStringify);
+  it('Cannot change users state while locked', async () => {
+    const questerAddr = await nonOwner.getAddress();
+
+    const step2 = ethers.utils.formatBytes32String("2")
+    const step3 = ethers.utils.formatBytes32String("3")
+
+    await quest.connect(stateChanger).setCurrentStepWithLock(await nonOwner.getAddress(), step3, 20, step2);
+    await shouldThrow(quest.connect(stateChanger).setCurrentStep(questerAddr, step2), /Locked on current step/);
+    await shouldThrow(quest.connect(stateChanger).setCurrentStepWithLock(questerAddr, step2, 10, step3), /Locked on current step/);
+  });
+
+  it('Only address with CAN_CHANGE_STATE role can set enclave/completed flag', async () => {
+    await shouldThrow(quest.connect(nonOwner).setCompleted(await nonOwner.getAddress()), aclErr);
+    await shouldThrow(quest.connect(nonOwner).setEnclave(await nonOwner.getAddress(), ethers.utils.formatBytes32String("structure")), aclErr);
+  });
+
+  it('Can only set enclave/completed flag when the user is in a given part of the quest', async () => {
+    await shouldThrow(quest.connect(stateChanger).setEnclave(await nonOwner.getAddress(), ethers.utils.formatBytes32String("structure")), /Cannot set enclave at current quest step/);
+    await quest.connect(stateChanger).setCurrentStep(await nonOwner.getAddress(), ethers.utils.formatBytes32String("1"));
+    await quest.connect(stateChanger).setEnclave(await nonOwner.getAddress(), ethers.utils.formatBytes32String("structure"));
+    expect(ethers.utils.parseBytes32String((await quest.dataOf(await nonOwner.getAddress())).enclave)).eq("structure")
+
+    await shouldThrow(quest.connect(stateChanger).setCompleted(await nonOwner.getAddress()), /Cannot set quest as completed at current step/);
+    await quest.connect(stateChanger).setCurrentStep(await nonOwner.getAddress(), ethers.utils.formatBytes32String("2"));
+    await quest.connect(stateChanger).setCompleted(await nonOwner.getAddress());
+    expect((await quest.dataOf(await nonOwner.getAddress())).completed).is.true;
+  });
+
+  it('Only admin can speed run/reset a users state', async () => {
+    await shouldThrow(quest.connect(stateChanger).removeLock(await nonOwner.getAddress()), aclErr);
+    await shouldThrow(quest.connect(stateChanger).overrideUserData(await nonOwner.getAddress(), {
+      stepWhenLocked: ethers.utils.formatBytes32String("1"),
+      stepWhenUnlocked: ethers.utils.formatBytes32String("2"),
+      lockedUntil: 0,
+      enclave: ethers.utils.formatBytes32String(""),
+      completed: false
+    }), aclErr);
+
+    await quest.connect(stateChanger).setCurrentStepWithLock(
+      await nonOwner.getAddress(), 
+      ethers.utils.formatBytes32String("2"), 
+      1000, 
+      ethers.utils.formatBytes32String("1"));
+    expect(await quest.getCurrentStep(await nonOwner.getAddress())).eq(ethers.utils.formatBytes32String("2"));
+    await quest.removeLock(await nonOwner.getAddress());
+    expect(await quest.getCurrentStep(await nonOwner.getAddress())).eq(ethers.utils.formatBytes32String("1"));
+
+    await quest.overrideUserData(await nonOwner.getAddress(), {
+      stepWhenLocked: ethers.utils.formatBytes32String("1"),
+      stepWhenUnlocked: ethers.utils.formatBytes32String("2"),
+      lockedUntil: 0,
+      enclave: ethers.utils.formatBytes32String(""),
+      completed: true
+    });
+    expect((await quest.dataOf(await nonOwner.getAddress())).completed).is.true;
   });
 });
