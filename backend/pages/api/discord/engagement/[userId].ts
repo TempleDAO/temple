47d85c51666d4ee1ebe6d81ec768671b40a90b42
Alex Stanciu <astanciu@gmail.com>
2021-12-12 13:29:48 -0500

    add userengagement endpoint

--- backend/pages/api/discord/engagement/[userId].ts
+++ backend/pages/api/discord/engagement/[userId].ts
@@ -6,8 +6,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { userId } = req.query;
+  const id = Array.isArray(userId) ? userId[0] : userId;
+  
   try {
-    const result = await discordRepo.memberSummary();
+    const result = await discordRepo.userEngagement(id);
     return res
       .status(200)
       .setHeader("cache-control", `s-maxage=${DEFAULT_CACHE_AGE}`)
@@ -15,7 +18,7 @@ export default async function handler(
   } catch (err) {
     console.error(err);
     res.status(500).json({
-      error: "Internal server error",
+      error: "Internal server error"
     });
   }
 }
