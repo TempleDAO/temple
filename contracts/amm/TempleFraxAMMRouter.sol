508b0f4fe70c49df192678e8e7c548f0b46bfc98
butlerji <>
2021-12-15 09:59:43 +1100

    Revert "Fixed Issue where DTP Can decrease on certain buys"
    This reverts commit b34fcf969dbddd7e3e20cc0a06480c552762a998.

--- contracts/amm/TempleFraxAMMRouter.sol
+++ contracts/amm/TempleFraxAMMRouter.sol
@@ -187,10 +187,7 @@ contract TempleFraxAMMRouter is Ownable, AccessControl {
             (uint numerator, uint denominator) = mintRatioAt(reserveTemple, reserveFrax);
             amountInProtocol = amountIn * numerator / denominator;
             // gas optimisation. Only update the temple component of the threshold price, keeping the frax component constant
-            uint newDynamicThresholdPricetemple = reserveTemple * dynamicThresholdPrice.frax * DYNAMIC_THRESHOLD_INCREASE_DENOMINATOR / (reserveFrax * dynamicThresholdIncreasePct);
-            if (newDynamicThresholdPricetemple > dynamicThresholdPrice.temple) {
-                dynamicThresholdPrice.temple = newDynamicThresholdPricetemple;
-            }
+            dynamicThresholdPrice.temple = reserveTemple * dynamicThresholdPrice.frax * DYNAMIC_THRESHOLD_INCREASE_DENOMINATOR / (reserveFrax * dynamicThresholdIncreasePct);
             decayStartBlock = block.number;
         }
 
