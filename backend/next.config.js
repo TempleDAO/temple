d595b6c105832227281731df5563e142da9e6fff
Alex Stanciu <astanciu@gmail.com>
2021-12-15 12:38:06 -0500

    update cors

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,28 +1,10 @@
 module.exports = {
   async headers() {
+    const corsHost = process.env.CORS_HOST || "https://templedao.link";
     return [
-      // This pattern matches localhost and allows connecting from local env
       {
         source: "/api/:path*",
-        has: [
-          {
-            type: "header",
-            key: "origin",
-            value: "^http://localhost:(?<port>[0-9]+)"
-          }
-        ],
-        headers: [{ key: "Access-Control-Allow-Origin", value: "http://localhost::port" }]
-      },
-      // this pattern will match anything EXCEPT localhost and will return "templedao.link" as the cors header
-      {
-        source: "/api/:path*",
-        has: [
-          {
-            type: "host",
-            value: "^(?!localhost$).*"
-          }
-        ],
-        headers: [{ key: "Access-Control-Allow-Origin", value: "http://templedao.link" }]
+        headers: [{ key: "Access-Control-Allow-Origin", value: corsHost }]
       }
     ];
   }
