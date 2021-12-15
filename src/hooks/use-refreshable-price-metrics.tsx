deb5030e74567475127e8f935c6291dae8e447c5
zelki <zelki.codes@protonmail.com>
2021-12-15 17:05:51 +0000

    feat: add price chart component and story

---
+++ src/hooks/use-refreshable-price-metrics.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import useInterval from 'use-interval';
+
+type PriceMetrics = {
+  templePrice: string;
+  timestamp: number;
+  treasuryStables: number;
+  templeSupply: number;
+};
+
+export default function useRefreshablePriceMetrics(
+  intervalMinutes = 10
+): PriceMetrics[] {
+  const [priceMetrics, setPriceMetrics] = useState<PriceMetrics[]>([]);
+
+  async function refreshMetrics() {
+    const result = await fetch(
+      'https://api.thegraph.com/subgraphs/name/medariox/temple-balances',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `{
+                protocolMetrics {
+                    templePrice
+                    timestamp
+                    treasuryStables
+                    templeSupply
+                }
+        }`,
+        }),
+      }
+    );
+    const metrics = await result.json();
+    setPriceMetrics(metrics?.data?.protocolMetrics ?? []);
+  }
+
+  useInterval(refreshMetrics, intervalMinutes * 60 * 1000, true);
+
+  return priceMetrics;
+}
