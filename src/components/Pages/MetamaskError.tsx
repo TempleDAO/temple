b58cccc4fd5f1d660c82c1c54f25aa2fdb1cf58f
zelki <zelki.codes@protonmail.com>
2021-12-15 14:23:01 +0000

    refactor: improved default routing

--- src/components/Pages/MetamaskError.tsx
+++ src/components/Pages/MetamaskError.tsx
@@ -15,18 +15,16 @@ const MetamaskErrorPage = () => {
       }}
     >
       <Image src={metamaskIcon} width={100} height={100} />
-      <Label>Please add MetaMask to your browser</Label>
-      <a href="https://metamask.io/download.html">Click to download</a>
+      <p className="color-brand">Please add MetaMask to your browser</p>
+      <a href="https://metamask.io/download.html" target="_blank" rel="noreferrer">
+        Click to download
+      </a>
     </StyledFlex>
   );
 };
 
 const StyledFlex = styled(Flex)`
   height: 31.25rem;
-`;
-
-const Label = styled.p`
-  color: ${({ theme }) => theme.palette.brand};
 `;
 
 export default MetamaskErrorPage;
