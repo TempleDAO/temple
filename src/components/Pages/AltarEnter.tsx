c986a3fd93dacf7fcaee0eac4fd34c35f54479c3
nichosystem <nichosystem@protonmail.com>
2021-12-17 23:41:53 -0600

    feat: add altar pages

---
+++ src/components/Pages/AltarEnter.tsx
@@ -0,0 +1,142 @@
+import React, {
+  Dispatch,
+  SetStateAction,
+  useLayoutEffect,
+  useState,
+} from 'react';
+import styled, { keyframes } from 'styled-components';
+import BackButton from 'components/Button/BackButton';
+import bgImage from 'assets/images/altar-enter-bg.png';
+import triangle from 'assets/images/triangle.svg';
+import glowLeft from 'assets/images/AMM_leftcut.png';
+import glowRight from 'assets/images/AMM_rightglow.png';
+import { getBgImgDimensions } from 'utils/imageSize';
+import { CustomRoutingPage } from 'hooks/use-custom-spa-routing';
+
+type BgDimension = {
+  width: number;
+  height: number;
+  scaleH: number;
+  scaleW: number;
+  imageWidth: number;
+  imageHeight: number;
+};
+
+enum Pages {
+  Foyer,
+  Left,
+  Center,
+  Right,
+}
+
+const EnterPage = () => {
+  // Used to determine door images size and position
+  const [bgDimensions, setBgDimensions]: [
+    BgDimension | undefined,
+    Dispatch<SetStateAction<BgDimension | undefined>>
+  ] = useState();
+
+  //   const { back, changePageTo } = routingHelper;
+
+  // Update bgDimensions state
+  function handleResize() {
+    const backgroundDimensions = getBgImgDimensions(
+      document.querySelector('#background'),
+      bgImage
+    );
+    if (!backgroundDimensions) return;
+    setBgDimensions(backgroundDimensions);
+  }
+
+  // Set event listeners for load and resize to handleResize()
+  useLayoutEffect(() => {
+    window.onload = () => handleResize();
+    window.addEventListener('resize', () => handleResize());
+    setTimeout(() => handleResize(), 500);
+    return window.removeEventListener('resize', () => handleResize());
+  }, []);
+
+  return (
+    <Background id="background" style={{ overflow: 'hidden' }}>
+      {bgDimensions != null && (
+        <>
+          <DoorGlow
+            src={glowLeft}
+            title="Enter"
+            // onClick={() => changePageTo(RitualsPosters)}
+            style={{
+              transform: `scale(${1 * bgDimensions.scaleW}%)`,
+              bottom: `${0.02 * bgDimensions.height}px`,
+              left:
+                bgDimensions.height == window.innerHeight
+                  ? `${
+                      bgDimensions.width * 0.0 -
+                      (bgDimensions.width - window.innerWidth) / 2
+                    }px`
+                  : `${0.0 * bgDimensions.width}px`,
+            }}
+          />
+          <DoorGlow
+            src={glowRight}
+            title="Enter"
+            // onClick={() => changePageTo(RitualsPosters)}
+            style={{
+              transform: `scale(${0.98 * bgDimensions.scaleW}%)`,
+              bottom: `${0.042 * bgDimensions.height}px`,
+              left:
+                bgDimensions.height == window.innerHeight
+                  ? `${
+                      bgDimensions.width * 0.685 -
+                      (bgDimensions.width - window.innerWidth) / 2
+                    }px`
+                  : `${0.685 * bgDimensions.width}px`,
+            }}
+          />
+        </>
+      )}
+      {/* <BackButton width={112} height={112} onClick={back} /> */}
+    </Background>
+  );
+};
+
+const Background = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${bgImage});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  position: relative;
+`;
+
+const flicker = keyframes`
+          0% {
+            opacity: 0.3;
+          }
+
+          33% {
+            opacity: 0.6;
+          }
+
+          60% {
+            opacity: 0.4;
+          }
+
+          100% {
+            opacity: 0.8;
+          }
+        `;
+
+const DoorGlow = styled.img`
+  position: absolute;
+  transform-origin: bottom left;
+  opacity: 0.75;
+  transition: opacity 150ms;
+  &:hover {
+    opacity: 1;
+    cursor: pointer;
+    animation: none;
+  }
+  animation: ${flicker} 3s infinite alternate ease-out;
+`;
+export default EnterPage;
