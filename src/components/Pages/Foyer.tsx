70f883657e1d99195496bccaa765f98c22fa271e
nichosystem <nichosystem@protonmail.com>
2021-12-18 00:04:23 -0600

    fix: image position

--- src/components/Pages/Foyer.tsx
+++ src/components/Pages/Foyer.tsx
@@ -77,9 +77,8 @@ const FoyerPage: CustomRoutingPage = ({ routingHelper }) => {
                   left:
                     bgDimensions.height == window.innerHeight
                       ? `${
-                          bgDimensions.imageWidth * 0.425 -
-                          (bgDimensions.imageWidth - window.innerWidth) / 2 -
-                          136
+                          bgDimensions.width * 0.18 -
+                          (bgDimensions.width - window.innerWidth) / 2
                         }px`
                       : `${0.18 * bgDimensions.width}px`,
                 }}
@@ -94,9 +93,8 @@ const FoyerPage: CustomRoutingPage = ({ routingHelper }) => {
                   left:
                     bgDimensions.height == window.innerHeight
                       ? `${
-                          bgDimensions.imageWidth * 0.485 -
-                          (bgDimensions.imageWidth - window.innerWidth) / 2 -
-                          23
+                          bgDimensions.width * 0.439 -
+                          (bgDimensions.width - window.innerWidth) / 2
                         }px`
                       : `${0.439 * bgDimensions.width}px`,
                 }}
@@ -111,9 +109,8 @@ const FoyerPage: CustomRoutingPage = ({ routingHelper }) => {
                   left:
                     bgDimensions.height == window.innerHeight
                       ? `${
-                          bgDimensions.imageWidth * 0.545 -
-                          (bgDimensions.imageWidth - window.innerWidth) / 2 +
-                          75
+                          bgDimensions.width * 0.685 -
+                          (bgDimensions.width - window.innerWidth) / 2
                         }px`
                       : `${0.685 * bgDimensions.width}px`,
                 }}
