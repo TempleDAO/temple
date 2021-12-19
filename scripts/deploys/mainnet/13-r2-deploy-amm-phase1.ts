05811e94c6615f756f9c47b77e1e77dfe3e3f4b8
butlerji <>
2021-12-20 04:50:20 +1100

    Updated contract addresses

--- scripts/deploys/mainnet/13-r2-deploy-amm-phase1.ts
+++ scripts/deploys/mainnet/13-r2-deploy-amm-phase1.ts
@@ -16,41 +16,67 @@ async function main() {
     DEPLOYED = DEPLOYED_CONTRACTS[network.name];
   }
   
-  const daoMultisig = "0x4D6175d58C5AceEf30F546C0d5A557efFa53A950";
+  // // Setup custom AMM
+  // const pairFactory = new TempleUniswapV2Pair__factory(owner);
+  // const pair: TempleUniswapV2Pair = await deployAndMine(
+  //   'TEMPLE_V2_PAIR', pairFactory, pairFactory.deploy,
+  //   DEPLOYED.MULTISIG,
+  //   DEPLOYED.TEMPLE,
+  //   DEPLOYED.FRAX);
 
-  // Setup custom AMM
-  const pairFactory = new TempleUniswapV2Pair__factory(owner);
-  const pair: TempleUniswapV2Pair = await deployAndMine(
-    'TEMPLE_V2_PAIR', pairFactory, pairFactory.deploy,
-    daoMultisig,
+  //const templeRouterFactory = new TempleFraxAMMRouter__factory(owner);
+  //const templeRouter: TempleFraxAMMRouter = await deployAndMine(
+  //  'TEMPLE_V2_ROUTER', templeRouterFactory, templeRouterFactory.deploy,
+  //   DEPLOYED.TEMPLE_V2_PAIR,
+  //   DEPLOYED.TEMPLE,
+  //   DEPLOYED.FRAX,
+  //   DEPLOYED.TREASURY,
+  //   DEPLOYED.TREASURY, // placeholder, to handle circular dep with router ops contract
+  //   {frax: 1000000, temple: 255100},
+  //   2, /* threshold decay per block */
+  //   {frax: 100000, temple: 50000},
+  //   {frax: 100000, temple: 5000},
+  //);
+
+  //// AMMWhitelist
+  //const verifierPublicKey = "0x5fc43464209bd9b0f7Bf8e738E427D41C6d2C87a" // generated and owned by XAV
+  //const ammWhitelistFactory = new AMMWhitelist__factory(owner);
+  //const ammWhiteList: AMMWhitelist = await deployAndMine(
+  //  'AMM_WHITELIST', ammWhitelistFactory, ammWhitelistFactory.deploy,
+  //  templeRouter.address,
+  //  verifierPublicKey);
+
+  const templeFraxAMMOpsFactory = new TempleFraxAMMOps__factory(owner);
+  const templeAmmOps: TempleFraxAMMOps = await deployAndMine(
+    'TEMPLE_AMM_OPS',templeFraxAMMOpsFactory, templeFraxAMMOpsFactory.deploy,
     DEPLOYED.TEMPLE,
-    DEPLOYED.FRAX);
-
-  const templeRouterFactory = new TempleFraxAMMRouter__factory(owner);
-  await deployAndMine(
-    'TEMPLE_V2_ROUTER', templeRouterFactory, templeRouterFactory.deploy,
-     pair.address,
-     DEPLOYED.TEMPLE,
-     DEPLOYED.FRAX,
-     DEPLOYED.TREASURY,
-     DEPLOYED.TREASURY, // placeholder, to handle circular dep with router ops contract
-     {frax: 100000, temple: 9000},
-     100, /* threshold decay per block */
-     {frax: 1000000, temple: 1000000},
-     {frax: 1000000, temple: 100000},
-  );
-
-  // TODO: Needs to be manually wired post deploy
-  console.log("pair.setRouter(templeRouter.address)")
-  console.log("add minter to TEMPLE TOKEN addMinter(templeRouter.address)");
-
-  // re-create the treasury management protocol
-  const treasuryManagementProxyFactory = new TreasuryManagementProxy__factory(owner);
-  await deployAndMine(
-    'TREASURY_MANAGEMENT_PROXY', treasuryManagementProxyFactory, treasuryManagementProxyFactory.deploy,
-    daoMultisig,
+    DEPLOYED.TEMPLE_V2_ROUTER,
+    DEPLOYED.TREASURY_MANAGEMENT,
+    DEPLOYED.FRAX,
     DEPLOYED.TREASURY,
+    DEPLOYED.TEMPLE_V2_PAIR,
   )
+
+  //const templeRouterFactory = new TempleFraxAMMRouter__factory(owner);
+  const templeRouter: TempleFraxAMMRouter = new TempleFraxAMMRouter__factory(owner).attach(DEPLOYED.TEMPLE_V2_ROUTER);
+  await mine(templeRouter.setProtocolMintEarningsAccount(templeAmmOps.address));
+  await mine(templeRouter.grantRole(await templeRouter.CAN_ADD_ALLOWED_USER(), DEPLOYED.AMM_WHITELIST))
+
+  // await mine(templeRouter.transferOwnership(DEPLOYED.MULTISIG));
+  await mine(templeAmmOps.transferOwnership(DEPLOYED.MULTISIG));
+  // await mine(ammWhiteList.transferOwnership(DEPLOYED.MULTISIG));
+
+  // // // TODO: Needs to be manually wired post deploy
+  // // console.log("pair.setRouter(templeRouter.address)")
+  // // console.log("add minter to TEMPLE TOKEN addMinter(templeRouter.address)");
+
+  // // re-create the treasury management protocol
+  // const treasuryManagementProxyFactory = new TreasuryManagementProxy__factory(owner);
+  // await deployAndMine(
+  //   'TREASURY_MANAGEMENT_PROXY', treasuryManagementProxyFactory, treasuryManagementProxyFactory.deploy,
+  //   DEPLOYED.MULTISIG,
+  //   DEPLOYED.TREASURY,
+  // )
 }
 
 // We recommend this pattern to be able to use async/await everywhere
