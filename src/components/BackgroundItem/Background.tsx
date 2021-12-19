2d5c53786da6c547626f521895f7708ab073d832
Nicho <nichosystem@protonmail.com>
2021-12-19 10:08:59 -0600

    fix: prevent background image from drag (#104)

--- src/components/BackgroundItem/Background.tsx
+++ src/components/BackgroundItem/Background.tsx
@@ -7,5 +7,6 @@ export const Background = styled.img`
   object-fit: cover;
   object-position: bottom center;
   position: absolute;
+  pointer-events: none;
   z-index: ${(props) => props.theme.zIndexes.base};
 `;
