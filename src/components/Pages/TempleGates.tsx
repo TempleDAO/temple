ed85e037ba09c411cd7333a1e07856d663bf4fb6
nichosystem <nichosystem@protonmail.com>
2021-12-19 16:54:58 -0600

    fix: open temple gates

--- src/components/Pages/TempleGates.tsx
+++ src/components/Pages/TempleGates.tsx
@@ -1,96 +1,19 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Foyer from 'components/Pages/Foyer';
 import gatesImage from 'assets/images/EnterTheGates.png';
-import { useWallet } from 'providers/WalletProvider';
-import { TempleFraxAMMRouter__factory } from 'types/typechain';
 import { CustomRoutingPage } from 'hooks/use-custom-spa-routing';
 import withWallet from 'hoc/withWallet';
 
-const ENV_VARS = import.meta.env;
-const TEMPLE_V2_ROUTER_ADDRESS = ENV_VARS.VITE_PUBLIC_TEMPLE_V2_ROUTER_ADDRESS;
-
-enum Status {
-  Start = 'ENTER',
-  Loading = 'WAIT',
-  Failed = 'DENIED',
-  Complete = 'WELCOME',
-}
-
 const TempleGatesPage: CustomRoutingPage = ({ routingHelper }) => {
-  const [key, setKey]: [string, Dispatch<SetStateAction<string>>] =
-    useState('');
-  const { wallet, signer, verifyAMMWhitelist } = useWallet();
-  const [status, setStatus]: [
-    Status | undefined,
-    Dispatch<SetStateAction<Status | undefined>>
-  ] = useState();
-
   const { changePageTo } = routingHelper;
 
-  // Check if user is whitelisted already
-  const isWhitelisted = async () => {
-    if (wallet && signer) {
-      try {
-        const fraxAMMRouter = new TempleFraxAMMRouter__factory()
-          .attach(TEMPLE_V2_ROUTER_ADDRESS)
-          .connect(signer);
-        const whitelisted = await fraxAMMRouter.allowed(wallet);
-        if (whitelisted) {
-          changePageTo(Foyer);
-        } else {
-          setStatus(Status.Start);
-        }
-      } catch (e) {
-        console.error(e);
-        setStatus(Status.Start);
-      }
-    } else setStatus(Status.Start);
-  };
-  useEffect(() => {
-    isWhitelisted();
-  }, [wallet, signer]);
-
-  // Send signature to contract to be whitelisted
-  const submit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setStatus(Status.Loading);
-    const tx = await verifyAMMWhitelist(key);
-    if (!tx) {
-      setStatus(Status.Failed);
-      return;
-    }
-    const res = await tx.wait();
-    // TODO: Check the value of res to see if transaction was successful
-    isWhitelisted();
-  };
-
   return (
-    <>
-      <TempleGatesContainer>
-        {ENV_VARS.VITE_ENV !== 'production' && (
-          <button
-            style={{
-              position: 'fixed',
-              left: '1rem',
-              bottom: '1rem',
-            }}
-            onClick={() => changePageTo(Foyer)}
-          >
-            BYPASS
-          </button>
-        )}
-        <KeyForm onSubmit={submit}>
-          <KeyInput
-            placeholder="Sacred Key"
-            type="text"
-            onChange={(e) => setKey(e.target.value)}
-            value={key}
-          />
-          <EnterButton>{status}</EnterButton>
-        </KeyForm>
-      </TempleGatesContainer>
-    </>
+    <TempleGatesContainer>
+      <KeyForm onSubmit={(e) => e.preventDefault()}>
+        <EnterButton onClick={() => changePageTo(Foyer)}>ENTER</EnterButton>
+      </KeyForm>
+    </TempleGatesContainer>
   );
 };
 
@@ -107,27 +30,6 @@ const KeyForm = styled.form`
   bottom: 2rem;
   right: 2rem;
   display: flex;
-`;
-
-const KeyInput = styled.input`
-  ${(props) => props.theme.typography.h4};
-  background: ${(props) => props.theme.palette.brand25};
-  border: none;
-  border-radius: 2px;
-  color: #d1c0a7;
-  margin-right: 1rem;
-  opacity: 1;
-  min-height: 3rem;
-  padding: 0rem 0.5rem;
-  text-align: center;
-  width: 14rem;
-  ::placeholder {
-    opacity: 1;
-  }
-  &:focus,
-  &:active {
-    outline: none;
-  }
 `;
 
 const EnterButton = styled.button`
