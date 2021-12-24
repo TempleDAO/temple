afc65d97373acd750e2428cdda763fcdcdd90254
zelkibuilds <94715286+zelkibuilds@users.noreply.github.com>
2021-12-24 00:06:28 +0000

    feat: add price thresold line series to PriceChart.tsx (#180)

--- src/hooks/use-refreshable-price-metrics.tsx
+++ src/hooks/use-refreshable-price-metrics.tsx
@@ -1,20 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useInterval from 'use-interval';
 
-type PriceMetrics = {
+export type PriceMetrics = {
   templePrice: string;
   timestamp: number;
   treasuryStables: number;
+  thresholdTemplePrice: number;
   templeSupply: number;
 };
 
-export default function useRefreshablePriceMetrics(
-  intervalMinutes = 10
-): PriceMetrics[] {
-  const [priceMetrics, setPriceMetrics] = useState<PriceMetrics[]>([]);
+export default function useRefreshablePriceMetrics(intervalMinutes = 20) {
+  const [hourlyPriceMetrics, setHourlyPriceMetrics] = useState<PriceMetrics[]>(
+    []
+  );
+
+  const [dailyPriceMetrics, setDailyPriceMetrics] = useState<PriceMetrics[]>(
+    []
+  );
 
   async function refreshMetrics() {
-    const result = await fetch(
+    const hourlyRequest = await fetch(
       'https://api.thegraph.com/subgraphs/name/medariox/temple-balances',
       {
         method: 'POST',
@@ -28,6 +33,7 @@ export default function useRefreshablePriceMetrics(
               timestamp
               templeSupply
               templePrice
+              thresholdTemplePrice
               treasuryStables
             }
           }`,
@@ -35,11 +41,43 @@ export default function useRefreshablePriceMetrics(
       }
     );
 
-    const metrics = await result.json();
-    setPriceMetrics(metrics?.data?.protocolMetrics ?? []);
+    const dailyRequest = await fetch(
+      'https://api.thegraph.com/subgraphs/name/medariox/temple-balances',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `{
+            dayProtocolMetrics(orderDirection: desc, orderBy: timestamp) {
+              timestamp
+              templeSupply
+              templePrice
+              thresholdTemplePrice
+              treasuryStables
+          }
+          }`,
+        }),
+      }
+    );
+
+    const [hourlyResult, dailyResult] = await Promise.all([
+      hourlyRequest,
+      dailyRequest,
+    ]);
+
+    const [hourlyMetrics, dailyMetrics] = await Promise.all([
+      hourlyResult.json(),
+      dailyResult.json(),
+    ]);
+
+    setHourlyPriceMetrics(hourlyMetrics?.data?.protocolMetrics ?? []);
+    setDailyPriceMetrics(dailyMetrics?.data?.dayProtocolMetrics ?? []);
   }
 
   useInterval(refreshMetrics, intervalMinutes * 60 * 1000, true);
 
-  return priceMetrics;
+  return { dailyPriceMetrics, hourlyPriceMetrics };
 }
