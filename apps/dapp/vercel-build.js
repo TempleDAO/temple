ffe2123f91ef1e5a5b28c18676f9b2ca18f68089
Alex Stanciu <astanciu@gmail.com>
2022-01-15 15:26:39 -0500

    fix build

--- apps/dapp/vercel-build.js
+++ apps/dapp/vercel-build.js
@@ -1,9 +1,11 @@
-const BRANCH = process.env.VERCEL_GIT_COMMIT_REF;
+// const BRANCH = process.env.VERCEL_GIT_COMMIT_REF;
 
-const VALID_BUILD_BRANCHES = ['main', 'develop', 'hotfix', 'staging', 'intra'];
+// const VALID_BUILD_BRANCHES = ['main', 'develop', 'hotfix', 'staging', 'intra'];
 
-if (VALID_BUILD_BRANCHES.find((validBranch) => validBranch === BRANCH)) {
-  process.exit(0);
-} else {
-  process.exit(1);
-}
+// if (VALID_BUILD_BRANCHES.find((validBranch) => validBranch === BRANCH)) {
+//   process.exit(0);
+// } else {
+//   process.exit(1);
+// }
+
+process.exit(0)
