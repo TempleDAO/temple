89d482bc0692ad284e007a01724856cbabcbf19c
H4K8R <h4k8@protonmail.com>
2022-01-27 17:00:33 +1100

    fix: lockAndVerify timestam arg to LockedOGTemple.lockFor

--- apps/protocol/contracts/LockedOGTemple.sol
+++ apps/protocol/contracts/LockedOGTemple.sol
@@ -28,10 +28,14 @@ contract LockedOGTemple {
         ogTempleToken = _ogTempleToken;
     }
 
-    /** lock up OG */
+    /**
+    * _staker: address of staker
+    * _amountOGTemple: OGTEMPLE to lock
+    * _unlockDelaySeconds: Delay to add to block.timestamp will pick max btw block.timestamp + _unlockDelaySeconds > lockEntry.lockedUntilTimestamp
+    */
     function lockFor(address _staker, uint256 _amountOGTemple, uint256 _unlockDelaySeconds) public {
         LockedEntry storage lockEntry = ogTempleLocked[_staker];
-        
+
         lockEntry.amount += _amountOGTemple;
         uint256 newLockedUntilTimestamp = block.timestamp + _unlockDelaySeconds;
         if (newLockedUntilTimestamp > lockEntry.lockedUntilTimestamp) {
