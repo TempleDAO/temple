4ce9fe8043b8ca00ef8059c4a986585b21cf71e5
Nyaa <just.nyaa@protonmail.com>
2022-01-31 00:17:56 -0500

    Remove unused ITempleStaking

--- apps/protocol/contracts/ITempleStaking.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.4;
-
-interface ITempleStaking {
-  function stakeFor(address _staker, uint256 _amountTemple)
-    external
-    returns (uint256 amountOgTemple);
-}
