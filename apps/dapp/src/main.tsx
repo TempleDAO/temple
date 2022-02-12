b65d55ad7b16f9c9759926af9a7be19dc6055f69
fujisawa <mrfujisawa@protonmail.com>
2022-02-11 17:48:32 -0800

    lazy load pages at router level

--- apps/dapp/src/main.tsx
+++ apps/dapp/src/main.tsx
@@ -2,22 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import PageLayout from 'components/Layouts/Page';
-import Disclaimer from 'components/Pages/Disclaimer';
-import Enter from 'components/Pages/Enter';
-import Exit from 'components/Pages/Exit';
-import Home from 'components/Pages/Home';
-import Claim from 'components/Pages/Claim';
-import AmmSpaRoot from 'components/Pages/AMM';
-import TeamPayments from 'components/Pages/TeamPayments';
-import FaithAirdrop from 'components/Pages/FaithAirdrop';
-import FireRitualistCashback from 'components/Pages/FireRitualistCashback';
 import NotificationManager from 'components/Notification/NotificationManager';
 import { NotificationProvider } from 'providers/NotificationProvider';
 import { WalletProvider } from 'providers/WalletProvider';
 import { GlobalStyle } from 'styles/GlobalStyle';
 import { theme } from 'styles/theme';
-import DAppRoot from 'components/Pages/DAppRoot';
+import Loader from 'components/Loader/Loader';
+
+const FireRitualistCashback = React.lazy(() => import('components/Pages/FireRitualistCashback'));
+const TeamPayments = React.lazy(() => import('components/Pages/TeamPayments'));
+const FaithAirdrop = React.lazy(() => import('components/Pages/FaithAirdrop'));
+const Claim = React.lazy(() => import('components/Pages/Claim'));
+const Enter = React.lazy(() => import('components/Pages/Enter'));
+const Exit = React.lazy(() => import('components/Pages/Exit'));
+const Disclaimer = React.lazy(() => import('components/Pages/Disclaimer'));
+const AmmSpaRoot = React.lazy(() => import('components/Pages/AMM'));
+const DAppRoot = React.lazy(() => import('components/Pages/DAppRoot'));
+const PageLayout = React.lazy(() => import('components/Layouts/Page'));
+const Home = React.lazy(() => import('components/Pages/Home'));
+
+interface LazyPageProps {
+  component: React.LazyExoticComponent<(props: unknown) => JSX.Element>;
+}
+
+const LazyPage = (
+  { component: Component }: LazyPageProps) => {
+  return (
+    <React.Suspense fallback={<Loader />}>
+      <Component />
+    </React.Suspense>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -28,20 +43,20 @@ ReactDOM.render(
           <BrowserRouter>
             <Routes>
               <>
-                <Route path="/the-temple" element={<AmmSpaRoot />} />
-                <Route path="/dapp" element={<DAppRoot />} />
-                <Route path="/" element={<PageLayout />}>
-                  <Route path="/" element={<Home />} />
-                  <Route path="disclaimer" element={<Disclaimer />} />
-                  <Route path="enter" element={<Enter />} />
-                  <Route path="exit" element={<Exit />} />
-                  <Route path="faith-airdrop" element={<FaithAirdrop />} />
+                <Route path="/the-temple" element={<LazyPage component={AmmSpaRoot} />} />
+                <Route path="/dapp" element={<LazyPage component={DAppRoot} />} />
+                <Route path="/" element={<LazyPage component={PageLayout} />}>
+                  <Route path="/" element={<LazyPage component={Home} />} />
+                  <Route path="disclaimer" element={<LazyPage component={Disclaimer} />} />
+                  <Route path="enter" element={<LazyPage component={Enter} />} />
+                  <Route path="exit" element={<LazyPage component={Exit} />} />
+                  <Route path="faith-airdrop" element={<LazyPage component={FaithAirdrop} />} />
                   <Route
                     path="fire-ritualist-apy-topup"
-                    element={<FireRitualistCashback />}
+                    element={<LazyPage component={FireRitualistCashback} />}
                   />
-                  <Route path="temple-claim" element={<Claim />} />
-                  <Route path="team-payments" element={<TeamPayments />} />
+                  <Route path="temple-claim" element={<LazyPage component={Claim} />} />
+                  <Route path="team-payments" element={<LazyPage component={TeamPayments} />} />
                   <Route path="/*" element={<Navigate replace to="/" />} />
                 </Route>
               </>
