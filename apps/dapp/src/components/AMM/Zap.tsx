d4d682d6f78e25741208f277e1279aa3c406de61
jebsley3 <cuiett@protonmail.com>
2022-02-18 19:05:40 +0000

    wip: get wallet token balances from zapper api

--- apps/dapp/src/components/AMM/Zap.tsx
+++ apps/dapp/src/components/AMM/Zap.tsx
@@ -15,27 +15,78 @@ import { formatNumberWithCommas } from 'utils/formatter';
 import { fromAtto, toAtto } from 'utils/bigNumber';
 import { ethers } from 'ethers';
 import useRefreshableTreasuryMetrics from 'hooks/use-refreshable-treasury-metrics';
+import axios from 'axios';
+import { InputSelect } from 'components/InputSelect/InputSelect';
+
+interface IZapperAPIResponse {
+  address: string;
+  balance: number;
+  balanceRaw: string;
+  balanceUSD: number;
+  decimals: number;
+  network: string;
+  price: number;
+  symbol: string;
+  type: string;
+}
+
+interface IToken {
+  symbol: string;
+  address: string;
+  balance: number;
+  price: number;
+}
 
 export const Zap = () => {
-  const { signer, balance, getBalance, zapIn, getTokenPriceInFrax } = useWallet();
+  const { signer, wallet, balance, getBalance, zapIn, getTokenPriceInFrax } =
+    useWallet();
   const treasuryMetrics = useRefreshableTreasuryMetrics();
+  const [tokensInWallet, setTokensInWallet] = useState<IToken[]>([]);
   const [tokenBalance, setTokenBalance] = useState(0);
   const [tokenAmount, setTokenAmount] = useState('0');
   const [zapped, setZapped] = useState(false);
   const [tokenPrice, setTokenPrice] = useState(0);
   const [templeQuote, setTempleQuote] = useState(0);
-  const [selectedToken, setSelectedToken] = useState({
+  const [selectedToken, setSelectedToken] = useState<IToken>({
     symbol: 'ETH',
-    address: ethers.constants.AddressZero
+    address: ethers.constants.AddressZero,
+    balance: 0,
+    price: 0,
   });
 
+  const getWalletTokenBalances = async () => {
+    if (wallet && signer) {
+      const PUBLIC_ZAPPER_API_KEY = '96e0cc51-a62e-42ca-acee-910ea7d2a241';
+      const res = await axios.get(
+        `https://api.zapper.fi/v1/protocols/tokens/balances?addresses[]=${wallet}&api_key=${PUBLIC_ZAPPER_API_KEY}`
+      );
+      if (res) {
+        //@ts-ignore
+        const tokenResponse: IZapperAPIResponse[] = Object.values(res.data)[0]
+          .products[0].assets;
+
+        const tokenArr: IToken[] = [];
+
+        tokenResponse.forEach((token) => {
+          tokenArr.push({
+            symbol: token.symbol,
+            address: token.address,
+            balance: token.balance,
+            price: token.price,
+          });
+        });
+        setTokensInWallet(tokenArr);
+      }
+    }
+  };
+
   //TODO: add to balance in walletprovider
-  const getEthBalance = async () => {
+  /*   const getEthBalance = async () => {
     if (signer) {
       const ethBal = await signer.getBalance();
       setTokenBalance(fromAtto(ethBal));
     } else setTokenBalance(0);
-  };
+  }; */
 
   const handleClick = (address: string, tokenAmount: string) => {
     setZapped(false);
@@ -45,27 +96,28 @@ export const Zap = () => {
   };
 
   const handleInput = async (value: string) => {
-    if(value !== ""){
+    if (value !== '') {
       setTokenAmount(value);
       // TODO: Replace hardcoded 0.7 with actual TEMPLE price
       //       once treasuryMetrics is working on this branch
-      setTempleQuote(tokenPrice * parseFloat(value) / 0.7);
+      setTempleQuote((selectedToken.price * parseFloat(value)) / 0.7);
     } else {
-      setTokenAmount("0");
+      setTokenAmount('0');
       setTempleQuote(0);
     }
   };
 
   const getQuote = async () => {
     const priceInFrax = await getTokenPriceInFrax(selectedToken.symbol);
-    if(priceInFrax){
+    if (priceInFrax) {
       setTokenPrice(priceInFrax);
     } else setTokenPrice(0);
-  }
+  };
 
   useEffect(() => {
+    getWalletTokenBalances();
     getQuote();
-    getEthBalance();
+    //getEthBalance();
     getBalance();
   }, [zapped]);
 
@@ -75,11 +127,28 @@ export const Zap = () => {
         <ConvoFlowTitle>ZAP ASSETS FOR {TEMPLE_TOKEN}</ConvoFlowTitle>
       </TitleWrapper>
 
+      <InputSelect
+        options={tokensInWallet.map((token) => {
+          return {
+            value: token.address,
+            label: token.symbol,
+          };
+        })}
+        onChange={(e) => {
+          tokensInWallet.forEach((token) => {
+            if (token.symbol === e.label) {
+              setSelectedToken(token);
+            }
+          });
+          getQuote();
+        }}
+      />
+
       <Input
         small
         isNumber
         crypto={{ kind: 'value', value: selectedToken.symbol }}
-        hint={`Balance: ${formatNumberWithCommas(tokenBalance)}`}
+        hint={`Balance: ${formatNumberWithCommas(selectedToken.balance)}`}
         placeholder={'0.00'}
         onChange={(e) => {
           handleInput(e.target.value);
