c962557cc391adc0152b2c3ff3581d1447d3350f
zelki <zelki.codes@protonmail.com>
2022-02-19 12:47:44 +0000

    chore(dapp): update yarn lock

--- apps/dapp/src/components/Pages/AMM/index.tsx
+++ apps/dapp/src/components/Pages/AMM/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import useCustomRouting, { CustomRoutingPageProps, NexusView } from 'hooks/use-custom-spa-routing';
+import useCustomRouting, {
+  CustomRoutingPageProps,
+  NexusView,
+} from 'hooks/use-custom-spa-routing';
 import MetamaskButton from 'components/Button/MetamaskButton';
 import DevotionCTA from 'components/Accessories/DevotionCTA';
 import { useHash } from 'hooks/use-query';
 import Spinner from 'components/Loader/Loader';
-import AMMErrorBoundary from './AMMErrorBoundary';
-import { AMMView } from '../AmmAltars';
+import AMMErrorBoundary from 'components/Pages/AMM/AMMErrorBoundary';
+import { AMMView } from 'components/Pages/AmmAltars';
 
-type ExoticComponentWithPreload = React.LazyExoticComponent<React.ComponentType<any>> & {
+type ExoticComponentWithPreload = React.LazyExoticComponent<
+  React.ComponentType<any>
+> & {
   preload: () => Promise<any>;
 };
 
 // Note(MrFujisawa): Struggling to type this stuff correctly.
 // Idea taken from https://medium.com/hackernoon/lazy-loading-and-preloading-components-in-react-16-6-804de091c82d
-const createLazyPreloadable = (dynamicImport: () => Promise<any>): ExoticComponentWithPreload => {
+const createLazyPreloadable = (
+  dynamicImport: () => Promise<any>
+): ExoticComponentWithPreload => {
   const ExoticComponent = React.lazy(dynamicImport);
   // @ts-ignore
   ExoticComponent.preload = dynamicImport;
@@ -27,14 +34,28 @@ import TempleGates from 'components/Pages/TempleGates';
 import Foyer from 'components/Pages/Foyer';
 
 const Account = createLazyPreloadable(() => import('components/Pages/Account'));
-const DashboardDoor = createLazyPreloadable(() => import('components/Pages/DashboardDoor'));
-const Dashboard = createLazyPreloadable(() => import('components/Pages/Dashboard'));
-const RitualsPosters = createLazyPreloadable(() => import('components/Pages/RitualsMoviePoster'));
+const DashboardDoor = createLazyPreloadable(
+  () => import('components/Pages/DashboardDoor')
+);
+const Dashboard = createLazyPreloadable(
+  () => import('components/Pages/Dashboard')
+);
+const RitualsPosters = createLazyPreloadable(
+  () => import('components/Pages/RitualsMoviePoster')
+);
 const Portals = createLazyPreloadable(() => import('components/Pages/Portals'));
-const AltarEnter = createLazyPreloadable(() => import('components/Pages/AltarEnter'));
-const AltarExit = createLazyPreloadable(() => import('components/Pages/AltarExit'));
-const AltarDevotion = createLazyPreloadable(() => import('components/Pages/AltarDevotion'));
-const AmmAltars = createLazyPreloadable(() => import('../AmmAltars'));
+const AltarEnter = createLazyPreloadable(
+  () => import('components/Pages/AltarEnter')
+);
+const AltarExit = createLazyPreloadable(
+  () => import('components/Pages/AltarExit')
+);
+const AltarDevotion = createLazyPreloadable(
+  () => import('components/Pages/AltarDevotion')
+);
+const AmmAltars = createLazyPreloadable(
+  () => import('components/Pages/AmmAltars')
+);
 
 const Container = styled.div`
   height: 100vh;
@@ -59,9 +80,7 @@ const Loader = () => (
 const CurrentPage = ({ routingHelper }: CustomRoutingPageProps) => {
   switch (routingHelper.currentPage) {
     case NexusView.TempleGates:
-      return (
-        <TempleGates routingHelper={routingHelper} />
-      );
+      return <TempleGates routingHelper={routingHelper} />;
     case NexusView.Account:
       return (
         <React.Suspense fallback={<Loader />}>
@@ -92,10 +111,9 @@ const CurrentPage = ({ routingHelper }: CustomRoutingPageProps) => {
         <React.Suspense fallback={<Loader />}>
           <DashboardDoor
             preloadPages={() => {
-              Promise.all([
-                Account.preload(),
-                Dashboard.preload(),
-              ]).catch((err) => console.error('[Preload Error] ', err));
+              Promise.all([Account.preload(), Dashboard.preload()]).catch(
+                (err) => console.error('[Preload Error] ', err)
+              );
             }}
             routingHelper={routingHelper}
           />
@@ -134,7 +152,9 @@ const CurrentPage = ({ routingHelper }: CustomRoutingPageProps) => {
           <AltarExit
             routingHelper={routingHelper}
             preloadPages={() => {
-              AmmAltars.preload().catch((err) => console.error('[Preload Error] ', err));
+              AmmAltars.preload().catch((err) =>
+                console.error('[Preload Error] ', err)
+              );
             }}
           />
         </React.Suspense>
@@ -162,7 +182,7 @@ const CurrentPage = ({ routingHelper }: CustomRoutingPageProps) => {
   // We should never get here as long as we've enumerated all the possible nexus routes.
   // throwing an error here for sanity sake -- which be picked up by our error boundary and logged.
   throw new Error('Programming Error: Attempted to render unknown NexusPage.');
-}
+};
 
 const AmmSpaRoot = () => {
   const hash = useHash();
@@ -171,7 +191,9 @@ const AmmSpaRoot = () => {
   const routingHelper = useCustomRouting(
     NexusView.TempleGates,
     isDiscordRedirect ? NexusView.Account : NexusView.TempleGates,
-    isDiscordRedirect ? [NexusView.TempleGates, NexusView.Foyer, NexusView.DashboardDoor] : []
+    isDiscordRedirect
+      ? [NexusView.TempleGates, NexusView.Foyer, NexusView.DashboardDoor]
+      : []
   );
 
   return (
