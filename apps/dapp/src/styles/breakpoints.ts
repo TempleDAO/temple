af763e5852ce8d7e0c91d46f1c5f2935be9f8c67
fujisawa <mrfujisawa@protonmail.com>
2022-02-21 14:02:30 -0800

    remove unused variable

--- apps/dapp/src/styles/breakpoints.ts
+++ apps/dapp/src/styles/breakpoints.ts
@@ -1,7 +1,5 @@
 import { css } from 'styled-components';
 
-export const MOBILE_BREAKPOINT = '64rem';
-
 export const aboveTabletBreakpoint = (styles: string) => {
   return css`
     @media screen and (min-width: ${({ theme }) => theme.metrics.devices.tablet}) {
