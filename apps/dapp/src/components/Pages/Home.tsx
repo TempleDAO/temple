f71eb91f33f3b3554fea49d3c92f65b2e24cd88b
fujisawa <mrfujisawa@protonmail.com>
2022-02-21 10:47:30 -0800

    move styles into the same file as component

--- apps/dapp/src/components/Pages/Home.tsx
+++ apps/dapp/src/components/Pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import styled, { css } from 'styled-components';
 
+import { Button, ButtonProps } from 'components/Button/Button';
+import { aboveMobileBreakpoint } from 'styles/breakpoints';
 import { Flex } from 'components/Layout/Flex';
 import Metrics from 'components/Metrics/Metrics';
 import useRefreshableTreasuryMetrics from 'hooks/use-refreshable-treasury-metrics';
@@ -17,21 +20,6 @@ import sunImage from 'assets/images/sun-art.svg';
 import sunsetImage from 'assets/images/sunset.svg';
 
 import { ResponsiveImage } from 'styles/common';
-
-import {
-  Row,
-  RowCell,
-  SunGateWrapper,
-  TempleDaoSun,
-  EarnStableGainsWrapper,
-  EarnStableGainsHeader,
-  SleepEasyStakingText,
-  ButtonGroup,
-  StyledButton,
-  ButtonContainer,
-  CircleBgWrapper,
-  MetricsWrapper,
-} from './styles';
 
 const Home = () => {
   const treasuryMetrics = useRefreshableTreasuryMetrics();
@@ -301,5 +289,142 @@ const Home = () => {
 };
 
 
+export const Row = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem 0;
+
+  ${aboveMobileBreakpoint(`
+    flex-direction: row;
+  `)}
+`;
+
+export const RowCell = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+
+  ${aboveMobileBreakpoint(`
+    width: 50%;
+    align-items: flex-start;
+
+    &:nth-child(odd) {
+      padding-right: 1.5rem;
+    }
+
+    &:nth-child(even) {
+      padding-left: 1.5rem;
+    }
+  `)}
+`;
+
+export const SunGateWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: fit-content;
+`;
+
+export const EarnStableGainsHeader = styled.h2`
+  font-size: 3rem;
+  line-height: 3.5625rem;
+  text-align: center;
+  margin: 0;
+
+  ${aboveMobileBreakpoint(`
+    text-align: left;
+    margin: 2em 0;
+  `)}
+`;
+
+export const SleepEasyStakingText = styled.h4`
+  margin: 0 0 2em;
+
+  ${aboveMobileBreakpoint(`
+    margin: 0 0 2.75em;
+  `)}
+`;
+
+export const EarnStableGainsWrapper = styled.div`
+  margin: 1rem 0 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+
+  ${aboveMobileBreakpoint(`
+    margin: 0;
+    text-align: initial;
+    margin-bottom: 3rem;
+  `)}
+`;
+
+export const TempleDaoSun = styled.div`
+  z-index: ${(props) => props.theme.zIndexes.up};
+
+  position: absolute;
+  transform-origin: center center;
+  transform: translateY(44%);
+  translate-origin: center center;
+
+  width: 53%;
+
+  img {
+    animation: ${(props) => props.theme.animations.spin};
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  max-width: 24.0625rem;
+  width: 100%;
+  margin: 0 auto 3rem;
+
+  ${aboveMobileBreakpoint(`
+    margin: 0 0 3.5rem;
+    justify-content: space-between;
+  `)}
+`;
+
+export const MetricsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  ${aboveMobileBreakpoint(`
+    justify-content: flex-start;
+  `)}
+`;
+
+interface CircleBgWrapperProps {
+  rightAlign?: boolean;
+}
+
+export const CircleBgWrapper = styled.div<CircleBgWrapperProps>`
+  position: absolute;
+  transform: translateX(-50%);
+
+  ${(props) =>
+    props.rightAlign &&
+    css`
+      right: 0;
+      transform: translateX(50%);
+    `}
+`;
+
+export const ButtonContainer = styled.div`
+  width: 9.6875rem;
+`;
+
+export const StyledButton = styled(Button)<ButtonProps>`
+  width: 100%;
+`;
 
 export default Home;
