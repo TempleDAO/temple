43f2b0fd2e540ee8ff4f6b876fe1df6dc240ae07
0xH4K8 <h4k8@protonmail.com>
2022-03-08 11:59:40 +1100

    chore: formatting

--- apps/dapp/src/components/RegisterToken/RegisterToken.tsx
+++ apps/dapp/src/components/RegisterToken/RegisterToken.tsx
@@ -4,20 +4,26 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 export interface Token {
-  type: 'ERC20',
+  type: 'ERC20';
   options: {
-    address: string,
-    symbol: string,
-    decimals: number,
-    image: string,
-  },
+    address: string;
+    symbol: string;
+    decimals: number;
+    image: string;
+  };
 }
 
 export interface RegisterTokenProps {
   token: Token;
 }
 
-const RegisterToken: FC<RegisterTokenProps> = ({token, token: {options: {image,symbol, address}}, children}) => {
+const RegisterToken: FC<RegisterTokenProps> = ({
+  token,
+  token: {
+    options: { image, symbol, address },
+  },
+  children,
+}) => {
   const { openNotification } = useNotification();
   const handleRegisterToken = async () => {
     try {
@@ -32,26 +38,33 @@ const RegisterToken: FC<RegisterTokenProps> = ({token, token: {options: {image,s
         openNotification({
           title: `Token ${symbol} added to your assets`,
           hash: address,
-          kind: 'SIMPLE'
-        })
+        });
       } else {
         console.log(`FAILED: Adding Token ${symbol} to your assets`);
       }
     } catch (error) {
       console.log(error);
     }
-  }
-  return <RegisterTokenButton onClick={handleRegisterToken}>
-    <Image src={image} width={32} height={32}  alt={`Register ${symbol} asset in Metamask`} title={`Register ${symbol} asset in Metamask`}/>
-    {/* Children can be used to add more info about the Token IE: [Image] register token [SYMBOL]*/}
-    {children}
-  </RegisterTokenButton>
-}
+  };
+  return (
+    <RegisterTokenButton onClick={handleRegisterToken}>
+      <Image
+        src={image}
+        width={32}
+        height={32}
+        alt={`Register ${symbol} asset in Metamask`}
+        title={`Register ${symbol} asset in Metamask`}
+      />
+      {/* Children can be used to add more info about the Token IE: [Image] register token [SYMBOL]*/}
+      {children}
+    </RegisterTokenButton>
+  );
+};
 
 const RegisterTokenButton = styled.div`
   display: inline-flex;
   align-items: center;
   cursor: pointer;
-`
+`;
 
 export default RegisterToken;
