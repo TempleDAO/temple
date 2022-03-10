4518e818e08d3313eb3428086ff844b365c23294
butlerji <>
2022-03-10 12:46:56 +1100

    Rebasing tokens all the way down implementation of vaults and strategies

---
+++ apps/protocol/contracts/core/Strategy.sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.8.4;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./RebasingERC20.sol";
+
+/**
+ * @title Captures the distribution of shares of a given strategy
+ *
+ * @notice When a strategy generates revenue, it's rebases (thereby)
+ * increasing the total supply of strategy tokens, without changing
+ * the relative proportions of any given share holder.
+ *
+ * This allows individual account holders to decide if they'd like
+ * to re-invest and compound their earnings from a strategy (the default
+ * if you take no action) or liquidate some portion of your share
+ */
+contract Strategy is Ownable, RebasingERC20 {
+    /// @dev Strategies are tightly coupled with vaults. Vaults
+    /// can increase (ie mint) and decrease (ie burn) their own
+    /// shares depending on their temple holdings
+    mapping(address => bool) public isVault;
+
+    /// @dev A number of the form p/q where q != 0
+    /// used to track amountPerShare
+    struct Rational {
+        uint256 p;
+        uint256 q;
+    }
+
+    Rational private _amountPerShare;
+
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * The default value of {decimals} is 18. To select a different value for
+     * {decimals} you should overload it.
+     *
+     * All two of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) { 
+        _amountPerShare.p = 1;
+        _amountPerShare.q = 1;
+    }
+
+    /**
+     * @notice Generate new strategy shares
+     * 
+     * @dev Only callable by a vault, and only to increase a vault's
+     * own share
+     */
+    function mint(uint256 amount) external onlyVault {
+        _mint(msg.sender, amount);
+    }
+
+    function addVault(address account) external onlyOwner {
+        isVault[account] = true;
+    }
+
+    function removeVault(address account) external onlyOwner {
+        isVault[account] = false;
+    }
+
+    function amountPerShare() public view override returns (uint256 p, uint256 q) {
+        p = _amountPerShare.p;
+        q = _amountPerShare.q;
+    }
+
+    function setAmountPerShare(uint256 p, uint256 q) external onlyOwner {
+        _amountPerShare.p = p;
+        _amountPerShare.q = q;
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
+    }
+
+    /**
+     * Throws if called by any account other than the manager.
+     */
+    modifier onlyVault() {
+        require(isVault[msg.sender], "Strategy: caller is not a vault");
+        _;
+    }
+}
--- apps/protocol/contracts/core/Vault.sol
+++ apps/protocol/contracts/core/Vault.sol
@@ -7,145 +7,143 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./VaultRevenueShare.sol";
+import "./RebasingERC20.sol";
+import "./Strategy.sol";
 
 // import "hardhat/console.sol";
 
 /**
- * Bookkeeping for a vault
+ * @title A temple investment vault, allows deposits and withdrawals on a set period (eg. monthly)
+ *
+ * @notice Each vault is a rebasing ERC20, users ca
+ *
+ * If a vault is in a cycle period, the any revenue is re-invested
  */
-contract Vault is EIP712, Ownable {
+contract Vault is EIP712, Ownable, RebasingERC20 {
     using Counters for Counters.Counter;
     mapping(address => Counters.Counter) public _nonces;
 
     // solhint-disable-next-line var-name-mixedcase
-    bytes32 public immutable LOCK_FOR_TYPEHASH = keccak256("lockFor(address owner, uint256 maxAmount, uint256 deadline, uint256 nonce)");
+    bytes32 public immutable DEPOSIT_FOR_TYPEHASH = keccak256("depositFor(address owner, uint256 maxAmount, uint256 deadline, uint256 nonce)");
 
-    // A number of the form p/q where q != 0
-    struct Rational {
-        uint256 p;
-        uint256 q;
-    }
+    // solhint-disable-next-line var-name-mixedcase
+    bytes32 public immutable WITHDRAW_FOR_TYPEHASH = keccak256("withdrawFor(address owner, uint256 amount, uint256 deadline, uint256 nonce)");
 
-    // All temple locked for any given user
-    mapping(address => uint256) public wenTemple;
-    uint256 public totalWenTemple;
+    /// @dev total temple deposited into vault, this isn't the total 
+    /// temple in the vault (as that is deposits + revenue). A users
+    /// share of a vault is a function of the total temple in the vault
+    /// (that is, their balanceOf(account) / totalSupply * totalTemple)
+    uint256 public totalDepositsTemple;
 
     IERC20 public templeToken;
-    VaultRevenueShare public vaultRevenueShare;
-    uint256 public lockEndTimestamp;
-    uint256 public joinEndTimestamp;
-    Rational public shareMultiplier;
 
-    event Join(address account, uint256 increasedByTemple, uint256 totalLockedTemple, uint256 lockedUntil, uint256 sharesEarned);
-    event Leave(address account, uint256 amount);
-    event Claim(address account, uint256 depositedTemple, uint256 claimedTemple);
+    /// @dev share of main revenue strategy owned by this vault, should be 1 to 1 with the
+    /// vault's total temple
+    Strategy public treasuryInvestmentRevenueStrategy;
+
+    /// @dev share of revenue from any re-investment strategy. At time 0 revenueSharetrategy
+    /// and revenueReinvestmentStrategy should be in the same proportions, and this will
+    /// diverge over time.
+    Strategy public revenueReinvestmentStrategy;
+
+    /// @dev timestamp (in seconds) of the first period in this vault
+    uint256 public firstPeriodStartTimestamp;
+
+    /// @dev how often a vault cycles, in seconds
+    uint256 public periodDuration;
+
+    /// @dev window from cycle start in which accounts can enter/exit the vault
+    uint256 public enterExitWindowDuration;
+
 
     constructor(
+        string memory _name,
+        string memory _symbol,
         IERC20 _templeToken,
-        RevenueShare _vaultRevenueShare,
-        uint256 lockDurationSeconds,
-        uint256 joinDurationSeconds,
-        Rational shareMultiplier
-
-    ) EIP712("Vault", "1") {
+        Strategy _treasuryInvestmentRevenueStrategy,
+        Strategy _revenueReinvestmentStrategy,
+        uint256 _periodDuration,
+        uint256 _enterExitWindowDuration
+    ) EIP712(_name, "1") ERC20(_name, _symbol)  {
         templeToken = _templeToken;
-        revenueShare = _vaultRevenueShare;
-        lockEndTimestamp = block.timestamp + lockDurationSeconds;
-        joinEndTimestamp = block.timestamp + joinDurationSeconds;
+        treasuryInvestmentRevenueStrategy = _treasuryInvestmentRevenueStrategy;
+        revenueReinvestmentStrategy = _revenueReinvestmentStrategy;
+        periodDuration = _periodDuration;
+        enterExitWindowDuration = _enterExitWindowDuration;
+
+        firstPeriodStartTimestamp = block.timestamp;
     }
 
     /**
-    * _amount: $TEMPLE to lock
-    */
-    function join(uint256 amount) public {
-        joinFor(msg.sender, amount);
+     * @notice Deposit temple into a vault
+     */
+    function deposit(uint256 amount) public {
+        depositFor(msg.sender, amount);
     }
 
     /**
-     * Lock for another user
+     * @notice Deposit for another user (gassless for the temple owner)
      * (assuming the owner has given authority to the caller to act on their behalf)
      * 
-     * Gasless for the owner
-     *
-     * NOTE: amount is explicitly _not_ part of the digest, as in the common use case
+     * @dev amount is explicitly _not_ part of the digest, as in the common use case
      * the owner often doesn't know exactly how much will be locked (example, AMM buy with
      * immediate lock). We do capture the max however to mitigate any possibly attack vectors
      */
-    function joinFor(address owner, uint256 amount, uint256 maxAmount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public {
+    function depositFor(address owner, uint256 amount, uint256 maxAmount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public {
         require(block.timestamp <= deadline, "Vault: expired deadline");
         require(amount <= maxAmount, "Vault: amount must be less than authorized maxAmount");
 
-        bytes32 structHash = keccak256(abi.encode(LOCK_FOR_TYPEHASH, owner, maxAmount, unlockDelaySeconds, deadline, _useNonce(owner)));
+        bytes32 structHash = keccak256(abi.encode(DEPOSIT_FOR_TYPEHASH, owner, maxAmount, deadline, _useNonce(owner)));
         bytes32 digest = _hashTypedDataV4(structHash);
         address signer = ECDSA.recover(digest, v, r, s);
 
         require(signer == owner, "Vault: invalid signature");
 
-        lockFor(owner, amount, unlockDelaySeconds);
+        depositFor(owner, amount);
     }
 
     /**
-     * Must be private, otherwise security issue where anyone can
-     * refresh lock for another wallet (and lock more if they have an allowance)
+     * @dev public and permisionless, however for correctness within reasonably
+     * time bounds this only needs to be called during a vault deposit/withdrawal
      */
-    function joinFor(address _account, uint256 _amount, uint256 _unlockDelaySeconds) private {
-        require(block.timestamp <= joinEndTimestamp, "Vault: Cannot no longer join vault");
+    function syncStrategyShares() public {
+        uint256 totalTemple = templeToken.balanceOf(address(this));
+        uint totalRevenueShares = treasuryInvestmentRevenueStrategy.balanceOf(address(this));
 
-        uint256 lockDuration = newLockedUntilTimestamp - block.timestamp;
-
-        wenTemple[_account] += _amount;
-        totalWenTemple += _amount;
-
-        if (_amount > 0) {
-            SafeERC20.safeTransferFrom(templeToken, _account, address(this), _amount);
-            revenueShare.increase(address(this), amount * shareMultiplier.p / shareMultiplier.q);
-        }
-
-        emit Lock(_account, _amount, lockEntry.amount, lockEntry.lockedUntilTimestamp);
-    }
-
-    /** Withdraw locked entry */
-    function leave(uint256 _amount) public {
-        require(block.timestamp <= joinEndTimestamp, "Vault: Can no longer leave vault until the end timestamp");
-        require(wenTemple[msg.sender] <= lockEntry.amount, "Vault: can't unlock more than originally locked");
-
-        revenueShare.decrease(address(this), _amount * shareMultiplier.p / shareMultiplier.q);
-        exitFor(msg.sender, _amount, _amount);
-        emit Unlock(msg.sender, _amount);
-    }
-
-    /**
-     * Once the vault expires, claim
-     */
-    function claimFor(address _account, uint256 _amount) public {
-        require(block.timestamp <= lockEndTimestamp, "Vault: temple still locked");
-        require(revenueShare.balanceOf(address(this)) == 0, "Vault: vault is not in claim mode (cannot claim while vault still has a revenue share)");
-
-        uint256 depositedTemple = wenTemple[_account];
-        require(depositedTemple > 0, "Vault: no temple in vault");
-
-        uint256 claimedTemple = templeToken.balanceOf(address(this)) * depositedTemple / totalWenTemple;
-        exitFor(_account, depositedTemple, claimedTemple);
-        emit Claim(_account, depositedTemple, claimedTemple);
-    }
-
-    /**
-     * Bookkeeping to remove a specified balance + claim from this vault
-     */
-    function exitFor(address _account, uint256 _depositedAmount, uint256 _claimAmount) private {
-        totalWenTemple -= _depositedTemple;
-        wenTemple[_account] -= _depositedTemple;
-
-        if (_claimAmount > 0) {
-            SafeERC20.safeTransfer(templeToken, _account, _amount);
+        if (totalTemple > totalRevenueShares) { // mint extra revenue/reinvestment shares
+            treasuryInvestmentRevenueStrategy.mint(totalTemple - totalRevenueShares);
+            revenueReinvestmentStrategy.mint(totalTemple - totalRevenueShares);
+        } else if (totalTemple < totalRevenueShares) { // burn shares to account for withdrawal
+            treasuryInvestmentRevenueStrategy.burn(totalRevenueShares - totalTemple);
+            revenueReinvestmentStrategy.burn(totalRevenueShares - totalTemple);
+            // TODO(butlerji): Is the above correct for balancing out the reinvestment strategy?
+            // I think so (it needs extra eyes)
         }
     }
 
-    // NOTE(bulterji): I don't think this belongs on each vault
-    function enableClaimMode() onlyOwner {
-        require(block.timestamp > joinEnd, "Vault: can only enable claim mode once the vault unlocks");
-        revenueShare.decrease(revenue.balanceOf(address(this)));
+    /**
+     * @notice Withdraw temple (and any earned revenue) from the vault
+     */
+    function withdraw(uint256 amount) public {
+        withdrawFor(msg.sender, amount);
+    }
+
+    /**
+     * @notice Withdraw for another user (gasless for the vault token holder)
+     * (assuming the owner has given authority for the caller to act on their behalf)
+     * 
+     * @dev amount is explicit, to allow use case of partial vault withdrawals
+     */
+    function withdrawFor(address owner, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public {
+        require(block.timestamp <= deadline, "Vault: expired deadline");
+
+        bytes32 structHash = keccak256(abi.encode(WITHDRAW_FOR_TYPEHASH, owner, amount, deadline, _useNonce(owner)));
+        bytes32 digest = _hashTypedDataV4(structHash);
+        address signer = ECDSA.recover(digest, v, r, s);
+
+        require(signer == owner, "Vault: invalid signature");
+
+        withdrawFor(owner, amount);
     }
 
     /**
@@ -171,4 +169,78 @@ contract Vault is EIP712, Ownable {
         current = nonce.current();
         nonce.increment();
     }
+
+    function amountPerShare() public view override returns (uint256 p, uint256 q) {
+        p = templeToken.balanceOf(address(this));
+        q = totalDepositsTemple;
+    }
+
+    function inEnterExitWindow() public returns (bool) {
+        // TODO(butlerji): Implement - there is a lot of chat on how to best do
+        // this so left as a stub for now
+        return true;
+    }
+
+    /**
+     * @dev shared private implementation of depositFor. Must be private, to prevent
+     * security issue where anyone can deposit (and lock) for another account, once
+     * said account as approved this contract to pull temple funds.
+     */
+    function depositFor(address _account, uint256 _amount) private {
+        require(inEnterExitWindow(), "Vault: Cannot join vault when outside of enter/exit window");
+
+        totalDepositsTemple += _amount;
+
+        if (_amount > 0) {
+            SafeERC20.safeTransferFrom(templeToken, _account, address(this), _amount);
+            syncStrategyShares();
+            _mint(_account, _amount);
+        }
+
+        emit Deposit(_account, _amount);
+    }
+
+    /**
+     * @dev shared private implementation of withdrawFor. Must be private, to prevent
+     * security issue where anyone can withdraw for another account. Isn't as severe as
+     * depositFor (as there are no locks), however still a nucance if an account is
+     * exited from a vault without consent.
+     */
+    function withdrawFor(address _account, uint256 _amount) private {
+        require(inEnterExitWindow(), "Vault: Cannot exit vault when outside of enter/exit window");
+
+        // TODO(bulterji): I think this math is wrong. Review
+        totalDepositsTemple -= toSharesAmount(_amount);
+        SafeERC20.safeTransferFrom(templeToken, address(this), msg.sender, _amount);
+
+        if (_amount > 0) {
+            syncStrategyShares();
+            _burnFrom(_account, _amount);
+        }
+
+        emit Withdraw(_account, _amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function _burnFrom(address account, uint256 amount) private {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
+    }
+
+    event Deposit(address account, uint256 amount);
+    event Withdraw(address account, uint256 amount);
 }
