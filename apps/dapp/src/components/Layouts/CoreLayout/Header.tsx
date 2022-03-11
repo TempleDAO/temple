1f9fd2eca134062dea897af47cd7900ee087e274
fujisawa <mrfujisawa@protonmail.com>
2022-03-10 18:41:04 -0800

    Add mobile navigation

--- apps/dapp/src/components/Layouts/CoreLayout/Header.tsx
+++ apps/dapp/src/components/Layouts/CoreLayout/Header.tsx
@@ -1,35 +1,81 @@
-import { FC, useRef, useState, useCallback, useLayoutEffect } from 'react';
+import { FC, useRef, useState, useCallback, useLayoutEffect, SyntheticEvent } from 'react';
 import { Link, useResolvedPath, useMatch } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import styled from 'styled-components';
 
 import { useWallet } from 'providers/WalletProvider';
 import { flexCenter, buttonResets, backgroundImage } from 'styles/mixins';
 import { UnstyledList } from 'styles/common';
+import { theme } from 'styles/theme';
+import { tabletAndAbove } from 'styles/breakpoints';
 
 import selectorIcon from 'assets/icons/nav-selector-icon.svg';
 import templeDaoLogo from 'assets/images/sun-art.svg';
 import metamaskLogo from 'assets/images/metamask-transparent.svg';
+import hamburger from 'assets/icons/core-hamburger.svg';
+import hamburgerX from 'assets/icons/core-x-hamburger.svg';
+import mobileBackgoundImage from 'assets/images/mobile-background-geometry.svg';
 
 const Header = () => {
   const { connectWallet, changeWalletAddress, wallet } = useWallet();
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const isAboveMobile = useMediaQuery({
+    query: `(min-width: ${theme.metrics.devices.tablet})`,
+  });
 
+  const onClickMenuItem = useCallback(() => {
+    setIsNavOpen(false);
+  }, [setIsNavOpen]);
+
+  // Desktop
+  if (isAboveMobile) {
+    return (
+      <Wrapper>
+        <Logo to="/core">
+          TempleDAO
+        </Logo>
+        <Navigation isNavOpenMobile={isNavOpen} />
+        <MetamaskButton
+          aria-label={wallet ? 'Change Wallet' : 'Connect Wallet'}
+          onClick={wallet ? changeWalletAddress : connectWallet}
+        />
+      </Wrapper>
+    );
+  }
+
+  // Mobile
   return (
     <Wrapper>
+      <MobileNavLeft>
+        <HamburgerBun
+          aria-label={`${isNavOpen ? 'Close' : 'Open'} navigation`}
+          $isOpen={isNavOpen}
+          onClick={() => setIsNavOpen((isOpen) => !isOpen)}
+        />
+        <MetamaskButton
+          aria-label={wallet ? 'Change Wallet' : 'Connect Wallet'}
+          onClick={wallet ? changeWalletAddress : connectWallet}
+        />
+      </MobileNavLeft>
+      <Navigation
+        isNavOpenMobile={isNavOpen}
+        onClickMenuItem={onClickMenuItem}
+      />
       <Logo to="/core">
         TempleDAO
       </Logo>
-      <Navigation />
-      <MetamaskButton
-        aria-label={wallet ? 'Change Wallet' : 'Connect Wallet'}
-        onClick={wallet ? changeWalletAddress : connectWallet}
-      />
     </Wrapper>
   );
 };
 
 export default Header;
 
-const Navigation = () => {
+interface NavigationProps {
+  isNavOpenMobile: boolean;
+  onClickMenuItem?: (event: SyntheticEvent) => void;
+}
+
+const Navigation = ({ isNavOpenMobile, onClickMenuItem }: NavigationProps) => {
   const [selectorPosition, setSelectorPosition] = useState(0);
 
   const onMenuItemActive = useCallback((offsetX: number) => {
@@ -38,36 +84,41 @@ const Navigation = () => {
   }, [setSelectorPosition]);
 
   return (
-    <NavWrapper>
+    <NavWrapper $isOpen={isNavOpenMobile}>
       <Menu>
         <MenuItem
           to="/core"
           strictMatch
           onMenuItemActive={onMenuItemActive}
+          onClick={onClickMenuItem}
         >
           Home
         </MenuItem>
         <MenuItem
           to="/core/vaults"
           onMenuItemActive={onMenuItemActive}
+          onClick={onClickMenuItem}
         >
           Vaults
         </MenuItem>
         <MenuItem
           to="/core/trade"
           onMenuItemActive={onMenuItemActive}
+          onClick={onClickMenuItem}
         >
           Trade
         </MenuItem>
         <MenuItem
           to="/core/profile"
           onMenuItemActive={onMenuItemActive}
+          onClick={onClickMenuItem}
         >
           Profile
         </MenuItem>
         <MenuItem
           to="/core/analytics"
           onMenuItemActive={onMenuItemActive}
+          onClick={onClickMenuItem}
         >
           Analytics
         </MenuItem>
@@ -81,6 +132,7 @@ interface MenuItemProps {
   to: string;
   strictMatch?: boolean;
   onMenuItemActive: (offsetX: number) => void;
+  onClick?: (event: SyntheticEvent) => void;
 }
 
 const MenuItem: FC<MenuItemProps> = ({
@@ -88,6 +140,7 @@ const MenuItem: FC<MenuItemProps> = ({
   children,
   onMenuItemActive,
   strictMatch = false,
+  onClick,
 }) => {
   const resolved = useResolvedPath(to);
   const match = useMatch({ path: resolved.pathname, end: strictMatch });
@@ -111,6 +164,7 @@ const MenuItem: FC<MenuItemProps> = ({
         ref={menuItemRef}
         to={to}
         $active={!!match}
+        onClick={onClick}
       >
         {children}
       </NavLink>
@@ -118,7 +172,8 @@ const MenuItem: FC<MenuItemProps> = ({
   );
 };
 
-const SELECTOR_WIDTH = 23;
+const SELECTOR_WIDTH = 23; // pixels
+const NAV_MOBILE_HEIGHT = 52; // pixels
 
 // Component Colors
 const COLOR_NAV_HOVER = '#FFDEC9';
@@ -126,27 +181,57 @@ const COLOR_NAV_SHADOW = '0px 0px 0.3125rem rgba(222, 92, 6, 0.5)';
 const COLOR_NAV_BACKGROUND_GRADIENT_START = '#0B0A0A';
 const COLOR_NAV_BACKGROUND_GRADIENT_END = '#1D1A1A';
 
+const HamburgerBun = styled.button<{ $isOpen: boolean }>`
+  ${buttonResets}
+  ${({ $isOpen }) => backgroundImage($isOpen ? hamburgerX : hamburger, { position: 'left center' })}
+  width: ${({ $isOpen }) => $isOpen ? 1.3125 : 1.3125}rem;
+  height: ${({ $isOpen }) => $isOpen ? 1 : 0.875}rem;
+  margin-right: 1.125rem;
+
+  ${tabletAndAbove(`
+    display: none;
+  `)}
+`;
+
+const MobileNavLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const Wrapper = styled.header`
-  background: linear-gradient(180deg, ${COLOR_NAV_BACKGROUND_GRADIENT_START} 0%, ${COLOR_NAV_BACKGROUND_GRADIENT_END} 100%);
-  border-bottom: 0.0625rem solid ${({ theme }) => theme.palette.brand};
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 0 1.75rem;
+  height: ${NAV_MOBILE_HEIGHT / 16}rem;
+  background: ${COLOR_NAV_BACKGROUND_GRADIENT_START};
+
+  ${tabletAndAbove(`
+    height: auto;
+    background: linear-gradient(180deg, ${COLOR_NAV_BACKGROUND_GRADIENT_START} 0%, ${COLOR_NAV_BACKGROUND_GRADIENT_END} 100%);
+    border-bottom: 0.0625rem solid ${theme.palette.brand};
+  `)}
 `;
 
 const Logo = styled(Link)`
   ${backgroundImage(templeDaoLogo)}
   display: block;
-  width: 2.625rem;
-  height: 2.625rem;
+  width: 1.75rem;
+  height: 1.75rem;
   overflow: hidden;
   text-indent: -999rem;
+
+  ${tabletAndAbove(`
+    width: 2.625rem;
+    height: 2.625rem;
+  `)}
 `;
 
 const Selector = styled.span<{ $position: number }>`
   ${backgroundImage(selectorIcon)}
+
+  display: none;
   bottom: 0;
   content: '';
   width: ${SELECTOR_WIDTH / 16}rem;
@@ -154,56 +239,112 @@ const Selector = styled.span<{ $position: number }>`
   position: absolute;
 
   transition: transform 250ms ease-in-out;
-  display: ${({ $position }) => $position ? 'block' : 'none'};
   transform: translate(${({ $position }) => $position / 16}rem, 50%);
+
+  ${({ $position }) => tabletAndAbove(`
+    display: ${$position ? 'block' : 'none'};
+  `)}
 `;
 
-const NavWrapper = styled.nav`
-  position: relative;
+const NavWrapper = styled.nav<{ $isOpen: boolean }>`
+  display: ${({ $isOpen }) => $isOpen ? 'flex' : 'none'};
+  position: fixed;
+  top: ${NAV_MOBILE_HEIGHT / 16}rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding-top: 5.0625rem;
+
+  ${backgroundImage(mobileBackgoundImage, {
+    color: COLOR_NAV_BACKGROUND_GRADIENT_START,
+    size: 'cover',
+  })}
+
+  ${tabletAndAbove(`
+    top: 0;
+    padding-top: 0;
+    position: relative;
+    display: block;
+    background: transparent;
+  `)}
 `;
 
 const Menu = styled(UnstyledList)`
-  display: flex;
-  flex-direction: row;
-  border-left: 0.0625rem solid ${({ theme }) => theme.palette.brand};
-  border-right: 0.0625rem solid ${({ theme }) => theme.palette.brand};
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 1.9375rem;
 
   > li {
-    border-right: 0.0625rem solid ${({ theme }) => theme.palette.brand};
+    margin-bottom: 2.5rem;
 
     &:last-of-type {
-      border-right: none;
+      margin-bottom: 0;
     }
   }
+
+  ${tabletAndAbove(`
+    padding: 0;
+    flex-direction: row;
+    border-left: 0.0625rem solid ${theme.palette.brand};
+    border-right: 0.0625rem solid ${theme.palette.brand};
+
+    > li {
+      margin: 0;
+      border-right: 0.0625rem solid ${theme.palette.brand};
+
+      &:last-of-type {
+        border-right: none;
+      }
+    }
+  `)}
 `;
 
 const MetamaskButton = styled.button`
   ${buttonResets}
   ${backgroundImage(metamaskLogo)}
-  width: 3.4375rem;
-  height: 3.375rem;
+  width: 1.5rem;
+  height: 1.5rem;
+
+  ${tabletAndAbove(`
+    width: 3.4375rem;
+    height: 3.375rem;
+  `)}
 `;
 
 const NavLink = styled(Link)<{ $active?: boolean }>`
   ${flexCenter}
   ${({ theme }) => theme.typography.fonts.fontBody}
 
-  font-size: 1rem;
-  line-height: 1.25rem;
-  font-weight: bold;
-  letter-spacing: 0.05em;
-  min-width: 7.5rem;
-  position: relative;
-
-  padding: 1.625rem 0.875rem 1.3125rem;
+  font-size: 2rem;
+  line-height: 2.3125rem;
+  letter-spacing: 0.1em;
+  font-weight: normal;
   transition: all 150ms ease-in;
 
   color: ${({ theme, $active }) => $active ? COLOR_NAV_HOVER : theme.palette.brand};
-  text-shadow: ${({ $active }) => $active ? COLOR_NAV_SHADOW : 'none'};
+  text-shadow: ${({ $active }) => $active ? '0px 0px 0.6428rem rgba(222, 92, 6, 0.5)' : 'none'};
 
   &:hover {
     color: ${COLOR_NAV_HOVER};
-    text-shadow: ${COLOR_NAV_SHADOW};
+    text-shadow: ${({ $active }) => $active ? '0px 0px 0.6428rem rgba(222, 92, 6, 0.5)' : 'none'};
   }
+
+  ${({ $active }) => tabletAndAbove(`
+    font-size: 1rem;
+    line-height: 1.25rem;
+    font-weight: bold;
+    letter-spacing: 0.05em;
+    min-width: 7.5rem;
+    position: relative;
+
+    padding: 1.625rem 0.875rem 1.3125rem;
+
+    text-shadow: ${$active ? COLOR_NAV_SHADOW : 'none'};
+
+    &:hover {
+      text-shadow: ${COLOR_NAV_SHADOW};
+    }
+  `)}
 `;
