b7d659d4eb116c6230f40d32e0e611929a9c5576
Alex <astanciu@gmail.com>
2022-03-16 19:37:47 -0400

    update app layout
    Add an app container that servers as flex root. From there flex columns for header and body. This is to ensure everything is flex parented correctly

--- apps/dapp/src/components/Layouts/CoreLayout/index.tsx
+++ apps/dapp/src/components/Layouts/CoreLayout/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import { tabletAndAbove } from 'styles/breakpoints';
 import { pixelsToRems } from 'styles/mixins';
@@ -9,26 +9,39 @@ import Header, { NAV_MOBILE_HEIGHT_PIXELS } from './Header';
 const CoreLayout = () => (
   <>
     <GlobalStyleOverride />
-    <Header />
-    <Main>
-      <Outlet />
-    </Main>
+    <AppContainer>
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </AppContainer>
   </>
 );
 
 export default CoreLayout;
 
 const GlobalStyleOverride = createGlobalStyle`
-  html, body {
-    min-height: 100vh;
-    min-width: 100vw;
+html, body {
+   height: 100%;
+   height: -webkit-fill-available;
+    margin: 0;
+    padding: 0;
   }
 `;
 
+const AppContainer = styled.div`
+  border: 1px dashed white;
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
+  height: 100vh;
+`;
 const Main = styled.main`
+  border: 1px solid blue;
+  min-width: ${({ theme }) => theme.metrics.desktop.minWidth};
   max-width: ${({ theme }) => theme.metrics.desktop.maxWidth};
-  margin: 0 auto;
-  padding: ${pixelsToRems(NAV_MOBILE_HEIGHT_PIXELS)}rem 1.75rem 1.75rem;
+  flex-grow: 1;
+  align-self: center;
 
   ${tabletAndAbove(`
     padding: 0 1.75rem;
