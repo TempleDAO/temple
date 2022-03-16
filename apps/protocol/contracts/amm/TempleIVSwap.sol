2624ce82da1eb6d2f75cf999360214180ccc2e00
butlerji <>
2022-03-16 17:32:00 +1100

    Made TempleIVSwap compile

--- apps/protocol/contracts/amm/TempleIVSwap.sol
+++ apps/protocol/contracts/amm/TempleIVSwap.sol
@@ -2,50 +2,70 @@ pragma solidity ^0.8.4;
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-import "../TempleERC20Token.sol";
 
 import "hardhat/console.sol";
 
 
 contract TempleIVSwap is Ownable {
-    TempleERC20Token public immutable templeToken;
+    ERC20Burnable public immutable templeToken;
+    IERC20 public immutable stablecToken;
 
     struct Price {
         uint frax;
         uint temple;
     }
 
-    // some portion of all buys above threshold get minted on protocol
+    /// @notice intrinsinc value gauranteed by the protocol
     Price public iv;
 
-    modifier ensure(uint deadline) {
-        require(deadline >= block.timestamp, 'TempleFraxAMMRouter: EXPIRED');
-        _;
-    }
-
     constructor(
-        TempleERC20Token _templeToken,
+        ERC20Burnable _templeToken,
+        IERC20 _stablecToken,
         Price memory _iv
     ) {
         templeToken = _templeToken;
+        stablecToken = _stablecToken;
         iv = _iv;
     }
 
-
-    function swapExactTempleForFrax(
+    /**
+     * @notice swap temple for stablec at IV
+     */
+    function swapTempleForIV(
         uint amountIn,
         address to,
         uint deadline
     ) external virtual ensure(deadline) {
-        SafeERC20.burnFrom(templeToken, msg.sender);
-        SafeERC20.transfer(templeToken, to, amountIn * (iv.frax / iv.temple));
+        templeToken.burnFrom(msg.sender, amountIn);
+        SafeERC20.safeTransfer(stablecToken, to, amountIn * (iv.frax / iv.temple));
     }
 
+    /**
+     * Explicitly set IV
+     */
     function setIV(uint256 frax, uint256 temple) external onlyOwner {
         iv.frax = frax;
         iv.temple = temple;
+    }
+
+    /**
+     * @notice transfer out amount of token to provided address
+     */
+    function withdraw(address token, address to, uint256 amount) external onlyOwner {
+      require(to != address(0), "to address zero");
+      if (token == address(0)) {
+        (bool sent,) = payable(to).call{value: amount}("");
+        require(sent, "send failed");
+      } else {
+        SafeERC20.safeTransfer(IERC20(token), to, amount);
+      }
+    }
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, 'TempleIVSwap: EXPIRED');
+        _;
     }
 }
