6af45fa24558aa885a87a3c0873182eab56729b5
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-03-16 14:33:17 -0700

    Add mobile menu (#159)
    * wip: dummy commit in order to open PR
    
    * Add mobile navigation
    
    * dry up a bit:
    
    * vertically center menu
    
    * use theme color
    
    * use variables for colors
    
    * Fix positioning, z-indexes, make mobile header fixed'
    
    * add mobile padding
    
    * fix formatting
    
    * Add pixel to rems helper
    
    * pr feedback
    
    Co-authored-by: zelki <zelki.codes@protonmail.com>

--- apps/dapp/src/components/Layouts/CoreLayout/index.tsx
+++ apps/dapp/src/components/Layouts/CoreLayout/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
+import { tabletAndAbove } from 'styles/breakpoints';
+import { pixelsToRems } from 'styles/mixins';
 
-import Header from './Header';
+import Header, { NAV_MOBILE_HEIGHT_PIXELS } from './Header';
 
 const CoreLayout = () => (
   <>
@@ -26,5 +28,9 @@ const GlobalStyleOverride = createGlobalStyle`
 const Main = styled.main`
   max-width: ${({ theme }) => theme.metrics.desktop.maxWidth};
   margin: 0 auto;
-  padding: 0 1.75rem 1.75rem;
+  padding: ${pixelsToRems(NAV_MOBILE_HEIGHT_PIXELS)}rem 1.75rem 1.75rem;
+
+  ${tabletAndAbove(`
+    padding: 0 1.75rem 1.75rem;
+  `)}
 `;
