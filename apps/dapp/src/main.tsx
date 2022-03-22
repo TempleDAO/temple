92a54582a6b0c0765b8bf516363cf865a09dffb5
jebsley3 <cuiett@protonmail.com>
2022-03-22 18:46:35 +0000

    fix: update import

--- apps/dapp/src/main.tsx
+++ apps/dapp/src/main.tsx
@@ -17,7 +17,7 @@ import Disclaimer from 'components/Pages/Disclaimer';
 import CoreLayout from 'components/Layouts/CoreLayout';
 import AnalyticsPage from 'components/Pages/Core/Analytics';
 import VaultPage from 'components/Pages/Core/Vault';
-import ProfilePage from 'components/Pages/Core/Profile';
+import ProfilePage from 'components/Pages/Core/Profile/Profile';
 import VaultListPage from 'components/Pages/Core/VaultList';
 import { Claim as VaultClaim } from 'components/Pages/Core/VaultPages/Claim';
 import { Stake } from 'components/Pages/Core/VaultPages/Stake';
