9dedb0b63abf7401e1df5b80d2cd59ba296265b7
ALECKS <0xalecks@gmail.com>
2022-03-28 11:13:14 -0400

    refactor desktop vault split

--- apps/dapp/src/components/Vault/VaultSVG.tsx
+++ apps/dapp/src/components/Vault/VaultSVG.tsx
@@ -1,20 +1,12 @@
-import { useRef, useState, PropsWithChildren, useEffect } from 'react';
-import styled from 'styled-components';
-import { useLocation, useNavigate } from 'react-router-dom';
-
-import { Definitions } from './desktop-parts/Definitions';
-import { Background } from './desktop-parts/Background';
-import { InnerRing } from './desktop-parts/InnerRing';
-import { OuterRing } from './desktop-parts/OuterRing';
-import { MarkerBubble } from './desktop-parts/MarkerBubble';
+import { useRef, useState, PropsWithChildren } from 'react';
 import { useOutsideClick } from '../../hooks/useOutsideClick';
-import { Entry, Point, Vault, VaultPage } from './types';
+import { Entry, Point, Vault, VaultRef } from './types';
 import { processData } from './desktop-parts/utils';
-import { RingButtons } from './desktop-parts/RingButtons';
-import { Timeline } from './desktop-parts/timeline/Timeline';
-import { pixelsToRems } from 'styles/mixins';
-import { NAV_DESKTOP_HEIGHT_PIXELS } from 'components/Layouts/CoreLayout/Header';
-import { Maybe } from 'types/util';
+import { useMediaQuery } from 'react-responsive';
+import { theme } from 'styles/theme';
+import { VaultDesktop } from './VaultDesktop';
+import { VaultMobile } from './VaultMobile';
+import { useSelectedPage } from './useSelectedPage';
 
 type Props = {
   data: Vault;
@@ -42,13 +34,16 @@ const useSelectedVaultPage = (): Maybe<VaultPage> => {
 };
 
 export const VaultSVG = ({ data, children }: PropsWithChildren<Props>) => {
-  const navigate = useNavigate();
-  const selectedNav = useSelectedVaultPage();
-  const svgRef = useRef<SVGSVGElement>(null);
-  const popupRef = useRef(null);
+  const selectedNav = useSelectedPage();
+  const vaultRef = useRef<VaultRef>(null);
   const [selectedEntry, setSelectedEntry] = useState<Entry>();
   const [markerPosition, setMarkerPosition] = useState<Point>({ x: 0, y: 0 });
-  useOutsideClick(popupRef, () => {
+
+  const isDesktop = useMediaQuery({
+    query: `(min-width: ${theme.metrics.devices.tablet})`,
+  });
+
+  useOutsideClick(vaultRef.current?.popupRef!, () => {
     setSelectedEntry(undefined);
   });
 
@@ -61,7 +56,7 @@ export const VaultSVG = ({ data, children }: PropsWithChildren<Props>) => {
 
     const point = DOMPoint.fromPoint(markerCenterInScreenCoords);
     const marketCenterInSVGCoords = point.matrixTransform(
-      svgRef.current?.getScreenCTM()?.inverse()
+      vaultRef.current?.svgRef?.getScreenCTM()?.inverse()
     );
     // offset so the location is in the circle not the top left of bubble
     marketCenterInSVGCoords.x -= 125;
@@ -72,62 +67,25 @@ export const VaultSVG = ({ data, children }: PropsWithChildren<Props>) => {
   };
 
   const vault = processData(data);
-  return (
-    <>
-      <BoundingBox>
-        <svg height="100%" viewBox="0 0 1000 1000" fill="none" ref={svgRef}>
-          <Background />
-          <OuterRing selected={selectedNav} />
-          <RingButtons
-            selected={selectedNav}
-            onClickButton={(page) => {
-              navigate(`/core/dapp/vaults/${data.id}/${page}`);
-            }}
-          />
-          <Timeline data={vault} onMarkerClick={markerClick} />
-          <InnerRing selected={selectedNav} />
-          <ForeignObject x="239.5" y="239.5" width="520" height="520">
-            <Content>{children}</Content>
-          </ForeignObject>
-          <Definitions />
-          {selectedEntry && (
-            <MarkerBubble
-              ref={popupRef}
-              months={vault.months}
-              entry={selectedEntry}
-              position={markerPosition}
-            />
-          )}
-        </svg>
-      </BoundingBox>
-    </>
+  return isDesktop ? (
+    <VaultDesktop
+      ref={vaultRef}
+      vault={vault}
+      selectedNav={selectedNav!}
+      markerClick={markerClick}
+      markerPosition={markerPosition}
+      selectedEntry={selectedEntry!}
+      children={children}
+    />
+  ) : (
+    <VaultMobile
+      // ref={vaultRef}
+      vault={vault}
+      selectedNav={selectedNav!}
+      markerClick={markerClick}
+      markerPosition={markerPosition}
+      selectedEntry={selectedEntry!}
+      children={children}
+    />
   );
 };
-
-// Need to 'trim' the corners on this square
-// otherwise the corners cover some of the ring buttons.
-// TODO Long Term: SVG should have a "circular hole" in it so
-// we can simply layer things correctly.
-const ForeignObject = styled.foreignObject`
-  border-radius: 50%;
-`;
-
-const BoundingBox = styled.div`
-  height: calc(100vh - ${NAV_DESKTOP_HEIGHT_PIXELS}px);
-  display: flex;
-  margin: 0 auto;
-  flex-direction: column;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-`;
-
-const Content = styled.div`
-  display: flex;
-  height: 100%;
-  border-radius: ${pixelsToRems(260)}rem;
-  overflow: hidden;
-  justify-content: center;
-  padding-top: ${pixelsToRems(10)}rem;
-`;
