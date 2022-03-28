fc656d36bfe1439c7554dbbf67fe73daf1086a5b
Alex <astanciu@gmail.com>
2022-03-28 15:31:25 -0400

    fix layouts

--- apps/dapp/src/components/Layouts/CoreLayout/index.tsx
+++ apps/dapp/src/components/Layouts/CoreLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import { tabletAndAbove } from 'styles/breakpoints';
+import { phoneAndAbove } from 'styles/breakpoints';
 import { theme } from 'styles/theme';
 
 import Header from './Header';
@@ -19,8 +19,7 @@ export default CoreLayout;
 const Main = styled.main`
   margin: 0 auto;
   padding: 0px;
-  ${tabletAndAbove(`
-    width: ${theme.metrics.desktop.maxWidth};
-    padding: 0 1.75rem;
+  ${phoneAndAbove(`
+    max-width: ${theme.metrics.desktop.maxWidth};
   `)}
 `;
