26091fa88e3fef7240bf9bf204c74f75014a085f
marshall <cameronmarshall2112@gmail.com>
2022-04-06 17:04:18 -0400

    Apply design feedback and finishing touches

--- apps/dapp/src/components/Vault/VaultStake.tsx
+++ apps/dapp/src/components/Vault/VaultStake.tsx
@@ -1,64 +1,138 @@
-import { copyBalance } from 'components/AMM/helpers/methods';
-import { Button } from 'components/Button/Button';
-import { Input } from 'components/Input/Input';
 import { Option } from 'components/InputSelect/InputSelect';
 import { VaultButton } from 'components/Vault/VaultContent';
 import { TICKER_SYMBOL } from 'enums/ticker-symbol';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { SmallDescription } from 'styles/Typography';
 import { formatNumber } from 'utils/formatter';
+import { Header } from 'styles/vault';
+import { theme } from 'styles/theme';
+import { VaultInput } from 'components/Input/VaultInput';
+import { CryptoSelect } from 'components/Input/CryptoSelect';
 
 /* TODO: Update for real data */
 const dummyOptions = [
-  { value: '$TEMPLE', label: '$TEMPLE' },
   { value: '$FRAX', label: '$FRAX' },
+  { value: '$TEMPLE', label: '$TEMPLE' },
+  { value: '$OGTEMPLE', label: '$OGTEMPLE' },
+  { value: '$FAITH', label: '$FAITH' },
   { value: '$ETH', label: '$ETH' },
+  { value: '$USDC', label: '$USDC' },
+  { value: '$FEI', label: '$FEI' },
 ];
+
+const defaultOption = { value: '$FRAX', label: '$FRAX' };
+
+const dummyWalletBalances = {
+  $FRAX: 4834,
+  $TEMPLE: 12834,
+  $OGTEMPLE: 41834,
+  $FAITH: 3954,
+  $ETH: 12,
+  $USDC: 402,
+  $FEI: 945,
+};
+
+const dummyCurrencyToTemple = {
+  $FRAX: 423,
+  $TEMPLE: 343334,
+  $OGTEMPLE: 502933,
+  $FAITH: 554,
+  $ETH: 14454,
+  $USDC: 49233,
+  $FEI: 9293,
+};
+
 const VaultClaim = () => {
+  const [stakingAmount, setStakingAmount] = useState<number | ''>('');
   const [templeAmount, setTempleAmount] = useState<number | ''>('');
-  const [templeWalletAmount, setTempleWalletAmount] = useState<number>(1234.12);
   const [ticker, setTicker] = useState(dummyOptions[0].value);
+
+  const [walletCurrencyBalance, setWalletCurrencyBalance] = useState<number>(0);
 
   const handleTickerUpdate = (val: Option) => {
     setTicker(val.value as string);
+    setWalletCurrencyBalance(
+      dummyWalletBalances[
+        val.value as keyof typeof dummyWalletBalances
+      ] as number
+    );
+    setStakingAmount('');
+    setTempleAmount('');
   };
 
-  const handleUpdateTempleAmount = async (value: number) => {
-    setTempleAmount(value === 0 ? '' : value);
+  const handleUpdateStakingAmount = async (value: number) => {
+    setStakingAmount(value === 0 ? '' : value);
+    conditionallySetTempleAmount();
   };
+
+  const handleHintClick = () => {
+    handleUpdateStakingAmount(walletCurrencyBalance);
+  };
+
+  const conditionallySetTempleAmount = () => {
+    if (ticker === '$TEMPLE') {
+      setTempleAmount('');
+      return;
+    }
+    setTempleAmount(
+      dummyCurrencyToTemple[ticker as keyof typeof dummyCurrencyToTemple]
+    );
+  };
+
+  useEffect(() => {
+    handleTickerUpdate(defaultOption);
+  }, []);
 
   return (
     <>
-      <h2 className={'margin-remove'}>Enter Vault</h2>
-      <br />
-      <small className={'color-brandLight'}>
-        DEPOSIT {TICKER_SYMBOL.TEMPLE_TOKEN} TO ENTER VAULT{' '}
-      </small>
-      <br />
-      <br />
-      <Input
-        crypto={{
-          kind: 'select',
-          cryptoOptions: dummyOptions,
-          onCryptoChange: handleTickerUpdate,
-          defaultValue: dummyOptions.find((option) => option.value === ticker),
-        }}
-        value={templeAmount}
-        hint={`Balance: ${formatNumber(templeWalletAmount)}`}
-        handleChange={handleUpdateTempleAmount}
-        onHintClick={() => copyBalance(templeWalletAmount, setTempleAmount)}
+      <Header>Stake</Header>
+      <DepositContainer>
+        DEPOSIT{' '}
+        <SelectContainer>
+          <CryptoSelect
+            options={dummyOptions}
+            defaultValue={defaultOption}
+            onChange={handleTickerUpdate}
+          />
+        </SelectContainer>
+      </DepositContainer>
+      <VaultInput
+        tickerSymbol={ticker}
+        handleChange={handleUpdateStakingAmount}
+        hint={`Balance: ${formatNumber(walletCurrencyBalance)}`}
+        onHintClick={handleHintClick}
+        isNumber
+        placeholder={'0.00'}
+        value={stakingAmount}
       />
-      <InputDescription>1.1x Multiplier on Temple Staked</InputDescription>
-      <br />
-      <br />
+      <AmountInTemple>
+        {templeAmount !== ''
+          ? `Staking ${templeAmount} ${TICKER_SYMBOL.TEMPLE_TOKEN}`
+          : ''}{' '}
+        {'\u00A0'}
+      </AmountInTemple>
       <VaultButton label={'stake'} autoWidth />
     </>
   );
 };
 
-const InputDescription = styled(SmallDescription)`
-  align-self: flex-end;
+const SelectContainer = styled.div`
+  margin: 0 auto;
+  width: 50%;
+  padding: 1rem;
+  display: inline-block;
+  z-index: 4;
+`;
+
+const AmountInTemple = styled.p`
+  color: ${theme.palette.brandLight};
+`;
+
+const DepositContainer = styled.div`
+  color: ${theme.palette.brandLight};
+  font-size: 1.5rem;
+  padding: 1.4rem 0 1.8rem;
+  display: inline-block;
 `;
 
 export default VaultClaim;
