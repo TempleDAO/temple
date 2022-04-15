d037b18586202b6be728bffe8f15fdd5fd6e65b1
jebsley3 <cuiett@protonmail.com>
2022-04-15 13:49:22 +0100

    wip: alecks suggestions - add vault title - add proper title to strategy page

--- apps/dapp/src/components/Pages/Core/VaultPages/Strategy.tsx
+++ apps/dapp/src/components/Pages/Core/VaultPages/Strategy.tsx
@@ -1,3 +1,13 @@
-export const Strategy = () => <div>
-  <h3>Strategy</h3>
-</div>
+import React from 'react';
+
+import VaultContent from 'components/Vault/VaultContent';
+
+import { Header } from 'styles/vault';
+
+export const Strategy = () => {
+  return (
+    <VaultContent>
+      <Header>Strategy</Header>
+    </VaultContent>
+  );
+};
