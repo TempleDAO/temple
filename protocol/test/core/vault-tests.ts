1fa926464a6330cf3b54e067b01e99327cde033d
butlerji <92957262+butlerji@users.noreply.github.com>
2022-04-22 16:50:54 +1000

    Feature/vault erc20 tests (#223)
    * Test ERC20 interface explicitly
    
    Given our implementation is a fair depature from the out of the box
    openzepplin ERC20 implementations, felt it prudent to bake explicit
    tests around the ERC20 behaviour
    
    * Fixed bug in test setup where accounts wasn't reset on each run
    
    Co-authored-by: butlerji <>

--- protocol/test/core/vault-tests.ts
+++ protocol/test/core/vault-tests.ts
@@ -1,13 +1,13 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { deployAndAirdropTemple, fromAtto, mineForwardSeconds, mineToTimestamp, toAtto } from "../helpers";
-import { Signer } from "ethers";
+import { deployAndAirdropTemple, fromAtto, mineForwardSeconds, toAtto } from "../helpers";
+import { BigNumber, Signer } from "ethers";
 import { 
-    JoiningFee,
-    JoiningFee__factory,
-    TempleERC20Token, 
-    Vault, 
-    Vault__factory
+  JoiningFee,
+  JoiningFee__factory,
+  TempleERC20Token, 
+  Vault, 
+  Vault__factory
 } from "../../typechain";
 import { fail } from "assert";
 
@@ -107,5 +107,121 @@ describe("Temple Core Vault", async () => {
   xit("allows redeeming an exposure back to temple", async () => {
     // TODO(butler): write test
     fail("Unimplemented");
+  });
+
+  describe("ERC20 verification", async () => {
+    let accounts: [Signer, BigNumber][] = [];
+
+    beforeEach(async () => {
+      await vault.connect(alan).deposit(toAtto(300));
+      await vault.connect(ben).deposit(toAtto(300));
+      accounts = [[alan, toAtto(300)], [ben,  toAtto(300)]]
+    })
+
+    it("Has expected initial balances", async () => {
+      for (const [user, initialBalance] of accounts) {
+        expect(await vault.balanceOf(await user.getAddress())).eql(initialBalance)
+      }
+    })
+
+    it("Transfer changes balances", async () => {
+      const [[alan, _0], [ben, _1]] = accounts;
+
+      await expect(async () => vault.connect(alan).transfer(await ben.getAddress(), 111))
+        .to.changeTokenBalances(vault, [alan, ben], [-111, 111])
+    })
+
+    it("Transfer works across rebases balances", async () => {
+      const [[alan, _0], [ben, _1]] = accounts;
+
+      await expect(async () => vault.connect(alan).transfer(await ben.getAddress(), toAtto(111)))
+        .to.changeTokenBalances(vault, [alan, ben], [`-${toAtto(111).toString()}`, toAtto(111)])
+
+      await templeToken.transfer(vault.address, toAtto(600));
+
+      await expect(async () => vault.connect(alan).transfer(await ben.getAddress(), toAtto(111)))
+        .to.changeTokenBalances(vault, [alan, ben], [`-${toAtto(111).toString()}`, toAtto(111)])
+    })
+
+    it("Transfer emits event", async () => {
+      const [[alan, _0], [ben, _1]] = accounts;
+
+      await expect(vault.connect(alan).transfer(await ben.getAddress(), toAtto(100)))
+        .to.emit(vault, "Transfer")
+        .withArgs(await alan.getAddress(), await ben.getAddress(), toAtto(100))
+    })
+
+    it("Can not transfer above the amount", async () => {
+      const [[alan, alanBalance], [ben, _]] = accounts;
+
+      await expect(vault.connect(alan).transfer(await ben.getAddress(), alanBalance.add(toAtto(100))))
+        .to.be.revertedWith("ERC20: transfer amount exceeds balance")
+    })
+
+    it("totalSupply", async () => {
+      let expectedTotalSupply = BigNumber.from(0);
+
+      for (const [user, initialBalance] of accounts) {
+        expectedTotalSupply = expectedTotalSupply.add(initialBalance);
+      }
+
+      expect(await vault.totalSupply()).eql(expectedTotalSupply);
+    })
+
+    it("initial balances + totalSupply rebases correctly", async () => {
+      await templeToken.transfer(vault.address, toAtto(600));
+
+      let expectedTotalSupply = BigNumber.from(0);
+      for (const [user, initialBalance] of accounts) {
+        expect(await vault.balanceOf(await user.getAddress())).eql(initialBalance.mul(2))
+        expectedTotalSupply = expectedTotalSupply.add(initialBalance);
+      }
+
+      expect(await vault.totalSupply()).eql(expectedTotalSupply.mul(2));
+    })
+
+    it("allowance", async () => {
+      const [[alan, _1], [ben, _2]] = accounts;
+
+      expect(await vault.allowance(await alan.getAddress(), await ben.getAddress())).eq(0);
+      await vault.connect(alan).approve(await ben.getAddress(), 111);
+      expect(await vault.allowance(await alan.getAddress(), await ben.getAddress())).eq(111);
+    })
+
+    it("allowance remains unchanged during rebases", async () => {
+      const [[alan, _1], [ben, _2]] = accounts;
+
+      await vault.connect(alan).approve(await ben.getAddress(), toAtto(111));
+      expect(fromAtto(await vault.allowance(await alan.getAddress(), await ben.getAddress()))).eq(111);
+
+      await templeToken.transfer(vault.address, toAtto(600));
+      expect(fromAtto(await vault.allowance(await alan.getAddress(), await ben.getAddress()))).eq(111);
+    })
+
+    it("transferFrom", async () => {
+      const [[alan, _1], [ben, _2]] = accounts;
+
+      await expect(vault.transferFrom(await alan.getAddress(), await ben.getAddress(), 111))
+        .to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+
+      await vault.connect(alan).approve(await ben.getAddress(), 111);
+
+      await expect(async () => vault.connect(ben).transferFrom(await alan.getAddress(), await ben.getAddress(), 111))
+        .to.changeTokenBalances(vault, [alan, ben], [-111, 111]);
+    })
+
+    it("transferFrom works across rebases", async () => {
+      const [[alan, _1], [ben, _2]] = accounts;
+
+      await vault.connect(alan).approve(await ben.getAddress(), toAtto(1000));
+
+      await expect(async () => vault.connect(ben).transferFrom(await alan.getAddress(), await ben.getAddress(), toAtto(111)))
+        .to.changeTokenBalances(vault, [alan, ben], [`-${toAtto(111).toString()}`, toAtto(111)])
+
+      await templeToken.transfer(vault.address, toAtto(600));
+
+      await expect(async () => vault.connect(ben).transferFrom(await alan.getAddress(), await ben.getAddress(), toAtto(111)))
+        .to.changeTokenBalances(vault, [alan, ben], [`-${toAtto(111).toString()}`, toAtto(111)])
+    })
   });
 })
