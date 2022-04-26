1f154ec599a52658d768500bbc1d8661e5f5c373
Marshall <99344331+marshall2112@users.noreply.github.com>
2022-04-26 13:49:20 -0400

    Subgraph updates (#253)
    * Changes from standalone repo
    
    * Update startBlock

---
+++ apps/core-subgraph/src/mappings/vault.ts
@@ -0,0 +1,14 @@
+import { log } from '@graphprotocol/graph-ts'
+import { Deposit, Withdraw } from '../../generated/OpsManager/Vault'
+
+import { createDeposit } from '../entities/deposit'
+import { createWithdraw } from '../entities/withdraw'
+
+
+export function onDeposit(event: Deposit): void {
+    createDeposit(event)
+}
+
+export function onWithdraw(event: Withdraw): void {
+    createWithdraw(event)
+}
--- apps/core-subgraph/src/utils/constants.ts
+++ apps/core-subgraph/src/utils/constants.ts
@@ -10,13 +10,5 @@ export const BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18');
 export const BIG_DECIMAL_1E7 = BigDecimal.fromString('1e7');
 export const BIG_DECIMAL_0 = BigDecimal.fromString('0');
 
-export const GAUGE_CONTROLLER = '0x8EfeB2ef8f4dF4892D7115160eFDcD5F93Eb3F26';
-export const GAUGE_CONTROLLER_ADDRESS = Address.fromString('0x8EfeB2ef8f4dF4892D7115160eFDcD5F93Eb3F26');
-export const VOTING_ESCROW = Address.fromString('0x617d46FcECA1C8290d191709F93118e593a459d3');
-export const DUAL_REWARDS_ADDRESS = Address.fromString('0x17979fe6643f056d99717EcB40cdc2FFb5830576');
-export const SMART_WALLET_WL_ADDRESS = Address.fromString('0x409a9Ba77FfE1a7701E0A08F32c38a2b9707C7D7');
-
-export const STAX_ERC20_CONTRACT = Address.fromString('0x1F747beA017eC1B023BebA463BA5d31E10d3286f');
-export const FXS_ERC20_CONTRACT = Address.fromString('0x9cB761d3f220262F03120098F0a9170f83a00C0e');
-export const FXS_STAX_LP = Address.fromString('0x757405819fA116982F03DAedD65c779728e349c8');
-export const FXS_PRICE_FEED = Address.fromString('0x6c0fe985d3cacbcde428b84fc9431792694d0f51');
+export const OPS_MANAGER_LOCAL = '0x2e2ed0cfd3ad2f1d34481277b3204d807ca2f8c2';
+export const TEMPLE_LOCAL_ADDRESS = Address.fromString('0x5fbdb2315678afecb367f032d93f642f64180aa3');
