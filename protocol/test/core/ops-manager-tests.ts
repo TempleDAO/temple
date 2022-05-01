4e618f79732060e217e2bef08526a819633ef955
butlerji <>
2022-05-01 19:59:28 +1000

    Breadcrumbs on what needs to be tested

--- protocol/test/core/ops-manager-tests.ts
+++ protocol/test/core/ops-manager-tests.ts
@@ -49,6 +49,8 @@ describe("Temple Core Ops Manager", async () => {
   it("Create a new exposure", async () => {
     await opsManager.createExposure("Temple FXS Exposure", "TE-FXS", fxsToken.address);
     await opsManager.createExposure("Temple CRV Exposure", "TE-CRV", crvToken.address);
+
+    // TODO... Check it created exposures
   })
 
   it("Create new vaults", async () => {
@@ -71,6 +73,58 @@ describe("Temple Core Ops Manager", async () => {
       {p: 1, q: 1},
       firstVaultstartTime + 3600,
     )
+
+    // TODO... Check it created vaults
+  })
+
+  it("End to end flow", async () => {
+    const firstVaultstartTime = await blockTimestamp()
+
+    // create some vaults
+    await opsManager.createVaultInstance(
+      "temple-1d-vault",
+      "TV-1D-1",
+      86400,
+      3600,
+      {p: 1, q: 1},
+      firstVaultstartTime,
+    )
+
+    await opsManager.createVaultInstance(
+      "temple-1d-vault",
+      "TV-1D-1",
+      86400,
+      3600,
+      {p: 1, q: 1},
+      firstVaultstartTime + 3600,
+    )
+
+    // create two exposures as a result of our farming activities
+    await opsManager.createExposure("Temple FXS Exposure", "TE-FXS", fxsToken.address);
+    await opsManager.createExposure("Temple CRV Exposure", "TE-CRV", crvToken.address);
+
+    // Deposit temple into the current open vault
+    // TODO...
+
+    // Fast forward to close vault, and rebalance open vault
+    // with revenue share. Before callign rebalance, check
+    // which vaults require a rebalance
+    // TODO implement + check shares are as we expect
+
+    // Add revenue to each each exposure
+    // TODO implement + check unclaimed revenue is as expected
+
+    // Claim a vaults share of primary revenue, via the
+    // rebalance method
+    // TODO implement + check unclaimed revenue is as expected
+
+    // Claim a vaults share of primary revenue, via the
+    // rebalance method
+    // TODO implement + check unclaimed revenue is as expected
+
+    // liquidate a vaults exposure
+    // TODO implement - for now just confirm the necessary
+    // events fire so we can do this process manually
   })
 })
 
