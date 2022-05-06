37a0e243e1d6154c9d0f2f9614ed84c375c3d8a2
jebsley3 <cuiett@protonmail.com>
2022-05-06 20:19:00 +0100

    use set for buy and sell tokens

--- apps/dapp/src/components/Pages/Core/Trade/constants.ts
+++ apps/dapp/src/components/Pages/Core/Trade/constants.ts
@@ -2,7 +2,10 @@ import { TICKER_SYMBOL } from 'enums/ticker-symbol';
 
 const { FRAX, FEI, ETH, USDC } = TICKER_SYMBOL;
 
+const buyTokens = new Set([FRAX, FEI, ETH, USDC]);
+const sellTokens = new Set([FRAX, FEI]);
+
 export const TOKENS_BY_MODE = {
-  BUY: [FRAX, FEI, ETH, USDC],
-  SELL: [FRAX, FEI],
+  BUY: [...buyTokens],
+  SELL: [...sellTokens],
 };
