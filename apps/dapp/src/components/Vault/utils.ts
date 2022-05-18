14a39c1b82539b50ed70e0a2932907822005e234
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-05-18 15:39:38 -0700

    More profile page stuff (#305)
    * cleanup profile page some more
    
    * wip
    
    * Cleanup
    
    * cleanup
    
    * cleanup
    
    * cleanup

--- apps/dapp/src/components/Vault/utils.ts
+++ apps/dapp/src/components/Vault/utils.ts
@@ -1,12 +1,12 @@
 import { differenceInSeconds, addSeconds, subSeconds, format } from 'date-fns';
 
 import { GraphVault, GraphVaultGroup } from 'hooks/core/types';
-import { Vault, VaultGroup, MarkerType, Marker } from '../types';
+import { Vault, VaultGroup, MarkerType, Marker } from './types';
 import { VaultGroupBalances } from 'hooks/core/use-vault-group-token-balance';
 
 export const SECONDS_IN_MONTH = 60 * 60 * 24 * 30;
 
-const createDateFromSeconds = (dateInSeconds: string | number) => {
+export const createDateFromSeconds = (dateInSeconds: string | number) => {
   const dateMs = Number(dateInSeconds) * 1000;
   return new Date(dateMs);
 };
