35f994dd519aabacdc6e05dacd0235ddf031eb0d
marshall <cameronmarshall2112@gmail.com>
2022-05-31 19:11:54 -0400

    Fix issue with rebase

--- apps/dapp/vite.config.ts
+++ apps/dapp/vite.config.ts
@@ -5,26 +5,14 @@ import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { dependencies } from './package.json';
 
-const VENDOR_CHUNKS = new Set([
-  'react',
-  'react-router-dom',
-  'react-dom',
-  'styled-components',
-  'polished',
-]);
+const VENDOR_CHUNKS = new Set(['react', 'react-router-dom', 'react-dom', 'styled-components', 'polished']);
 
-const VISUALIZATION_CHUNKS = new Set([
-  'd3-shape',
-  'react-vis',
-]);
+const VISUALIZATION_CHUNKS = new Set(['d3-shape', 'react-vis']);
 
 const renderChunks = (deps: Record<string, string>) => {
   const chunks: Record<string, string[]> = {};
   Object.keys(deps).forEach((dep) => {
-    if (
-      VENDOR_CHUNKS.has(dep) ||
-      VISUALIZATION_CHUNKS.has(dep)
-    ) {
+    if (VENDOR_CHUNKS.has(dep) || VISUALIZATION_CHUNKS.has(dep)) {
       return;
     }
     chunks[dep] = [dep];
@@ -32,10 +20,7 @@ const renderChunks = (deps: Record<string, string>) => {
   return chunks;
 };
 
-const plugins = [
-  react(),
-  legacy(),
-];
+const plugins = [react(), legacy()];
 
 if (process.env.BUILD_STATS) {
   plugins.push(visualizer());
@@ -61,7 +46,7 @@ export default defineConfig({
   },
   define: {
     // WalletLink requires process to be a global.
-    'process.env': {}
+    'process.env': {},
   },
   resolve: {
     alias: {
@@ -79,7 +64,6 @@ export default defineConfig({
       constants: path.resolve(__dirname, 'src/constants'),
       data: path.resolve(__dirname, 'src/data'),
       types: path.resolve(__dirname, 'src/types'),
-      constants: path.resolve(__dirname, 'src/constants'),
     },
   },
   envPrefix: 'VITE',
