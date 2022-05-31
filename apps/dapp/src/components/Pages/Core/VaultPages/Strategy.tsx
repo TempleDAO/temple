92de499b0638c6bd33b644ad86966d5c443e3943
Alex <astanciu@gmail.com>
2022-05-31 15:51:59 -0400

    set strategy text

--- apps/dapp/src/components/Pages/Core/VaultPages/Strategy.tsx
+++ apps/dapp/src/components/Pages/Core/VaultPages/Strategy.tsx
@@ -4,27 +4,18 @@ import VaultContent from 'components/Pages/Core/VaultPages/VaultContent';
 
 import { Header } from 'styles/vault';
 import styled from 'styled-components';
+import { useVaultContext } from '../VaultContext';
+import { DefaultText, strategies } from './strategies/Strategies';
 
 export const Strategy = () => {
+  const { vaultGroup } = useVaultContext();
+  const id = vaultGroup.id;
+  const strategyContent = strategies[id] || DefaultText;
+
   return (
     <VaultContent>
       <Header>Strategy</Header>
-      <StrategyContainer>
-        <P>This is a fake vault used just for testing </P>
-      </StrategyContainer>
+      {strategyContent}
     </VaultContent>
   );
 };
-
-const StrategyContainer = styled.div`
-  color: #ffdec9;
-  font-size: 1.5rem;
-  margin: 30px 20px 0px 20px;
-  height: 300px;
-  text-align: left;
-  padding: 10px;
-`;
-
-const P = styled.div`
-  font-size: inherit;
-`
