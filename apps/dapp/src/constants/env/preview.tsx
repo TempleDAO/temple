3ff17da5680699b1e184625326f91c9d323336f7
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-05-31 10:24:44 -0700

    add sentry (#339)

--- apps/dapp/src/constants/env/preview.tsx
+++ apps/dapp/src/constants/env/preview.tsx
@@ -7,6 +7,10 @@ const env: Environment = {
   contracts: {
     vaultProxy: '0x348757a999881B2EEAd0239650cda9CD5Ca704B2',
   },
+  sentry: {
+    environment: 'preview',
+    dsn: 'https://ab0c41c30fbb405da50d9803819b1d60@o1268430.ingest.sentry.io/6456054',
+  },
 };
 
 export default env;
