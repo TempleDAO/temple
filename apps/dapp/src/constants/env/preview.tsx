d0965007b44fa4c7a618079e39d08e87603aa023
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-06-01 14:53:03 -0700

    Fix gas limits (#350)
    * remove unused var
    
    * Set gas limit
    
    * Adjust down
    
    * set to 450k

--- apps/dapp/src/constants/env/preview.tsx
+++ apps/dapp/src/constants/env/preview.tsx
@@ -4,9 +4,7 @@ const env: Environment = {
   subgraph: {
     templeCore: 'https://api.thegraph.com/subgraphs/name/templedao/templedao-core',
   },
-  contracts: {
-    vaultProxy: '0x348757a999881B2EEAd0239650cda9CD5Ca704B2',
-  },
+  contracts: {},
   sentry: {
     environment: 'preview',
     dsn: 'https://ab0c41c30fbb405da50d9803819b1d60@o1268430.ingest.sentry.io/6456054',
