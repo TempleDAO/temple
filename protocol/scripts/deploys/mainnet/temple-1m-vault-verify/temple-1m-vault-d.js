751192af099d32956edeef612f8cdfd64802aadc
scooprinder <92979684+scooprinder@users.noreply.github.com>
2022-06-04 09:40:51 +0100

    Add mainnet deploy scripts (#356)
    * Add mainnet deploy scripts
    
    * Code review changes

---
+++ protocol/scripts/deploys/mainnet/temple-1m-vault-verify/temple-1m-vault-d.js
@@ -0,0 +1,10 @@
+module.exports = [
+    'temple-1m-vault',
+    'temple-1m-vault-d',
+    '0x470ebf5f030ed85fc1ed4c2d36b9dd02e77cf1b7', // temple erc20 mainnet
+    60*60*24*28,
+    60*60*24*7,
+    {p:1,q:1},
+    '<JOINING-FEE-ADDR>',
+    1654048170 // TODO: replace with starting timestamp for vault
+]
