6ead287edb9a79050044879d965d50bad9f88a0f
jebsley3 <96315255+jebsley3@users.noreply.github.com>
2022-06-07 22:54:03 +0100

    Core: use subgraph for analytics data (#383)
    * wip
    
    * get analytics from subgraph
    
    * cleanup
    
    * query formatting

--- apps/dapp/src/constants/env/types.ts
+++ apps/dapp/src/constants/env/types.ts
@@ -1,12 +1,13 @@
 interface Subgraphs {
   templeCore: string;
+  protocolMetrics: string;
 }
 
 interface Contracts {}
 
 interface Sentry {
   environment: string;
-  dsn: string; 
+  dsn: string;
 }
 
 export interface Environment {
