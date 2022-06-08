ef984e95eac117bc6062b290661e2523b42482db
fujisawa <mrfujisawa@protonmail.com>
2022-06-08 13:32:25 -0700

    simplify client code

--- apps/dapp/src/hooks/core/use-vault-balance.tsx
+++ apps/dapp/src/hooks/core/use-vault-balance.tsx
@@ -1,14 +1,12 @@
 import { BigNumber } from 'ethers';
 import { useVaultContext } from 'components/Pages/Core/VaultContext';
 import { ZERO } from 'utils/bigNumber';
-import { asyncNoopBool } from 'utils/helpers';
 
 type HookResponseType = [
   {
     isLoading: boolean;
     balance: BigNumber;
     staked: BigNumber;
-    canExit: Function;
   },
   () => Promise<void>
 ];
@@ -20,16 +18,15 @@ const DEFAULT_STATE = {
 };
 
 export const useVaultBalance = (vaultContractAddress: string): HookResponseType => {
-  const { balances, refreshVaultBalance, canExit: _canExit } = useVaultContext();
+  const { balances, refreshVaultBalance } = useVaultContext();
   const fetchBalance = () => refreshVaultBalance(vaultContractAddress);
   const vaultBalance = balances[vaultContractAddress] || DEFAULT_STATE;
-  const canExit = () => _canExit(vaultContractAddress);
+
   return [
     {
       isLoading: vaultBalance.isLoading,
       balance: vaultBalance.balance || DEFAULT_STATE.balance,
       staked: vaultBalance.staked || DEFAULT_STATE.staked,
-      canExit,
     },
     fetchBalance,
   ];
