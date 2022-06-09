ea2ae2550c4fcdec8377d64e8be2c8bc9798e923
fujisawa <mrfujisawa@protonmail.com>
2022-06-09 16:54:46 -0700

    use bigNumber hack

--- apps/dapp/src/utils/bigNumber.ts
+++ apps/dapp/src/utils/bigNumber.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from 'ethers';
-import { formatEther, parseEther } from 'ethers/lib/utils';
+import { formatEther, parseEther, parseUnits } from 'ethers/lib/utils';
 
 export const atto = 1e-18;
 
@@ -8,7 +8,16 @@ export const fromAtto = (bn: BigNumber): number => {
 };
 
 export const toAtto = (n: number): BigNumber => {
-  return parseEther(n.toString());
+  const numberString = n.toString();
+  if (numberString.includes('e-')) {
+    const [a, _] = numberString.split('e-');
+    const num = a.split('.').join('');
+    const big = parseUnits(num, 'wei');
+
+    return big;
+  }
+
+  return parseEther(numberString);
 };
 
 export const ZERO = Object.freeze(BigNumber.from(0));
