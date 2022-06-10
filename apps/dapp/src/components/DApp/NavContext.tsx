165f17025af2fd6cd565509b2895fbd6b111e1d0
fujisawa <mrfujisawa@protonmail.com>
2022-06-10 13:17:58 -0700

    delete old dapp

--- apps/dapp/src/components/DApp/NavContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { DAppView } from 'enums/dapp-view';
-
-export const NavContext = React.createContext({
-  activeView: DAppView.BUY,
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function,  @typescript-eslint/no-unused-vars
-  setView: (view: DAppView) => {},
-});
