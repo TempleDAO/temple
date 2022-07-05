a3658e3150a67702cee46f3af20e1803682e87cb
fujisawa <mrfujisawa@protonmail.com>
2022-07-05 13:22:35 -0700

    fix

--- apps/dapp/src/components/Pages/Core/Profile/Profile.tsx
+++ apps/dapp/src/components/Pages/Core/Profile/Profile.tsx
@@ -308,7 +308,7 @@ const ProfileMeta = styled.div`
 const StatCards = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
   gap: 0.75rem;
 `;
 
