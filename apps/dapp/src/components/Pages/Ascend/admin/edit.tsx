6257f3cf21f636345d2b765716fbc201b9ebbbdb
marshall <cameronmarshall2112@gmail.com>
2022-07-19 16:29:21 -0400

    Edit page WIP

--- apps/dapp/src/components/Pages/Ascend/admin/edit.tsx
+++ apps/dapp/src/components/Pages/Ascend/admin/edit.tsx
@@ -1,12 +1,29 @@
+import { SubgraphPool } from 'components/Layouts/Ascend/types';
+import { createPool } from 'components/Layouts/Ascend/utils';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { LBPForm } from '../components/LBPForm';
+import { useTemplePool } from '../hooks';
 
 export const EditLBPPage = () => {
-  // TODO: Load auction? pool? identifier from path and then load details from the subgraph
-  const { auctionId } = useParams();
-  console.log(auctionId);
+  const { poolAddress } = useParams();
 
-  return (
-    <LBPForm />
-  );
+  if (!poolAddress) {
+    return <LBPForm />;
+  }
+
+  const [request, { response, error }] = useTemplePool(poolAddress);
+
+  useEffect(() => {
+    request();
+  }, [request]);
+
+  if (error || !response?.data?.pools[0]) {
+    return <h3>Error loading pool...</h3>;
+  }
+
+  const subgraphPool:SubgraphPool = response?.data?.pools[0];
+  const pool = createPool(subgraphPool);
+
+  return <LBPForm pool={pool}/>;
 };
