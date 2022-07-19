589c8edc4264c8e02884b7690a85f749b083bd36
marshall <cameronmarshall2112@gmail.com>
2022-07-19 09:24:44 -0400

    Use new form components

--- apps/dapp/src/components/Pages/Ascend/PoolCreate.tsx
+++
@@ -1,6 +0,0 @@
-const PoolCreatePage = () => {
-    return <p>Placeholder for Pool Create</p>;
-  };
-  
-  export default PoolCreatePage;
-  
--- apps/dapp/src/components/Pages/Ascend/admin/edit.tsx
+++ apps/dapp/src/components/Pages/Ascend/admin/edit.tsx
@@ -1,6 +1,11 @@
+import { useParams } from 'react-router-dom';
 import { LBPForm } from '../components/LBPForm';
 
 export const EditLBPPage = () => {
+  // TODO: Load auction? pool? identifier from path and then load details from the subgraph
+  const { auctionId } = useParams();
+  console.log(auctionId);
+
   return (
     <LBPForm />
   );
