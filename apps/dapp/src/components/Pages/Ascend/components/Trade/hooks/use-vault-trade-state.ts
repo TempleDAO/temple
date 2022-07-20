ddeb11fc08d8552a6853dee4853270681839b334
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-07-20 16:35:44 -0700

    Trade states (#456)
    * cleanup
    
    * Add some button states
    
    * Add circular loader
    
    * wip
    
    * fix limits, deadline, error message, loading states/styling
    
    * separate hooks

---
+++ apps/dapp/src/components/Pages/Ascend/components/Trade/hooks/use-vault-trade-state.ts
@@ -0,0 +1,285 @@
+import { useReducer } from 'react';
+import { BigNumber } from 'ethers';
+import { useBalance } from 'wagmi';
+
+import { Pool } from 'components/Layouts/Ascend/types';
+import { useWallet } from 'providers/WalletProvider';
+import { ZERO } from 'utils/bigNumber';
+import { getBigNumberFromString } from 'components/Vault/utils';
+import { Nullable } from 'types/util';
+import { getSwapLimit, getSwapDeadline } from '../utils';
+
+import { useVaultContract } from './use-vault-contract';
+
+type Action<A extends ActionType, P extends any> = { type: A, payload: P };
+
+enum ActionType {
+  TogglePair,
+  SetSellValue,
+  SetSwapQuote,
+  SetSwapQuoteLoading,
+  SetTransactionSettings,
+  ResetQuoteState,
+  UpdateSwapState,
+};
+
+type Actions = 
+  Action<ActionType.TogglePair, null> |
+  Action<ActionType.SetSellValue, string> |
+  Action<ActionType.SetSwapQuote, Nullable<BigNumber>> |
+  Action<ActionType.SetSwapQuoteLoading, boolean> |
+  Action<ActionType.SetTransactionSettings, TradeState['transactionSettings']> |
+  Action<ActionType.ResetQuoteState, null> |
+  Action<ActionType.UpdateSwapState, { isLoading: boolean; error: string }>;
+
+interface TradeState {
+  sell: {
+    name: string;
+    weight: BigNumber;
+    address: string;
+    symbol: string;
+    // Input value
+    value: string;
+  };
+  buy: {
+    name: string;
+    weight: BigNumber;
+    address: string;
+    symbol: string;
+  };
+  quote: {
+    loading: boolean;
+    estimate: Nullable<BigNumber>;
+    estimateWithSlippage: Nullable<BigNumber>;
+  };
+  swap: {
+    error: Nullable<string>;
+    isLoading: boolean;
+  };
+  transactionSettings: {
+    slippageTolerance: number;
+    deadlineMinutes: number;
+  };
+}
+
+const reducer = (state: TradeState, action: Actions): TradeState => {
+  switch (action.type) {
+    case ActionType.SetSellValue: {
+      return {
+        ...state,
+        sell: {
+          ...state.sell,
+          value: action.payload,
+        },
+      };
+    }
+    case ActionType.ResetQuoteState: {
+      return {
+        ...state,
+        sell: {
+          ...state.sell,
+          value: '',
+        },
+        quote: {
+          loading: false,
+          estimate: null,
+          estimateWithSlippage: null,
+        },
+      };
+    }
+    case ActionType.TogglePair: {
+      return {
+        ...state,
+        sell: {
+          ...state.buy,
+          value: '',
+        },
+        buy: {
+          ...state.sell,
+        },
+        quote: {
+          loading: false,
+          estimate: null,
+          estimateWithSlippage: null,
+        },
+      };
+    }
+    case ActionType.SetSwapQuote: {
+      return {
+        ...state,
+        quote: {
+          ...state.quote,
+          estimate: action.payload,
+          estimateWithSlippage: getSwapLimit(action.payload, state.transactionSettings.slippageTolerance),
+        },
+      };
+    }
+    case ActionType.SetSwapQuoteLoading: {
+      return {
+        ...state,
+        quote: {
+          ...state.quote,
+          loading: action.payload,
+        },
+      };
+    }
+    case ActionType.SetTransactionSettings: {
+      return {
+        ...state,
+        quote: {
+          ...state.quote,
+          estimateWithSlippage: getSwapLimit(state.quote.estimateWithSlippage, action.payload.slippageTolerance)
+        },
+        transactionSettings: {
+          ...action.payload,
+        },
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export const useVaultTradeState = (pool: Pool) => {
+  const { wallet } = useWallet();
+  const vaultContract = useVaultContract(pool);
+
+  const [main, base] = pool.tokens;
+  const [state, dispatch] = useReducer(reducer, {
+    sell: {
+      name: base.name,
+      weight: base.weight,
+      address: base.address,
+      symbol: base.symbol,
+      value: '',
+    },
+    buy: {
+      name: main.name,
+      weight: main.weight,
+      address: main.address,
+      symbol: main.symbol,
+    },
+    quote: {
+      loading: false,
+      estimate: null,
+      estimateWithSlippage: null,
+    },
+    swap: {
+      error: '',
+      isLoading: false,
+    },
+    transactionSettings: {
+      slippageTolerance: 1,
+      deadlineMinutes: 5,
+    },
+  });
+
+  const _sellTokenBalance = useBalance({
+    addressOrName: (wallet || '').toLowerCase(),
+    token: state.sell.address.toLowerCase(),
+    enabled: !!wallet,
+    watch: true,
+  });
+
+  const _buyTokenBalance = useBalance({
+    addressOrName: (wallet || '').toLowerCase(),
+    token: state.buy.address.toLowerCase(),
+    enabled: !!wallet,
+    watch: true,
+  });
+
+  const sellTokenBalance =
+    (_sellTokenBalance.isLoading || _sellTokenBalance.isError || !_sellTokenBalance.data) ?
+    ZERO :
+    _sellTokenBalance.data.value;
+  
+  const buyTokenBalance = 
+    (_buyTokenBalance.isLoading || _buyTokenBalance.isError || !_buyTokenBalance.data) ?
+    ZERO :
+    _buyTokenBalance.data.value;
+
+  const updateSwapQuote = async (amount: BigNumber) => {
+    if (amount.eq(ZERO)) {
+      dispatch({
+        type: ActionType.SetSwapQuote,
+        payload: null,
+      });
+
+      return;
+    }
+
+    dispatch({ type: ActionType.SetSwapQuoteLoading, payload: true });
+
+    try {
+      const quotes = await vaultContract.getSwapQuote(amount, state.sell.address, state.buy.address);
+      const indexOfBuy = pool.tokensList.findIndex((address) => address === state.buy.address);
+      const quote = quotes[indexOfBuy].abs();
+     
+      dispatch({
+        type: ActionType.SetSwapQuote,
+        payload: quote,
+      });
+    } catch (err) {
+      console.error('Error fetching swap quote', err)
+    } finally {
+      dispatch({ type: ActionType.SetSwapQuoteLoading, payload: false });
+    }
+  };
+
+  const swap = async () => {
+    const { value } = state.sell;
+
+    if (!value) {
+      console.error('A sell amount is required');
+      return;
+    }
+
+    dispatch({ type: ActionType.UpdateSwapState, payload: { isLoading: true, error: '' } });
+
+    try {
+      const amount = getBigNumberFromString(value);
+      const deadline = getSwapDeadline(state.transactionSettings.deadlineMinutes);
+      await vaultContract.swap(
+        amount,
+        state.sell.address,
+        state.buy.address,
+        state.quote.estimateWithSlippage!,
+        deadline,
+      );
+      
+      dispatch({ type: ActionType.UpdateSwapState, payload: { isLoading: false, error: '' } });
+      dispatch({ type: ActionType.ResetQuoteState, payload: null });
+
+    } catch (err) {
+      console.error('Error swapping', err)
+      dispatch({ type: ActionType.UpdateSwapState, payload: { isLoading: false, error: (err as Error).message } });
+    }
+  };
+
+  return {
+    state: {
+      ...state,
+      sell: {
+        ...state.sell,
+        balance: sellTokenBalance,
+      },
+      buy: {
+        ...state.buy,
+        balance: buyTokenBalance,
+      },
+    },
+    vaultAddress: vaultContract.address,
+    togglePair: () => dispatch({ type: ActionType.TogglePair, payload: null }),
+    setSellValue: async (value: string) => {
+      dispatch({ type: ActionType.SetSellValue, payload: value });
+      
+      const amount = getBigNumberFromString(value);
+      updateSwapQuote(amount);
+    },
+    swap,
+    setTransactionSettings: (settings: TradeState['transactionSettings']) => {
+      dispatch({ type: ActionType.SetTransactionSettings, payload: settings });
+    },
+  };
+}
--- apps/dapp/src/components/Pages/Ascend/components/Trade/index.tsx
+++ apps/dapp/src/components/Pages/Ascend/components/Trade/index.tsx
@@ -1,201 +1,29 @@
-import { useState, useEffect, useReducer, useRef } from 'react';
-import { BigNumber, Contract } from 'ethers';
-import styled from 'styled-components';
-import { useBalance, useContractReads } from 'wagmi';
+import { useState } from 'react';
+import { BigNumber } from 'ethers';
 
-import balancerPoolAbi from 'data/abis/balancerPool.json'
-import balancerVaultAbi from 'data/abis/balancerVault.json'
-import { formatNumber } from 'utils/formatter';
+import { formatNumber, formatNumberFixedDecimals } from 'utils/formatter';
 import { Pool } from 'components/Layouts/Ascend/types';
 import { Input } from 'components/Input/Input';
-import { useWallet } from 'providers/WalletProvider';
 import { ZERO } from 'utils/bigNumber';
-import { buttonResets, flexCenter } from 'styles/mixins';
 import { getBigNumberFromString, formatBigNumber } from 'components/Vault/utils';
-import { Nullable } from 'types/util';
 import { TransactionSettingsModal } from 'components/TransactionSettingsModal/TransactionSettingsModal';
 import { useTokenContractAllowance } from 'hooks/core/use-token-contract-allowance';
+import { CircularLoader as BaseCircularLoader } from 'components/Loader/CircularLoader';
+import Loader from 'components/Loader/Loader';
+
+import { useVaultTradeState } from './hooks/use-vault-trade-state';
 
 import {
   TradeWrapper,
   TradeHeader,
-} from '../../styles';
-import Loader from 'components/Loader/Loader';
-
-import { useTradeState } from './hooks';
-
-interface Props {
-  pool: Pool;
-}
-
-type Action<A extends ActionType, P extends any> = { type: A, payload: P };
-
-enum ActionType {
-  TogglePair,
-  SetSellValue,
-  SetSwapQuote,
-  SetSwapQuoteLoading,
-}
-
-type Actions = 
-  Action<ActionType.TogglePair, null> |
-  Action<ActionType.SetSellValue, string> |
-  Action<ActionType.SetSwapQuote, [string, string]> |
-  Action<ActionType.SetSwapQuoteLoading, boolean>;
-
-interface TradeState {
-  sell: {
-    name: string;
-    weight: BigNumber;
-    address: string;
-    symbol: string;
-    // Input value
-    value: string;
-  };
-  buy: {
-    name: string;
-    weight: BigNumber;
-    address: string;
-    symbol: string;
-    // input Value
-    value: string;
-  };
-  quote: {
-    loading: boolean;
-    value: Nullable<string[]>;
-  };
-}
-
-const reducer = (state: TradeState, action: Actions) => {
-  switch (action.type) {
-    case ActionType.SetSellValue: {
-      return {
-        ...state,
-        sell: {
-          ...state.sell,
-          value: action.payload,
-        },
-      };
-    }
-    case ActionType.TogglePair: {
-      return {
-        ...state,
-        sell: {
-          ...state.buy,
-          value: '',
-        },
-        buy: {
-          ...state.sell,
-          value: '',
-        },
-        quote: {
-          value: null,
-          loading: false,
-        },
-      };
-    }
-    case ActionType.SetSwapQuote: {
-      return {
-        ...state,
-        quote: {
-          ...state.quote,
-          value: action.payload,
-        },
-      };
-    }
-    case ActionType.SetSwapQuoteLoading: {
-      return {
-        ...state,
-        quote: {
-          ...state.quote,
-          loading: action.payload,
-        },
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-};
-
-class BalancerQuote {
-  private _wallet: string;
-  private _contract: Contract;
-  private _pool: Pool;
-  private _currentQuote: Nullable<{
-    assetOut: string;
-    assetIn: string;
-    value: BigNumber;
-    result: Nullable<[string, string]>;
-  }>;
-
-  constructor(
-    contract: Contract,
-    pool: Pool,
-    wallet: string,
-  ) {
-    this._contract = contract;
-    this._pool = pool;
-    this._currentQuote = null;
-    this._wallet = wallet;
-  }
-
-  isSameQuote = (value: BigNumber, buyAsset: string, sellAsset: string): boolean => {
-    const quote = this._currentQuote;
-    if (!quote) {
-      return false;
-    }
-
-    return quote.value.eq(value) && quote.assetIn === sellAsset && quote.assetOut === buyAsset;
-  }
-
-  getSwapQuote = async (value: BigNumber, buyAsset: string, sellAsset: string, onComplete: (value: [string, string]) => void) => {
-    if (this.isSameQuote(value, buyAsset, sellAsset)) {
-      return;
-    }
-
-    const assetOutIndex = this._pool.tokensList.findIndex((address) => address === buyAsset);
-    const assetInIndex = this._pool.tokensList.findIndex((address) => address === sellAsset);
-
-    this._currentQuote = {
-      value,
-      assetOut: buyAsset,
-      assetIn: sellAsset,
-      result: null,
-    };
-    
-    /*
-    * queryBatchSwap simulates a call to `batchSwap`, returning an array of Vault asset deltas. Calls to `swap` cannot be
-    * simulated directly, but an equivalent `batchSwap` call can and will yield the exact same result.
-    *
-    * Each element in the array corresponds to the asset at the same index, and indicates the number of tokens (or ETH)
-    * the Vault would take from the sender (if positive) or send to the recipient (if negative). The arguments it
-    * receives are the same that an equivalent `batchSwap` call would receive.
-    */
-    const quote = await this._contract.callStatic.queryBatchSwap(
-      0,
-      [{
-        poolId: this._pool.id,
-        assetInIndex,
-        assetOutIndex,
-        amount: value,
-        userData: '0x',
-      }],
-      this._pool.tokensList,
-      {
-        sender: this._wallet.toLowerCase(),
-        recipient: this._wallet.toLowerCase(),
-        fromInternalBalance: false,
-        toInternalBalance: false,
-      }
-    );
-
-    if (this.isSameQuote(value, buyAsset, sellAsset)) {
-      this._currentQuote.result = quote.map((value: BigNumber) => value.toString());
-      onComplete(this._currentQuote.result!);
-    }
-  };
-}
+  SwapControls,
+  ToggleButton,
+  LoadWrapper,
+  ReceivedValues,
+  SlippageButton,
+  SwapButton,
+  ErrorMessage,
+} from './styles';
 
 interface Props {
   pool: Pool;
@@ -204,22 +32,19 @@ interface Props {
 export const Trade = ({ pool }: Props) => {
   const [transactionSettingsOpen, setTransactionSettingsOpen] = useState(false);
   const {
+    swap,
     state,
     togglePair,
     setSellValue,
-    swap,
-    vaultAddress: lbpVaultAddress,
-  } = useTradeState(pool);
-  const [{ allowance, isLoading: allowanceIsLoading }, increaseAllowance] = useTokenContractAllowance(state.sell, lbpVaultAddress);
-
+    vaultAddress,
+    setTransactionSettings,
+  } = useVaultTradeState(pool);
+  const [{ allowance, isLoading: allowanceIsLoading }, increaseAllowance] = useTokenContractAllowance(state.sell, vaultAddress);
   const bigSellAmount = getBigNumberFromString(state.sell.value);
 
-  const indexOfSell = pool.tokensList.findIndex((address) => address === state.sell.address);
-  const indexOfBuy = pool.tokensList.findIndex((address) => address === state.buy.address);
-
   let receiveEstimate = '';
-  if (state.quote.value) {
-    receiveEstimate = Math.abs(Number(state.quote.value[indexOfBuy])).toString();
+  if (state.quote.estimate) {
+    receiveEstimate = formatBigNumber(state.quote.estimate);
   } 
 
   return (
@@ -227,8 +52,11 @@ export const Trade = ({ pool }: Props) => {
       <TransactionSettingsModal
         isOpen={transactionSettingsOpen}
         onClose={() => setTransactionSettingsOpen(false)}
-        onChange={(v) => {
-          console.log(v)
+        onChange={({ slippageTolerance, deadlineMinutes }) => {
+          if (slippageTolerance < 0 || slippageTolerance > 100) {
+            return;
+          }
+          setTransactionSettings({ slippageTolerance, deadlineMinutes });
         }}
       />
       <TradeWrapper>
@@ -268,6 +96,29 @@ export const Trade = ({ pool }: Props) => {
           hint={`Balance: ${formatNumber(formatBigNumber(state.buy.balance as BigNumber))}`}
           disabled
         />
+        <SwapControls>
+          {state.quote.loading && (
+            <LoadWrapper>
+              <BaseCircularLoader />
+            </LoadWrapper>
+          )}
+          <ReceivedValues>
+            {(!!receiveEstimate && !state.quote.loading) && (
+              <>
+                Expected Output: {formatNumberFixedDecimals(receiveEstimate, 3)}<br />
+                Minimum Amount: {formatNumberFixedDecimals(formatBigNumber(state.quote.estimateWithSlippage!), 3)}
+              </>
+            )}
+            {state.quote.loading && (
+              <>
+                Fetching Price...
+              </>
+            )}
+          </ReceivedValues>
+          <SlippageButton type="button" onClick={() => setTransactionSettingsOpen(true)}>
+            {state.transactionSettings.slippageTolerance}%
+          </SlippageButton>
+        </SwapControls>
         {allowance === 0 && (
           <SwapButton
             type="button"
@@ -287,7 +138,9 @@ export const Trade = ({ pool }: Props) => {
             disabled={
               bigSellAmount.eq(ZERO) ||
               bigSellAmount.gt(state.sell.balance) ||
-              state.quote.loading
+              state.quote.loading ||
+              !state.quote.estimate ||
+              state.swap.isLoading
             }
             onClick={() => {
               swap();
@@ -296,42 +149,12 @@ export const Trade = ({ pool }: Props) => {
             Swap
           </SwapButton>
         )}
-        {state.quote.value && (
-          <div>
-            Estimate <br />
-            Receive: {Math.abs(Number(state.quote.value[indexOfBuy]))}
-            For: {state.quote.value[indexOfSell]}
-          </div>
+        {!!state.swap.error && (
+          <ErrorMessage>
+            {state.swap.error}
+          </ErrorMessage>
         )}
       </TradeWrapper>
     </>
   );
 };
-
-const ToggleButton = styled.button`
-  ${buttonResets}
-
-  background-color: ${({ theme }) => theme.palette.brand};
- 
-
-  width: 1.875rem;
-  height: 1.875rem;
-  border-radius: 50%;
-  margin-top: calc(-0.2rem - 0.5625rem);
-  margin-bottom: -0.5625rem;
-  z-index: 50;
-`;
-
-const SwapButton = styled.button`
-  ${buttonResets}
-  ${flexCenter}
-
-  border-radius: 0.625rem;
-  background-color: ${({ theme }) => theme.palette.brand};
-  font-weight: 700;
-  color: #fff;
-  display: flex;
-  padding: 1.25rem 0;
-  width: 100%;
-  text-transform: uppercase;
-`;
