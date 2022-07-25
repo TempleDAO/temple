0d3bd6206d1d51dd34bb4b82b3c93b7c87e72bb0
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-07-25 11:54:25 -0700

    Some more chart styling (#471)
    * more chart styling
    
    * console error

--- apps/dapp/src/components/Pages/Ascend/components/Chart/index.tsx
+++ apps/dapp/src/components/Pages/Ascend/components/Chart/index.tsx
@@ -9,6 +9,7 @@ import {
   VerticalGridLines,
   Crosshair,
   ChartLabel,
+  LineMarkSeries,
 } from 'react-vis';
 import { curveCatmullRom } from 'd3-shape';
 
@@ -17,6 +18,9 @@ import { formatNumberFixedDecimals } from 'utils/formatter';
 import { Pool } from 'components/Layouts/Ascend/types';
 import Loader from 'components/Loader/Loader';
 import { theme } from 'styles/theme';
+import { getSpotPrice } from '../../utils';
+import { useAuctionContext } from '../AuctionContext';
+import { DecimalBigNumber } from 'utils/DecimalBigNumber';
 
 import { useCrosshairs, useLatestPriceData, Point } from './hooks';
 
@@ -25,9 +29,10 @@ interface Props {
 }
 
 export const Chart = ({ pool }: Props) => {
+  const { balances } = useAuctionContext();
   const [request, { response, isLoading }] = useLatestPriceData(pool);
 
-  const { data, yDomain, xDomain } = useMemo(() => {
+  const { data, yDomain, xDomain, predicted } = useMemo(() => {
     const {joinExits, ...data} = (response?.data || {})
 
     const points = Object.values(data)
@@ -37,17 +42,50 @@ export const Chart = ({ pool }: Props) => {
         y: Number(value[0].price),
       }));
 
-    
-    const sortedDesc = points.sort((a, b) => b.y - a.y);
     const lastUpdate = pool.weightUpdates[pool.weightUpdates.length - 1];
+    const lastUpdateEnd = lastUpdate.endTimestamp.getTime();
+    const lastUpdateStart = lastUpdate.startTimestamp.getTime();
+    const lbpLength = lastUpdateEnd - lastUpdateStart;
+
+    const sortedDesc = points.sort((a, b) => b.y - a.y);
     const ceiling = sortedDesc.length > 0 ? sortedDesc[0].y : 0;
     const yDomain = points.length > 0 ? [0, ceiling + (ceiling * 0.1)] : null;
+   
+    const [buy, sell] = pool.tokensList;
+    const predicted = [];
+    if (balances) {
+      try {
+        const spotPriceEstimate = getSpotPrice(
+          balances[sell]!,
+          balances[buy]!,
+          DecimalBigNumber.fromBN(lastUpdate.endWeights[1], 18),
+          DecimalBigNumber.fromBN(lastUpdate.endWeights[0], 18),
+          pool.swapFee
+        );
 
+        if (spotPriceEstimate) {
+          predicted.push({
+            y: points[points.length - 1].y,
+            x: points[points.length - 1].x,
+          });
+
+          predicted.push({
+            y: formatNumberFixedDecimals(formatBigNumber(spotPriceEstimate), 4),
+            x: lastUpdateEnd,
+          });
+        }
+      } catch (err) {
+        // intentionally empty
+        console.error('Failed to calculate predicted chart data points');
+      }
+    }
+    
     return {
       data: points,
+      predicted,
       yDomain,
-      xDomain: [lastUpdate.startTimestamp.getTime(), lastUpdate.endTimestamp.getTime()],
-    }
+      xDomain: [lastUpdateStart, lastUpdateEnd + (lbpLength * 0.05)],
+    };
   }, [pool, response]);
 
   const { crosshairValues, onMouseLeave, onNearestX } = useCrosshairs(data);
@@ -92,7 +130,10 @@ export const Chart = ({ pool }: Props) => {
       yDomain={yDomain}
       onMouseLeave={onMouseLeave}
       height={400}
+      margin={{ right: 20}}
     >
+      <VerticalGridLines style={{ stroke: theme.palette.brand25 }} />
+      <HorizontalGridLines style={{ stroke: theme.palette.brand25 }} />
       <XAxis
         style={{
           line: { stroke: theme.palette.brand },
@@ -122,6 +163,14 @@ export const Chart = ({ pool }: Props) => {
         // @ts-ignore
         strokeWidth={2}
         onNearestX={onNearestX}
+      />
+      <LineMarkSeries
+        data={predicted}
+        color={theme.palette.brandLight}
+        curve={curveCatmullRom}
+        strokeStyle="dashed"
+        // @ts-ignore
+        strokeWidth={1}
       />
       <Crosshair
         values={crosshairValues}
