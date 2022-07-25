3205e7456ff013c9e7d4e275da0e74a05fc91efe
fujisawa <mrfujisawa@protonmail.com>
2022-07-24 19:35:00 -0700

    wip

--- apps/dapp/src/components/Pages/Ascend/components/Chart/index.tsx
+++ apps/dapp/src/components/Pages/Ascend/components/Chart/index.tsx
@@ -27,10 +27,10 @@ interface Props {
 export const Chart = ({ pool }: Props) => {
   const [request, { response, isLoading }] = useLatestPriceData(pool);
 
-  const data = useMemo(() => {
-    const {joinExists, ...points} = (response?.data || {})
+  const { data, yDomain, xDomain } = useMemo(() => {
+    const {joinExists, ...data} = (response?.data || {})
 
-    const dataPoints = Object.values(points)
+    const points = Object.values(data)
       .filter((value) => value.length > 0)
       .map((value) => ({
         x: Number(value[0].timestamp) * 1000,
@@ -38,17 +38,11 @@ export const Chart = ({ pool }: Props) => {
       }));
     
     const lastUpdate = pool.weightUpdates[pool.weightUpdates.length - 1];
-    const start = {
-      x: Number(lastUpdate.startTimestamp),
-      // y: 
-    };
-
-    // const 
 
     return {
-      data,
+      data: points,
       yDomain: [0, 1],
-      xDomain: [Number(lastUpdate.startTimestamp), Number(lastUpdate.endTimestamp)],
+      xDomain: [lastUpdate.startTimestamp.getTime(), lastUpdate.endTimestamp.getTime()],
     }
   }, [pool, response]);
 
@@ -65,38 +59,36 @@ export const Chart = ({ pool }: Props) => {
       </div>
     );
   }
-  console.log(response)
 
-  let numTicks = 0;
-  let updates: any = {}
-  if (data.length > 0) {
-    updates = pool.weightUpdates[pool.weightUpdates.length - 1];
-    console.log('data', data)
-  }
-
-  if (!data.data.length ) {
-    return null
+  if (!data.length) {
+    return (
+      <FlexibleXYPlot
+        height={400}
+        xType="time"
+        dontCheckIfEmpty
+      >
+        <ChartLabel
+          text="Not enough data"
+          includeMargin={false}
+          xPercent={0.5}
+          yPercent={0.5}
+          style={{
+            transform: 'translate(-50, 0)',
+          }}
+        />
+      </FlexibleXYPlot>
+    )
   }
 
   return (
     <FlexibleXYPlot
       xType="time"
       dontCheckIfEmpty
-      xDomain={data.xDomain}
-      yDomain={data.yDomain}
-      height={500}
+      xDomain={xDomain}
+      yDomain={yDomain}
       onMouseLeave={onMouseLeave}
+      height={400}
     >
-      <HorizontalGridLines
-        style={{
-          stroke: theme.palette.brand,
-        }}
-      />
-      <VerticalGridLines
-        style={{
-          stroke: theme.palette.brand,
-        }}
-      />
       <XAxis
         style={{
           line: { stroke: theme.palette.brand },
@@ -135,16 +127,6 @@ export const Chart = ({ pool }: Props) => {
             value: format(x, 'd LLL'),
           });
         }}
-        style={{
-          line: {
-            stroke: theme.palette.brandLight,
-            fill: theme.palette.brandLight,
-            path: theme.palette.brandLight,
-            color: theme.palette.brandLight,
-          },
-          title: {},
-          box: {}
-        }}
         itemsFormat={([{ y }]: Point[]) => {
           const value = formatNumberFixedDecimals(formatBigNumber(getBigNumberFromString(`${y}`)), 4);
           return [
@@ -152,90 +134,6 @@ export const Chart = ({ pool }: Props) => {
           ];
         }}
       />
-    </FlexibleXYPlot>
-  )
-
-  console.log(data)
-  return (
-    <FlexibleXYPlot
-      xType="time"
-      // height={322}
-      // width={400}
-      dontCheckIfEmpty
-      xDomain={updates.endTimestamp ? [Number(updates.startTimestamp), Number(updates.endTimestamp)] : []}
-      // yDomain={[0, 1]}
-      // onMouseLeave={onMouseLeave}
-    >
-      {data.length > 0 ? (
-        <>
-          <HorizontalGridLines
-            style={{
-              stroke: theme.palette.brand,
-            }}
-          />
-          <VerticalGridLines
-            style={{
-              stroke: theme.palette.brand,
-            }}
-          />
-          <XAxis
-            style={{
-              stroke: theme.palette.brand,
-              line: { stroke: '#2b2a2d' },
-              ticks: { stroke: '#6b6b76' },
-            }}
-            tickFormat={(value: number) => {
-              return format(value, 'd LLL');
-            }}
-            tickTotal={data.length + 2}
-          />
-          <YAxis
-            style={{
-              line: { stroke: '#2b2a2d' },
-              ticks: { stroke: '#6b6b76' },
-              text: { stroke: 'none', fill: '#6b6b76', fontWeight: 600 },
-            }}
-            tickTotal={5}
-            tickFormat={(t) => t}
-          />
-          <LineSeries
-            color={theme.palette.brand}
-            data={data}
-            // @ts-ignore
-            strokeWidth={2}
-            curve={curveCatmullRom}
-            onNearestX={onNearestX}
-          />
-          {/* <Crosshair
-            values={crosshairValues}
-            titleFormat={([{ x }]: Point[]) => {
-              return ({
-                title: 'date',
-                value: format(x, 'd LLL'),
-              });
-            }}
-            style={{
-              stroke: theme.palette.brandLight
-            }}
-            itemsFormat={([{ y }]: Point[]) => {
-              const value = formatNumberFixedDecimals(formatBigNumber(getBigNumberFromString(`${y}`)), 4);
-              return [
-                { title: 'price', value },
-              ];
-            }}
-          /> */}
-        </>
-      ) : (
-        <ChartLabel
-          text="Not enough data"
-          includeMargin={false}
-          xPercent={0.5}
-          yPercent={0.5}
-          style={{
-            transform: 'translate(-50, 0)',
-          }}
-        />
-      )}
     </FlexibleXYPlot>
   );
 };
