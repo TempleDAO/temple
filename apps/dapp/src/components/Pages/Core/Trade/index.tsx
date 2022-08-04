b2c2982f227868f0405ddeabc75183278164a2ec
zelki <zelki.codes@protonmail.com>
2022-08-04 09:45:04 +0100

    fix: deprecate ogt staking UI and routes

--- apps/dapp/src/components/Pages/Core/Trade/index.tsx
+++ apps/dapp/src/components/Pages/Core/Trade/index.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
-import { Route, Routes, Link as BaseLink, useResolvedPath, useMatch } from 'react-router-dom';
+import { Route, Routes, Link as BaseLink, useResolvedPath, useMatch, Navigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import BaseImage from 'components/Image/Image';
 import { useWatchAsset, TEMPLE_ASSET } from 'hooks/use-watch-asset';
 import { Trade } from './views/Trade';
 import { Unstake } from './views/Unstake';
-import { Stake } from './views/Stake';
 import { PageWrapper } from '../utils';
 import { Container } from './styles';
 import { tabletAndAbove } from 'styles/breakpoints';
@@ -15,7 +14,7 @@ import { TICKER_SYMBOL } from 'enums/ticker-symbol';
 
 const TradeRoutes = () => {
   const [watchAsset] = useWatchAsset(TEMPLE_ASSET);
- 
+
   return (
     <>
       <PageWrapper>
@@ -24,7 +23,6 @@ const TradeRoutes = () => {
             <MenuWrapper>
               <Menu>
                 <TradeLink to="/dapp/trade">Trade</TradeLink>
-                <TradeLink to="/dapp/trade/stake">Stake</TradeLink>
                 <TradeLink to="/dapp/trade/unstake">Unstake</TradeLink>
               </Menu>
             </MenuWrapper>
@@ -32,11 +30,11 @@ const TradeRoutes = () => {
               <Routes>
                 <Route path="/" element={<Trade />} />
                 <Route path="/unstake" element={<Unstake />} />
-                <Route path="/stake" element={<Stake />} />
+                <Route path="/*" element={<Navigate to=".." replace />} />
               </Routes>
             </Wrapper>
             {watchAsset && (
-               <AddTokenButton type="button" onClick={watchAsset}>
+              <AddTokenButton type="button" onClick={watchAsset}>
                 <Image
                   src={TEMPLE_ASSET.image}
                   width={24}
@@ -57,9 +55,7 @@ const TradeRoutes = () => {
 const TradeLink: FC<{ to: string }> = (props) => {
   const resolved = useResolvedPath(props.to);
   const match = useMatch({ path: resolved.pathname, end: true });
-  return (
-    <Link {...props} $isActive={!!match} />
-  );
+  return <Link {...props} $isActive={!!match} />;
 };
 
 const Wrapper = styled.div`
@@ -88,10 +84,10 @@ const Menu = styled.div`
 `;
 
 const Link = styled(BaseLink)<{ $isActive: boolean }>`
-  padding: .5rem 1rem;
-  border-radius: .5rem;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
   display: block;
-  background: ${({ $isActive, theme }) => $isActive ? '#1D1A1A' : 'transparent'};
+  background: ${({ $isActive, theme }) => ($isActive ? '#1D1A1A' : 'transparent')};
 `;
 
 const AddTokenButton = styled.button`
