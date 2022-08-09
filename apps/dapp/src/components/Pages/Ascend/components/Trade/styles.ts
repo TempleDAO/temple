8e92d9edfb21a5f7fcfbb1d92e2b277692d4fd7c
Mr.Fujisawa <96760955+mrfujisawa@users.noreply.github.com>
2022-08-08 18:24:21 -0700

    ascend bug fixes (#496)
    * use from context
    
    * fix error parsing
    
    * format pool composition
    
    * Add arrow to ascend trade
    
    * convert more numbers to decimal big numbers, fix crashing slippage interaction
    
    * cleanup
    
    * fix note
    
    * remove dead code
    
    * remove unused code

--- apps/dapp/src/components/Pages/Ascend/components/Trade/styles.ts
+++ apps/dapp/src/components/Pages/Ascend/components/Trade/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-import { buttonResets, flexCenter } from 'styles/mixins';
+import { backgroundImage, buttonResets, flexCenter } from 'styles/mixins';
+import arrowIcon from 'assets/icons/arrow-icon-no-background.svg';
+import { theme } from 'styles/theme'; 
 
 export const Wrapper = styled.div<{ verticalAlignment?: 'top' | 'center' }>`
   padding: 28px;
@@ -48,9 +50,16 @@ export const SwapControls = styled.div`
 export const ToggleButton = styled.button`
   ${buttonResets}
 
-  background-color: ${({ theme }) => theme.palette.brand};
- 
+  ${backgroundImage(arrowIcon, {
+    color: theme.palette.brand,
+    size: '50%',
+  })}
 
+  &:hover {
+    transform: rotate(180deg);
+  }
+
+  transition: all 150ms;
   width: 1.875rem;
   height: 1.875rem;
   border-radius: 50%;
