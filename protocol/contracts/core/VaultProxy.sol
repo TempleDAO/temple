b4fac2a6cceb2f36cf7dfef47d22d644f8bd99c7
scooprinder <hi@scoop.codes>
2022-08-12 09:44:42 +0100

    Remove faith from proxy and add early exit mechanism

--- protocol/contracts/core/VaultProxy.sol
+++ protocol/contracts/core/VaultProxy.sol
@@ -6,75 +6,31 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./Vault.sol";
-import "../util/ABDKMathQuad.sol";
 import "../deprecated/TempleStaking.sol";
-import "../deprecated/Faith.sol";
 
 /**
     @notice A proxy contract for interacting with Temple Vaults. 
  */
 contract VaultProxy is Ownable {
-    using ABDKMathQuad for bytes16;
     /** @notice Tokens / Contracted required for the proxy contract  */
     IERC20 public immutable ogTemple;
     IERC20 public immutable temple;
     TempleStaking public immutable templeStaking;
-    Faith public immutable faith;
-    bool public faithClaimEnabled;
+
+    // Errors
+    error NoTempleReceivedWhenUnstaking();
+    error SendToAddressZero();
+    error WithdrawSendFailed();
+    error CanNotExitVault();
 
     constructor(
         OGTemple _ogTemple,
         IERC20 _temple,
-        TempleStaking _templeStaking,
-        Faith _faith
+        TempleStaking _templeStaking
     ) {
         ogTemple = _ogTemple;
         temple = _temple;
         templeStaking = _templeStaking;
-        faith = _faith;
-        faithClaimEnabled = true;
-    }
-
-    bytes16 private constant MAX_MULT = 0x3fff4ccccccccccccccccccccccccccc; // 1.3
-    bytes16 private constant TA_MULT = 0x40004000000000000000000000000000; // 2.5
-
-    /**
-        @notice Given an amount of Faith and Temple, apply the boosting curve and produce the amount of boosted Temple one can expect to receive.
-                Formula is BoostedTemple = TempleProvided * min(1.3, (1+faith/(2.5*TempleProvided)))
-     */
-    function getFaithMultiplier(uint256 _amountFaith, uint256 _amountTemple) pure public returns (uint256) {
-        // Tl = Ta * min(1.3, (1+faith/2.5*Ta))
-        bytes16 amountFaith = ABDKMathQuad.fromUInt(_amountFaith);
-        bytes16 amountTemple = ABDKMathQuad.fromUInt(_amountTemple);
-        bytes16 t = ABDKMathQuad.fromUInt(1).add(amountFaith.div(TA_MULT.mul(amountTemple)));
-        bytes16 mult = MAX_MULT < t ? MAX_MULT : t;
-
-        return ABDKMathQuad.toUInt(amountTemple.mul(mult));
-    }
-
-    /**
-        @notice Takes provided faith and Temple, applies the boost then immediate deposits into a vault
-     */
-    function depositTempleWithFaith(uint256 _amountTemple, uint112 _amountFaith, Vault vault) public {
-        require(faithClaimEnabled, "VaultProxy: Faith claim no longer enabled");
-        faith.redeem(msg.sender, _amountFaith);
-        uint256 boostedAmount = getFaithMultiplier(_amountFaith, _amountTemple);
-        SafeERC20.safeTransferFrom(temple, msg.sender, address(this), _amountTemple);
-        SafeERC20.safeIncreaseAllowance(temple, address(vault), boostedAmount);
-        vault.depositFor(msg.sender, boostedAmount);
-    }
-
-    /**
-        @notice Takes provided faith and OGT, unstakes the OGT into Temple, applies the boost and then immediately
-        deposits into a vault
-     */
-    function unstakeAndDepositTempleWithFaith(uint256 _amountOGT, uint112 _amountFaith, Vault vault) external {
-        require(faithClaimEnabled, "VaultProxy: Faith claim no longer enabled");
-        faith.redeem(msg.sender, _amountFaith);
-        uint256 unstakedTemple = unstakeOGT(_amountOGT);
-        uint256 boostedAmount = getFaithMultiplier(_amountFaith, unstakedTemple);
-        SafeERC20.safeIncreaseAllowance(temple, address(vault), boostedAmount);
-        vault.depositFor(msg.sender, boostedAmount);
     }
 
     /**
@@ -97,7 +53,9 @@ contract VaultProxy is Ownable {
         uint256 templeBeforeBalance = temple.balanceOf(address(this));
         templeStaking.unstake(_amountOGT);
         uint256 templeAfterBalance = temple.balanceOf(address(this));
-        require(templeAfterBalance > templeBeforeBalance, "Vault Proxy: no Temple received when unstaking");
+        if (templeAfterBalance < templeBeforeBalance) {
+            revert NoTempleReceivedWhenUnstaking();
+        }
 
         return templeAfterBalance - templeBeforeBalance;
     }
@@ -113,23 +71,49 @@ contract VaultProxy is Ownable {
     }
 
     /**
-    * Toggle whether faith is claimable
+    * @notice Transfer out amount of token to provided address
     */
-    function toggleFaithClaimEnabled() external onlyOwner {
-        faithClaimEnabled = !faithClaimEnabled;
-    }
-
-    /**
-    * transfer out amount of token to provided address
-    */
-    function withdraw(address token, address to, uint256 amount) external onlyOwner {
-        require(to != address(0), "to address zero");
+    function withdraw(address token, address to, uint256 amount) public onlyOwner {
+        if (to == address(0)) {
+            revert SendToAddressZero();
+        }
 
         if (token == address(0)) {
             (bool sent,) = payable(to).call{value: amount}("");
-            require(sent, "send failed");
+            if (!sent) {
+                revert WithdrawSendFailed();
+            }
         } else {
             SafeERC20.safeTransfer(IERC20(token), to, amount);
         }
+    }
+
+    /**
+     * Allows users to withdraw their Temple from a vault before the exit window, forfeits any yield
+     */
+    function exitVaultEarly(uint256 amount, Vault vaultErc) external {
+        SafeERC20.safeTransferFrom(IERC20(vaultErc), msg.sender, address(this), amount);
+        uint256 shareAmount = vaultErc.toSharesAmount(amount);
+        uint256 templeBal = vaultErc.toTokenAmount(shareAmount);
+        SafeERC20.safeTransfer(temple, msg.sender, templeBal);
+    }
+
+    /**
+     * Allows owner to redeem any vault shares from users that have exited early
+     */
+    function redeemVaultTokenToTemple(Vault vaultErc) public onlyOwner {
+        if (!vaultErc.canExit()) {
+            revert CanNotExitVault();
+        }
+        uint256 balance = vaultErc.balanceOf(address(this));
+        vaultErc.withdraw(balance);
+    }
+
+    /**
+     * Allows owner to redeem any vault shares and then withdraw to the provided address
+     */
+    function redeemVaultTokenToTempleAndWithdraw(Vault vaultErc, address to, uint256 amount) external onlyOwner {
+        redeemVaultTokenToTemple(vaultErc);
+        withdraw(address(temple), to, amount);
     }
 }
