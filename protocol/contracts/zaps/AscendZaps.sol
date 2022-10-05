1af1ea2d4f6801f75531a331d7b3b00c58893758
0xlock <0xl0ck@proton.me>
2022-10-05 07:43:11 -0500

    optimize AscendZaps contract, add comments to AscendZaps contract

--- protocol/contracts/zaps/AscendZaps.sol
+++ protocol/contracts/zaps/AscendZaps.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.4;
+pragma solidity 0.8.17;
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -9,12 +9,20 @@ import "../core/Vault.sol";
 import "./interfaces/IBalancerVault.sol";
 
 contract AscendZaps is Ownable {
+    using SafeERC20 for IERC20;
+
+    /// @notice vaultedTemple address
     address public vaultedTemple;
-    IBalancerVault private immutable balancerVault = IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+
+    /// @notice temple token address
     IERC20 public immutable temple;
 
-    // Used to track how much Temple has been zapped
-    mapping(IERC20 => uint256) internal withdrawn;
+    /// @notice balancer vault address
+    IBalancerVault private immutable balancerVault =
+        IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+
+    /// @notice Used to track how much Temple has been zapped
+    mapping(address => uint256) internal withdrawn;
 
     // Errors
     error FirstVaultCycle();
@@ -25,45 +33,76 @@ contract AscendZaps is Ownable {
     error WithdrawSendFailed();
 
     // Events
-    event EarlyWithdraw(address account, uint256 fromAmount, address vault, address toToken, uint256 toTokenAmount);
+    event EarlyWithdraw(
+        address account,
+        uint256 fromAmount,
+        address vault,
+        address toToken,
+        uint256 toTokenAmount
+    );
 
     constructor(address _vaultedTemple, IERC20 _temple) {
         vaultedTemple = _vaultedTemple;
         temple = _temple;
     }
 
-     /**
+    /**
      * @notice Allows user to withdraw their Temple from a vault before the exit window, forfeits any yield
+     * @param amount vault token amount
+     * @param vaultErc vault address
+     * @param singleSwap balancer swap data
+     * @param funds balancer swap data
+     * @param limit balancer swap limit
+     * @param deadline balancer swap deadline
+     * @return amountCalculated swapped amount
      */
     function exitVaultEarly(
-        uint256 amount, 
-        Vault vaultErc, 
-        IBalancerVault.SingleSwap memory singleSwap, 
-        IBalancerVault.FundManagement memory funds, 
-        uint256 limit, 
+        uint256 amount,
+        Vault vaultErc,
+        IBalancerVault.SingleSwap memory singleSwap,
+        IBalancerVault.FundManagement memory funds,
+        uint256 limit,
         uint256 deadline
     ) external returns (uint256 amountCalculated) {
-        (uint256 cycleNum,) = vaultErc.inEnterExitWindow();
-        if (cycleNum == 0) { 
+        (uint256 cycleNum, ) = vaultErc.inEnterExitWindow();
+        if (cycleNum == 0) {
             revert FirstVaultCycle();
         }
         // Transfer vault tokens to contract, and determine how much Temple that is currently worth
-        SafeERC20.safeTransferFrom(IERC20(vaultErc), msg.sender, address(this), amount);
+        IERC20(vaultErc).safeTransferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
         uint256 shareAmount = vaultErc.toSharesAmount(amount);
         uint256 templeBal = vaultErc.toTokenAmount(shareAmount);
-        
-        // Check that the supplied offchain swap data is correct; not strictly needed 
-        if (address(singleSwap.assetIn) != address(temple) || singleSwap.amount != templeBal) {
+
+        // Check that the supplied offchain swap data is correct; not strictly needed
+        if (
+            address(singleSwap.assetIn) != address(temple) ||
+            singleSwap.amount != templeBal
+        ) {
             revert SwapDataDoesNotMatch();
         }
 
-        withdrawn[IERC20(vaultErc)] += templeBal;
+        withdrawn[address(vaultErc)] += templeBal;
 
-        SafeERC20.safeIncreaseAllowance(temple, address(balancerVault), templeBal);
+        temple.safeIncreaseAllowance(
+            address(balancerVault),
+            templeBal
+        );
 
-        // Swap 
-        try balancerVault.swap(singleSwap, funds, limit, deadline) returns (uint256 _amountCalculated) {
-            emit EarlyWithdraw(msg.sender, amount, address(vaultErc), address(singleSwap.assetOut), _amountCalculated);
+        // Swap
+        try balancerVault.swap(singleSwap, funds, limit, deadline) returns (
+            uint256 _amountCalculated
+        ) {
+            emit EarlyWithdraw(
+                msg.sender,
+                amount,
+                address(vaultErc),
+                address(singleSwap.assetOut),
+                _amountCalculated
+            );
             amountCalculated = _amountCalculated;
         } catch Error(string memory reason) {
             revert BalancerVaultSwapError(reason);
@@ -72,6 +111,7 @@ contract AscendZaps is Ownable {
 
     /**
      * @notice Allows owner to redeem any vault shares from users that have exited early. Expects that owner has called OpsManager#increaseVaultTemple prior
+     * @param vaultErc vault address
      */
     function redeemVaultTokenToTemple(Vault vaultErc) public onlyOwner {
         if (!vaultErc.canExit()) {
@@ -79,49 +119,64 @@ contract AscendZaps is Ownable {
         }
         uint256 balance = vaultErc.balanceOf(address(this));
 
-        uint256 amountTempleWithdrawn = withdrawn[IERC20(vaultErc)];
         uint256 currentShareAmount = vaultErc.toSharesAmount(balance);
-        uint256 currentTempleAmount = vaultErc.toTokenAmount(currentShareAmount);
+        uint256 currentTempleAmount = vaultErc.toTokenAmount(
+            currentShareAmount
+        );
 
-        uint256 delta = currentTempleAmount - currentTempleAmount;
+        uint256 delta = withdrawn[address(vaultErc)] - currentTempleAmount;
+        delete withdrawn[address(vaultErc)];
 
         vaultErc.withdraw(balance);
 
-        // redistribute interest 
-        SafeERC20.safeTransferFrom(temple, address(this), vaultedTemple, delta);
-
-        withdrawn[IERC20(vaultErc)] = 0;
+        // redistribute interest
+        temple.safeTransfer(vaultedTemple, delta);
     }
 
     /**
      * @notice Allows caller to check to see whether the zap contract has accrued any interest while holding vault tokens
+     * @param vaultErc vault address
+     * @return interestAccrued amount of interest accrued
      */
-    function checkInterestAccrued(Vault vaultErc) public view returns (uint256 interestAccrued) {
+    function checkInterestAccrued(Vault vaultErc)
+        public
+        view
+        returns (uint256 interestAccrued)
+    {
         // Get amount of Temple current 'owned' by the zapped in asset
         uint256 currentShareAmount = vaultErc.shareBalanceOf(address(this));
-        uint256 currentTempleAmount = vaultErc.toTokenAmount(currentShareAmount);
+        uint256 currentTempleAmount = vaultErc.toTokenAmount(
+            currentShareAmount
+        );
 
-        uint256 withdrawnAmount = withdrawn[IERC20(vaultErc)];
+        uint256 withdrawnAmount = withdrawn[address(vaultErc)];
 
-        uint256 interestAccrued = currentTempleAmount - withdrawnAmount;
+        interestAccrued = currentTempleAmount - withdrawnAmount;
     }
 
     /**
-    * @notice Transfer out amount of token to provided address. This function should never be called unless the accounting
+     * @notice Transfer out amount of token to provided address. This function should never be called unless the accounting
               within the contract prevents withdrawal and redistribution of the Vault ERC20.
-    */
-    function withdraw(address token, address to, uint256 amount) public onlyOwner {
+     * @param token token address to withdraw
+     * @param to recipient address
+     * @param amount withdraw amount
+     */
+    function withdraw(
+        address token,
+        address to,
+        uint256 amount
+    ) public onlyOwner {
         if (to == address(0)) {
             revert SendToAddressZero();
         }
 
         if (token == address(0)) {
-            (bool sent,) = payable(to).call{value: amount}("");
+            (bool sent, ) = payable(to).call{value: amount}("");
             if (!sent) {
                 revert WithdrawSendFailed();
             }
         } else {
-            SafeERC20.safeTransfer(IERC20(token), to, amount);
+            IERC20(token).safeTransfer(to, amount);
         }
     }
 }
