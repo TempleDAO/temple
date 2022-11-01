503980ec116e1cea133b9b5c4f29ab2565d49c63
mountainpath <mountainpath9@protonmail.com>
2022-11-01 17:33:06 +1100

    Remove unused imports

--- protocol/contracts/governance/ElderElection.sol
+++ protocol/contracts/governance/ElderElection.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Templar.sol";
+
 /**
  * @title A contract recording voting in the election of temple elders.
  *
