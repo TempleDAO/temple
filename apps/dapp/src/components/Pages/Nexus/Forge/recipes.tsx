a93f3c7643731a4cc2353b9f923c869cad239e6b
marshall <99344331+marshall2112@users.noreply.github.com>
2022-11-16 21:57:58 -0500

    Recipe order does not matter

--- apps/dapp/src/components/Pages/Nexus/Forge/recipes.tsx
+++ apps/dapp/src/components/Pages/Nexus/Forge/recipes.tsx
@@ -1,5 +1,5 @@
 import { RelicItemData } from 'providers/types';
-import { difference, isEqual } from 'lodash';
+import { intersection, isEqual } from 'lodash';
 
 export type Recipe = {
   id: number;
@@ -12,22 +12,33 @@ export type Recipe = {
 const RECIPES: Recipe[] = [
   { id: 0, required_ids: [0, 1], required_amounts: [1, 1], reward_ids: [2], reward_amounts: [1] },
   { id: 2, required_ids: [0, 1], required_amounts: [2, 3], reward_ids: [2], reward_amounts: [1] },
+  { id: 3, required_ids: [0, 1, 2], required_amounts: [1, 2, 1], reward_ids: [3], reward_amounts: [1] },
 ];
 
-export const getValidRecipe = (data: RelicItemData[]): Recipe | null => {
-  const inputItemIds = data.map((item) => item.id);
-  const inputItemCounts = data.map((item) => item.count);
+export const getValidRecipe = (items: RelicItemData[]): Recipe | null => {
+  if (!items) return null;
+  const inputItemIds = items.map((item) => item.id);
+  const inputItemCounts = items.map((item) => item.count);
 
-  const matchingRecipes = RECIPES.filter((recipe) => {
-    const sameIds = isEqual(recipe.required_ids, inputItemIds);
-    const sameCounts = isEqual(recipe.required_amounts, inputItemCounts);
-    return sameIds && sameCounts;
+  const matchingRecipesByIds = RECIPES.filter((recipe) => {
+    const sameIds = intersection(recipe.required_ids, inputItemIds);
+    return sameIds.length === recipe.required_ids.length;
   });
 
-  if (matchingRecipes.length === -1) {
+  const matchingRecipesByCountsAndIds = matchingRecipesByIds.filter((recipe) => {
+    for (let item of items) {
+      const idInRecipe = recipe.required_ids.indexOf(item.id);
+      if (recipe.required_amounts[idInRecipe] !== item.count) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  if (matchingRecipesByCountsAndIds.length === -1) {
     console.log('no matching recipes');
     return null;
   }
 
-  return matchingRecipes[0];
+  return matchingRecipesByCountsAndIds[0];
 };
