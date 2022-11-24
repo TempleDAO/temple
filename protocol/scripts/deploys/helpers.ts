94e7cac3f58885dd42bc4cf76d1ff2c652bc3aa2
punch <117326984+onetxpunch@users.noreply.github.com>
2022-11-23 20:18:23 -0500

    fix conflict

--- protocol/scripts/deploys/helpers.ts
+++ protocol/scripts/deploys/helpers.ts
@@ -147,15 +147,12 @@ export const DEPLOYED_CONTRACTS: { [key: string]: DeployedContracts } = {
     TEMPLE_TEAM_EPOCH_5: '0x32fbd318e0c029bfa6c6088196f184ca2e3fbdd1',
     TEMPLE_TEAM_EPOCH_6: '0x91ad65e053ae98b4fbab84fc38b7bddd17c32cda',
     TEMPLE_TEAM_EPOCH_7: '0x8cded928006feb238617fa5f7b04abeefcde36bf',
-<<<<<<< HEAD
     TEMPLE_TEAM_EPOCH_8: '0x008eCB3E53024628a5A8BbE0b142329791ad6f51',
     TEMPLE_TEAM_EPOCH_9: '0x5461d7Cd3eEB184a83c5a1678335D72ccaf04818',
-=======
 
     TEMPLE_TEAM_PAYMENTS_IMPLEMENTATION: '',
     TEMPLE_TEAM_PAYMENTS_FACTORY: '',
 
->>>>>>> 3b5d200 (update constructor, event tests, deploy scripts)
     TEMPLE_V2_FRAX_PAIR: '0x6021444f1706f15465bEe85463BCc7d7cC17Fc03',
     TEMPLE_V2_FEI_PAIR: '0xf994158766e0a4E64c26feCE675186f489EC9107',
     TEMPLE_V2_ROUTER: '0x98257c876ace5009e7b97843f8c71b3ae795c71e',
@@ -186,15 +183,12 @@ export const DEPLOYED_CONTRACTS: { [key: string]: DeployedContracts } = {
     TEMPLE_TEAM_EPOCH_5: process.env.TEMPLE_TEAM_EPOCH_5 || '',
     TEMPLE_TEAM_EPOCH_6: process.env.TEMPLE_TEAM_EPOCH_6 || '',
     TEMPLE_TEAM_EPOCH_7: process.env.TEMPLE_TEAM_EPOCH_7 || '',
-<<<<<<< HEAD
     TEMPLE_TEAM_EPOCH_8: process.env.TEMPLE_TEAM_EPOCH_8 || '',
     TEMPLE_TEAM_EPOCH_9: process.env.TEMPLE_TEAM_EPOCH_9 || '',
-=======
 
     TEMPLE_TEAM_PAYMENTS_IMPLEMENTATION: '',
     TEMPLE_TEAM_PAYMENTS_FACTORY: '',
 
->>>>>>> 3b5d200 (update constructor, event tests, deploy scripts)
     TEMPLE_V2_FRAX_PAIR: process.env.TEMPLE_V2_FRAX_PAIR || '',
     TEMPLE_V2_FEI_PAIR: process.env.TEMPLE_V2_FEI_PAIR || '',
     TEMPLE_V2_ROUTER: process.env.TEMPLE_V2_ROUTER || '',
