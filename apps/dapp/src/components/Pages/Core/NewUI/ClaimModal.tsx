414be2897709990ef43cb492f50b22d47cced55d
Nicho <nichosystem@protonmail.com>
2023-01-30 15:32:31 -0600

    Update early vault withdraw modal

--- apps/dapp/src/components/Pages/Core/NewUI/ClaimModal.tsx
+++ apps/dapp/src/components/Pages/Core/NewUI/ClaimModal.tsx
@@ -10,10 +10,8 @@ import { BigNumber } from 'ethers';
 import { useWithdrawFromVault } from 'hooks/core/use-withdraw-from-vault';
 import { VaultButton } from '../VaultPages/VaultContent';
 import { useTokenContractAllowance } from 'hooks/core/use-token-contract-allowance';
-import { VaultBalance } from 'hooks/core/use-vault-group-token-balance';
 import env from 'constants/env';
 import _ from 'lodash';
-import { ContinuousColorLegend } from 'react-vis';
 
 interface IProps {
   isOpen: boolean;
@@ -22,7 +20,6 @@ interface IProps {
 
 const EMPTY_CLAIM_STATE = {
   claimSubvaultAddress: '',
-  isEarly: false,
   claimAmount: '',
 };
 
@@ -32,40 +29,37 @@ export const ClaimModal: React.FC<IProps> = ({ isOpen, onClose }) => {
     vaultGroups: { vaultGroups, isLoading: vaultGroupsIsLoading },
     refreshVaultBalance,
   } = useVaultContext();
-
-  const { withdrawFromVault: withdrawRequest, withdrawEarly: earlyWithdrawRequest } = useWithdrawFromVault(
-    '',
-    async () => {
-      await refreshVaultBalance(claimState.claimSubvaultAddress);
-      // AnalyticsService.captureEvent(AnalyticsEvent.Vault.Claim, { name: vault.id, amount });
-      clearClaimState();
-    }
-  );
-
-  const [withdrawFromVault, { isLoading: withdrawIsLoading, error: withdrawError }] = withdrawRequest;
+  const { withdrawEarly: earlyWithdrawRequest } = useWithdrawFromVault('', async () => {
+    await refreshVaultBalance(claimState.claimSubvaultAddress);
+    // AnalyticsService.captureEvent(AnalyticsEvent.Vault.Claim, { name: vault.id, amount });
+    setClaimState(EMPTY_CLAIM_STATE);
+  });
   const [earlyWithdraw, { isLoading: earlyWithdrawIsLoading, error: earlyWithdrawError }] = earlyWithdrawRequest;
-
   const [assumedActiveVaultGroup, setAssumedActiveVaultGroup] = useState({} as VaultGroup);
 
+  // Initialize assumedActiveVaultGroup, claimState, zeroVaultBalance
   useEffect(() => {
     if (!balancesIsLoading && !vaultGroupsIsLoading) {
-      setAssumedActiveVaultGroup(vaultGroups[0]);
-      const nonZeroBalances = getVaultBalances(vaultGroups[0]?.vaults).filter(vb => vb);
-      setZeroVaultBalance(nonZeroBalances.length === 0);
+      const vaultGroup = vaultGroups[0];
+      setAssumedActiveVaultGroup(vaultGroup);
+      const initialVault = vaultGroup.vaults[0];
+      setClaimState({
+        claimSubvaultAddress: initialVault.id,
+        claimAmount: formatBigNumber(balances[vaultGroup.id][initialVault.id].balance || ZERO),
+      });
     }
-  }, [balancesIsLoading, vaultGroupsIsLoading, vaultGroups]);
+  }, [balancesIsLoading, balances, vaultGroupsIsLoading, vaultGroups]);
 
   const [claimState, setClaimState] = useState(EMPTY_CLAIM_STATE);
-  const [zeroVaultBalance, setZeroVaultBalance] = useState(false);
 
-  const claimAmountHandler = (contract: string, value: BigNumber, isEarly: boolean) => {
+  const claimAmountHandler = (contract: string, value: BigNumber) => {
     setClaimState({
       claimSubvaultAddress: contract,
       claimAmount: formatBigNumber(value),
-      isEarly,
     });
   };
 
+  // Early withdraw allowance hook
   const [
     { allowance: earlyWithdrawAllowance, isLoading: earlyWithdrawAllowanceIsLoading },
     increaseEarlyWithdrawAllowance,
@@ -74,95 +68,74 @@ export const ClaimModal: React.FC<IProps> = ({ isOpen, onClose }) => {
     env.contracts.vaultEarlyExit
   );
 
-  const buttonIsDisabled =
-    (claimState.isEarly && (earlyWithdrawIsLoading || !claimState.claimAmount)) ||
-    (!claimState.isEarly && (withdrawIsLoading || !claimState.claimAmount));
-
-  const clearClaimState = () => {
-    setClaimState(EMPTY_CLAIM_STATE);
-  };
-
   const formatErrorMessage = (errorMessage: string) => {
     const boundary = errorMessage.indexOf('(');
-    if (boundary > 0) {
-      return errorMessage.substring(0, boundary - 1);
-    }
+    if (boundary > 0) return errorMessage.substring(0, boundary - 1);
     return errorMessage.substring(0, 20).concat('...');
   };
 
-  const emptyBalance = (inputBalance: VaultBalance) => {
-    return !inputBalance.balance || inputBalance.balance?.lte(ZERO);
-  };
-
+  // Return component for all subvault balances
   const getVaultBalances = (vaults: Vault[] | undefined) => {
     if (!vaults) return [];
     return vaults.map((vault) => {
       const vaultGroupBalances = balances[vaultGroups[0].id];
       const vaultBalance = vaultGroupBalances[vault.id] || {};
-      const unlockValue = isDate(vault.unlockDate) ? format(vault.unlockDate as Date, 'MMM do') : 'now';
-      const isEarly = unlockValue !== 'now';
 
-      if (emptyBalance(vaultBalance)) {
-        // empty balance should not render
-        return;
-      }
+      // Empty balances won't render
+      // if (!vaultBalance.balance || vaultBalance.balance?.lte(ZERO)) return;
 
       return (
-        <SubvaultRow key={vault.id}>
-          <SubvaultCell>Subvault {vault.label}</SubvaultCell>
-          <SubvaultCell>
-            <ClaimAmount onClick={() => claimAmountHandler(vault.id, vaultBalance.balance || ZERO, isEarly)}>
-              Claim {formatTemple(vaultBalance.balance)} $T
+        <div key={vault.id}>
+          <div>
+            Subvault {vault.label}:{' '}
+            <ClaimAmount
+              isActive={vault.id == claimState.claimSubvaultAddress}
+              onClick={() => claimAmountHandler(vault.id, vaultBalance.balance || ZERO)}
+            >
+              {formatTemple(vaultBalance.balance)} TEMPLE
             </ClaimAmount>
-          </SubvaultCell>
-        </SubvaultRow>
+          </div>
+        </div>
       );
     });
   };
+
+  console.log(claimState.claimAmount);
 
   return (
     <>
       <Popover isOpen={isOpen} onClose={onClose} closeOnClickOutside showCloseButton>
         <ClaimContainer>
-          <ClaimTitle>Claim from vaults</ClaimTitle>
-          {zeroVaultBalance ? (
-            <ClaimSubtitle>Nothing to claim.</ClaimSubtitle>
-          ) : (
-            <>
-              <ClaimSubtitle>Select a subvault to claim:</ClaimSubtitle>
-              <SubvaultContainer>{getVaultBalances(assumedActiveVaultGroup?.vaults)}</SubvaultContainer>
-            </>
-          )}
-          <TempleAmountContainer>
-            <Temple>$TEMPLE</Temple>
-            <TempleAmount>{claimState.claimAmount ? formatTemple(Number(claimState.claimAmount)) : '0.00'}</TempleAmount>
-          </TempleAmountContainer>
-          {!!withdrawError && (
-            <ErrorLabel>{formatErrorMessage(withdrawError.message) || 'Something went wrong'}</ErrorLabel>
-          )}
+          <ClaimTitle>Claim from Vaults</ClaimTitle>
+          <ClaimSubtitle>Select Vault for Withdrawal</ClaimSubtitle>
+          <SubvaultContainer>{getVaultBalances(assumedActiveVaultGroup?.vaults)}</SubvaultContainer>
+
           {!!earlyWithdrawError && (
             <ErrorLabel>{formatErrorMessage(earlyWithdrawError.message) || 'Something went wrong'}</ErrorLabel>
           )}
-          {earlyWithdrawAllowance !== 0 && (
+
+          {Number(claimState.claimAmount) === 0 ? (
             <ClaimButton
-              label={'Claim'}
-              disabled={buttonIsDisabled}
+              label={`Nothing to Claim`}
+              disabled={true}
               onClick={async () => {
-                if (claimState.isEarly) {
-                  await earlyWithdraw(claimState.claimSubvaultAddress, claimState.claimAmount);
-                } else {
-                  await withdrawFromVault(claimState.claimSubvaultAddress, claimState.claimAmount);
-                }
+                await earlyWithdraw(claimState.claimSubvaultAddress, claimState.claimAmount);
               }}
             />
-          )}
-          {earlyWithdrawAllowance === 0 && (
+          ) : earlyWithdrawAllowance === 0 ? (
             <ClaimButton
-              label={'Approve'}
-              marginTop={withdrawError ? '0.5rem' : '3.5rem'}
+              label={'Approve Early Withdraw'}
               disabled={earlyWithdrawAllowanceIsLoading}
               onClick={async () => {
                 await increaseEarlyWithdrawAllowance();
+              }}
+            />
+          ) : (
+            <ClaimButton
+              label={`Redeem ${formatTemple(Number(claimState.claimAmount))} TEMPLE`}
+              disabled={earlyWithdrawIsLoading || !claimState.claimAmount}
+              onClick={async () => {
+                await earlyWithdraw(claimState.claimSubvaultAddress, claimState.claimAmount);
               }}
             />
           )}
@@ -178,34 +151,26 @@ const ErrorLabel = styled.span`
   margin: 1rem 0;
 `;
 
-const ClaimAmount = styled.div`
+const ClaimAmount = styled.span<{ isActive: boolean }>`
   text-decoration: underline;
-  color: #bd7b4f;
-  font-weight: bold;
+  color: ${({ isActive, theme }) => (isActive ? theme.palette.brandLight : theme.palette.brand75)};
+  font-weight: ${(props) => (props.isActive ? `bold` : `normal`)};
   cursor: pointer;
-`;
-
-const SubvaultCell = styled.div`
-  color: #bd7b4f;
-  padding: 5px;
-`;
-
-const SubvaultRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 5px;
+  &hover {
+    color: ${({ theme }) => theme.palette.brandLight};
+  }
 `;
 
 const SubvaultContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 10px;
+  justify-content: center;
+  padding-bottom: 1.5rem;
+  gap: 0.5rem;
 `;
 
 const ClaimButton = styled(VaultButton)`
-  width: 100px;
-  height: 60px;
-  background: linear-gradient(180deg, #353535 45.25%, #101010 87.55%);
+  background: ${({ theme }) => theme.palette.gradients.dark};
   border: 1px solid #95613f;
   box-shadow: 0px 0px 20px rgba(222, 92, 6, 0.4);
   border-radius: 0.75rem;
@@ -213,59 +178,27 @@ const ClaimButton = styled(VaultButton)`
   font-size: 1rem;
   letter-spacing: 0.1rem;
   text-transform: uppercase;
-  color: #ffdec9;
-  margin: auto;
-`;
-
-const TempleAmount = styled.div`
-  font-size: 36px;
-  display: flex;
-  align-items: center;
-  text-align: right;
-  color: #ffdec9;
-  padding: 20px;
-`;
-
-const Temple = styled.div`
-  font-style: normal;
-  font-size: 36px;
-  display: flex;
-  letter-spacing: 0.05em;
-  color: #ffdec9;
-  margin-right: auto;
+  margin-top: 0px;
 `;
 
 const ClaimSubtitle = styled.div`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 20px;
-  align-items: center;
-  letter-spacing: 0.1em;
-  color: #bd7b4f;
+  font-size: 1.15rem;
+  letter-spacing: 0.05rem;
+  padding-bottom: 0.5rem;
 `;
 
 const ClaimTitle = styled.div`
-  font-size: 24px;
-  line-height: 28px;
-  display: flex;
-  align-items: center;
-  color: #bd7b4f;
-  padding-bottom: 20px;
-`;
-
-const TempleAmountContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 20px;
-  margin-bottom: 20px;
+  font-size: 1.5rem;
+  padding-bottom: 1rem;
+  color: ${({ theme }) => theme.palette.brandLight};
 `;
 
 const ClaimContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  color: ${({ theme }) => theme.palette.brand};
   width: 350px;
 `;
 
