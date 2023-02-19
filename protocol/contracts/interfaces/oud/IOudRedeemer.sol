fdb618a4b2f548618ae345517d2e52f3f94638df
siftashead <siftashead@protonmail.com>
2023-02-19 16:54:16 +1100

    fix:natspec added to interface

--- protocol/contracts/interfaces/oud/IOudRedeemer.sol
+++ protocol/contracts/interfaces/oud/IOudRedeemer.sol
@@ -5,16 +5,26 @@ pragma solidity ^0.8.17;
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol';
 
-/// @notice An ERC20 token which can be minted/burnt by approved accounts
-interface IOudRedeemer {
 
+/**
+ * @title Allows for the redemption of Oud + a stable coin for Temple at the Temple Treasury Price index
+ */
+interface IOudRedeemer {
+  //  @notice Returns the index at which 'stable' token is required to mint Temple
   function treasuryPriceIndex() external view returns (uint256);
 
-  function TPI_PRECISION() external pure returns (uint256); // 9880 == $0.988, precision = 4
+  // @dev Decimal precision for 'tpi', 9880 == $0.988, precision = 4
+  function TPI_PRECISION() external pure returns (uint256);
 
+  // @dev Allows the tpi to be adjusted.
+  // @param 'value' is the new TPI
   function setTreasuryPriceIndex(uint256 value) external;
-    
+
   function setStableCoin(address newStable) external;
 
+  // @notice Redeems Oud and Stable token (@ tpi) for Temple
+  // @dev requires allowances for Oud and Stable Token
+  // @param 'oudAmount' The amount of Oud to be redeemed for Temple
+  // @returns 'templeAmount' The amount of Temple that has been minted to sender.
   function redeem(uint256 oudAmount) external returns (uint256 templeAmount);
 }
