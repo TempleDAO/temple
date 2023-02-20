8f466dfaf4fcc70fc1f29c24fb79fb2ddf9ef9dc
siftashead <siftashead@protonmail.com>
2023-02-20 20:35:30 +1100

    fix: names and locations

--- protocol/contracts/interfaces/templeLineOfCredit/ITempleLineOfCredit.sol
+++ protocol/contracts/interfaces/templeLineOfCredit/ITempleLineOfCredit.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.8.17;
 // SPDX-License-Identifier: AGPL-3.0-or-later
-// Oud (protocol/contracts/interfaces/oud/IOudtoken.sol)
+// Temple Line of Credit Interface (/interfaces/templeLineOfCredit/ITempleLineOfCredit.sol)
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol';
 
 /// @notice Helper interface for calling the TempleLineOfCredit contract
-interface Oud__ITempleLineOfCredit {
+interface ITempleLineOfCredit {
 
   function depositReserve(IERC20 debtToken, address account, uint256 amount) external;
 
