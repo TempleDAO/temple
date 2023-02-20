988bc1e11b530b12b0227f7fb3a8ccfc14d08bf7
siftashead <siftashead@protonmail.com>
2023-02-20 21:02:46 +1100

    fix: IOudRedeemer removed addresses from quote and fixed natspec

--- protocol/contracts/interfaces/oud/IOudRedeemer.sol
+++ protocol/contracts/interfaces/oud/IOudRedeemer.sol
@@ -9,14 +9,14 @@ import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft
  * @title Allows for the redemption of Oud + a stable coin for Temple at the Temple Treasury Price index
  */
 interface IOudRedeemer {
-  //  @notice Returns the index at which 'stable' token is required to mint Temple
+  ///  @notice Returns the index at which 'stable' token is required to mint Temple
   function treasuryPriceIndex() external view returns (uint256);
 
-  // @dev Decimal precision for 'tpi', 9880 == $0.988, precision = 4
+  /// @dev Decimal precision for 'tpi', 9880 == $0.988, precision = 4
   function TPI_PRECISION() external pure returns (uint256);
 
-  // @dev Allows the tpi to be adjusted.
-  // @param 'value' is the new TPI
+  /// @dev Allows the tpi to be adjusted.
+  /// @param value is the new TPI
   function setTreasuryPriceIndex(uint256 value) external;
 
   function setStableCoin(address newStable) external;
@@ -25,12 +25,8 @@ interface IOudRedeemer {
    * @notice Provides a quote for 'oudAmount' with relevant token addresses:
    *  'oudAmount' provided, 'stable' token required, 'templeAmount' that will be minted to caller
    * @param oudAmount amount of Oud that will be redeemed
-   * @return _oudAmount amount of Oud that wil be redeemed
-   * @return _oudTokenAddress address of the Oud token
    * @return _stableAmount amount of Stable token required at tpi
-   * @return _stableAddress address of the Stable token in use
    * @return _templeAmount amount of Temple that will be minted
-   * @return _templeTokenAddress address of the Temple token
    */
   function getQuote(
     uint256 oudAmount
@@ -38,17 +34,15 @@ interface IOudRedeemer {
     external
     view
     returns (
-      uint256 _oudAmount,
-      address _oudTokenAddress,
       uint256 _stableAmount,
-      address _stableAddress,
-      uint256 _templeAmount,
-      address _templeTokenAddress
+      uint256 _templeAmount
     );
 
-  // @notice Redeems Oud and Stable token (@ tpi) for Temple
-  // @dev requires allowances for Oud and Stable Token
-  // @param 'oudAmount' The amount of Oud to be redeemed for Temple
-  // @returns 'templeAmount' The amount of Temple that has been minted to sender.
+  /**
+  * @notice Redeems Oud and Stable token (@ tpi) for Temple
+  * @dev requires allowance Stable Token
+  * @param oudAmount The amount of Oud to be redeemed for Temple
+  * @return templeAmount The amount of Temple that has been minted to sender.
+  */
   function redeem(uint256 oudAmount) external returns (uint256 templeAmount);
 }
