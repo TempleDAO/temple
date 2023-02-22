b40d209d3e7e773c5a8c5675cfb036e0d1fbe432
zelki <zelki.codes@protonmail.com>
2023-02-22 13:12:43 +0000

    fix: ramos metrics queries

--- apps/dapp/src/hooks/core/subgraph.tsx
+++ apps/dapp/src/hooks/core/subgraph.tsx
@@ -109,7 +109,7 @@ const createVaultGroupQuery = (vaultGroupId: string, walletAddress = ''): SubGra
 
 export const createRAMOSDailyMetricsQuery = (): SubGraphQuery => ({
   query: `{
-    metricDailySnapshots(orderDirection: desc, orderBy: timestamp) {
+    metricDailySnapshots(orderDirection: desc, orderBy: timestamp, first: 365) {
       templeBurned
       totalProfitUSD
       treasuryPriceIndexUSD
@@ -121,7 +121,7 @@ export const createRAMOSDailyMetricsQuery = (): SubGraphQuery => ({
 
 export const createRAMOSHourlyMetricsQuery = (): SubGraphQuery => ({
   query: `{
-    metricHourlySnapshots(orderDirection: desc, orderBy: timestamp first: 24) {
+    metricHourlySnapshots(orderDirection: desc, orderBy: timestamp, first: 24) {
       templeBurned
       totalProfitUSD
       treasuryPriceIndexUSD
