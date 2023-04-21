1abe55a49941a064f50656a88470182c1089c865
Nicho <nichosystem@protonmail.com>
2023-04-20 19:51:26 -0500

    Fix SVG naming

--- apps/dapp/src/components/Pages/Core/NewUI/HomeInput.tsx
+++ apps/dapp/src/components/Pages/Core/NewUI/HomeInput.tsx
@@ -89,8 +89,8 @@ export const Input = ({
               {crypto.selected}
             </Ticker>
               <ChevronDownImg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
               </ChevronDownImg>
           </OptionContainer>
