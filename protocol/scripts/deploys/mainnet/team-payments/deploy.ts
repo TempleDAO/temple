0d33ca6a2d227657b9007f25552fea3346c1e8d2
frontier159 <103474701+frontier159@users.noreply.github.com>
2023-06-01 06:49:01 +1000

    Deploy Epoch17 team payments

--- protocol/scripts/deploys/mainnet/team-payments/deploy.ts
+++ protocol/scripts/deploys/mainnet/team-payments/deploy.ts
@@ -6,7 +6,7 @@ import {
   expectAddressWithPrivateKey,
   toAtto,
 } from '../../helpers';
-import snapshot from './json/epoch16.json';
+import snapshot from './json/epoch17.json';
 
 // TODO: Add command line arguments for json allocations file
 async function main() {
