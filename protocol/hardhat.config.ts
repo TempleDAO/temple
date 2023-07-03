ad6f2e08561e647bec6845af6d9cd14301fcebd5
frontier159 <103474701+frontier159@users.noreply.github.com>
2023-07-03 23:33:14 +1000

    update to 0.8.18, update slither db

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -70,7 +70,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.8.17',
+        version: '0.8.18',
         settings: {
         optimizer: {
             enabled: true,
