f39fc33a8154df537a4fbe558a2fc9de04f8fe27
frost ostrich <frost_ostrich@proton.me>
2023-07-06 06:24:48 +1000

    test: add local fork tests skeleton

---
+++ protocol/scripts/deploys/localhost/deploymentArgs/0x77AD263Cd578045105FBFC88A477CAd808d39Cf6.js
@@ -0,0 +1,9 @@
+// localhost: TEMPLE_BASE_STRATEGY=0x77AD263Cd578045105FBFC88A477CAd808d39Cf6
+// yarn hardhat verify --network localhost 0x77AD263Cd578045105FBFC88A477CAd808d39Cf6 --constructor-args scripts/deploys/localhost/deploymentArgs/0x77AD263Cd578045105FBFC88A477CAd808d39Cf6.js
+module.exports = [
+  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  "TempleBaseStrategy",
+  "0x9155497EAE31D432C0b13dBCc0615a37f55a2c87",
+  "0xE2b5bDE7e80f89975f7229d78aD9259b2723d11F"
+];
---
+++ protocol/scripts/deploys/localhost/deploymentArgs/0xce830DA8667097BB491A70da268b76a081211814.js
@@ -0,0 +1,9 @@
+// localhost: TLC_CIRCUIT_BREAKER_TEMPLE=0xce830DA8667097BB491A70da268b76a081211814
+// yarn hardhat verify --network localhost 0xce830DA8667097BB491A70da268b76a081211814 --constructor-args scripts/deploys/localhost/deploymentArgs/0xce830DA8667097BB491A70da268b76a081211814.js
+module.exports = [
+  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  93600,
+  13,
+  "100000000000000000000000"
+];
