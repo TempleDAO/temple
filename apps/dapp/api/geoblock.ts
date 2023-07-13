d8f18addf9ca250ea083c8a1a192abbcd07da95b
Nicho <nichosystem@protonmail.com>
2023-07-13 13:02:37 -0500

    Try test boilerplate

--- apps/dapp/api/geoblock.ts
+++ apps/dapp/api/geoblock.ts
@@ -1,13 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const BLOCKED_ISO_COUNTRIES = ['USA', 'PRK', 'IRN'];
+// const BLOCKED_ISO_COUNTRIES = ['USA', 'PRK', 'IRN'];
 
+// export default function handler(request: VercelRequest, response: VercelResponse) {
+//   console.log(request.headers);
+//   console.log(request.headers['x-vercel-ip-country']);
+//   console.log(BLOCKED_ISO_COUNTRIES.includes(request.headers['x-vercel-ip-country'] as string));
+//   // Send response to the user
+//   response.status(200).json({
+//     blocked: BLOCKED_ISO_COUNTRIES.includes(request.headers['x-vercel-ip-country'] as string),
+//   });
+// }
 export default function handler(request: VercelRequest, response: VercelResponse) {
-  console.log(request.headers);
-  console.log(request.headers['x-vercel-ip-country']);
-  console.log(BLOCKED_ISO_COUNTRIES.includes(request.headers['x-vercel-ip-country'] as string));
-  // Send response to the user
   response.status(200).json({
-    blocked: BLOCKED_ISO_COUNTRIES.includes(request.headers['x-vercel-ip-country'] as string),
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
   });
 }
