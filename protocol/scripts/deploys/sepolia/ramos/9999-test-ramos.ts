5a1ed225ffbd6f9aaac6259e11a237e0df71a421
frontier159 <103474701+frontier159@users.noreply.github.com>
2023-07-16 18:38:48 +1000

    Remove ramos liquidity

--- protocol/scripts/deploys/sepolia/ramos/9999-test-ramos.ts
+++ protocol/scripts/deploys/sepolia/ramos/9999-test-ramos.ts
@@ -4,17 +4,27 @@ import {
   ensureExpectedEnvvars,
   mine,
 } from "../../helpers";
-import { connectToContracts } from "../contract-addresses";
+import { ContractInstances, connectToContracts } from "../contract-addresses";
+import { BigNumber } from "ethers";
+
+async function addLiquidity(TEMPLE_V2_INSTANCES: ContractInstances, amount: BigNumber) {
+    const quote = await TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.callStatic.proportionalAddLiquidityQuote(amount, "100");
+    await mine(TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.addLiquidity(quote.requestData, {gasLimit:5000000}));
+}
+
+async function removeLiquidity(TEMPLE_V2_INSTANCES: ContractInstances, amount: BigNumber) {
+    const quote = await TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.callStatic.proportionalRemoveLiquidityQuote(amount, "100");
+    await mine(TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.removeLiquidity(quote.requestData, amount, {gasLimit:5000000}));
+}
 
 async function main() {
     ensureExpectedEnvvars();
     const [owner] = await ethers.getSigners();
     const TEMPLE_V2_INSTANCES = connectToContracts(owner);
 
-    const amount = ethers.utils.parseEther("1000000");
-    const quote = await TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.callStatic.proportionalAddLiquidityQuote(amount, "100");
+    await addLiquidity(TEMPLE_V2_INSTANCES, ethers.utils.parseEther("1000000"));
+    await removeLiquidity(TEMPLE_V2_INSTANCES, ethers.utils.parseEther("200000"));
 
-    await mine(TEMPLE_V2_INSTANCES.STRATEGIES.RAMOS_STRATEGY.INSTANCE.addLiquidity(quote.requestData, {gasLimit:5000000}));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
