f269f5a1647f1e411e9afe4572205ebe8dd7d817
frost ostrich <frost_ostrich@proton.me>
2023-07-18 16:51:47 +1000

    chore: move from ../scripts/v2/sepolia/* to ../scripts/sepolia/v2/*

---
+++ protocol/scripts/deploys/sepolia/v2/core/05-trv-dusd.ts
@@ -0,0 +1,35 @@
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+import { TempleDebtToken__factory } from '../../../../../typechain';
+import {
+  deployAndMine,
+  ensureExpectedEnvvars,
+} from '../../../helpers';
+import { getDeployedContracts } from '../contract-addresses';
+
+async function main() {
+  ensureExpectedEnvvars();
+  const [owner] = await ethers.getSigners();
+
+  const dUsdDebtTokenFactory = new TempleDebtToken__factory(owner);
+  await deployAndMine(
+    'CORE.TREASURY_RESERVES_VAULT.D_USD_TOKEN',
+    dUsdDebtTokenFactory,
+    dUsdDebtTokenFactory.deploy,
+    "Temple Debt USD",
+    "dUSD",
+    await owner.getAddress(),
+    await owner.getAddress(),
+    ethers.utils.parseEther("0.034304803691990293"), // 34.9% APR or ~34.304%APY
+  )
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ protocol/scripts/deploys/sepolia/v2/gnosis-strategy-1/01-gnosis-safe-circuit-breakers-dai.ts
@@ -0,0 +1,33 @@
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+import { TempleCircuitBreakerAllUsersPerPeriod__factory } from '../../../../../typechain';
+import {
+  deployAndMine,
+  ensureExpectedEnvvars,
+} from '../../../helpers';
+
+async function main() {
+  ensureExpectedEnvvars();
+  const [owner] = await ethers.getSigners();
+
+  const circuitBreakerFactory = new TempleCircuitBreakerAllUsersPerPeriod__factory(owner);
+  await deployAndMine(
+    'STRATEGIES.GNOSIS_SAFE_STRATEGY1.CIRCUIT_BREAKERS.DAI',
+    circuitBreakerFactory,
+    circuitBreakerFactory.deploy,
+    await owner.getAddress(),
+    await owner.getAddress(),
+    60*60*26, // 26 hours
+    13, // no of buckets
+    ethers.utils.parseEther("50000000"), // cap per bucket
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
