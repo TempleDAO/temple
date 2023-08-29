6cc2e86d2c824745c304b939774f3259bf1c46d5
frontier159 <103474701+frontier159@users.noreply.github.com>
2023-08-29 15:40:03 +1000

    updated safecast to align to upcoming OZ, removed unused fns

--- protocol/contracts/common/SafeCast.sol
+++ protocol/contracts/common/SafeCast.sol
@@ -5,28 +5,10 @@ pragma solidity 0.8.19;
 library SafeCast {
     error Overflow(uint256 amount);
 
-    function encodeUInt32(uint256 amount) internal pure returns (uint32 downcasted) {
-        downcasted = uint32(amount);
-        if (downcasted != amount) revert Overflow(amount);
-    }
-
-    function encodeUInt64(uint256 amount) internal pure returns (uint64 downcasted) {
-        downcasted = uint64(amount);
-        if (downcasted != amount) revert Overflow(amount);
-    }
-
-    function encodeUInt80(uint256 amount) internal pure returns (uint80 downcasted) {
-        downcasted = uint80(amount);
-        if (downcasted != amount) revert Overflow(amount);
-    }
-
-    function encodeUInt96(uint256 amount) internal pure returns (uint96 downcasted) {
-        downcasted = uint96(amount);
-        if (downcasted != amount) revert Overflow(amount);
-    }
-    
-    function encodeUInt128(uint256 amount) internal pure returns (uint128 downcasted) {
-        downcasted = uint128(amount);
-        if (downcasted != amount) revert Overflow(amount);
+    function encodeUInt128(uint256 amount) internal pure returns (uint128) {
+        if (amount > type(uint128).max) {
+            revert Overflow(amount);
+        }
+        return uint128(amount);
     }
 }
