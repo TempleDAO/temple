984d8f894a6a0070ec010ed0dab81bbecb06087e
frost ostrich <frost_ostrich@proton.me>
2023-10-17 13:29:59 +1000

    feat: strategies transaction history table with pagination

--- apps/dapp/src/components/Pages/Core/DappPages/Dashboard/Table/TxnHistoryTable.tsx
+++ apps/dapp/src/components/Pages/Core/DappPages/Dashboard/Table/TxnHistoryTable.tsx
@@ -1,43 +1,142 @@
-import { useEffect, useState } from 'react';
+import { SubGraphResponse } from 'hooks/core/types';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-type TableRow = {
-  date: string;
-  debit: number;
-  credit: number;
-};
+import { fetchGenericSubgraph } from 'utils/subgraph';
+import { TxHistoryFilterType } from '.';
+import { DashboardType } from '../DashboardContent';
+import { format } from 'date-fns';
+import { TableRow, TxnDataTable } from './TxnDataTable';
+import { PaginationControls } from './TxnPaginationControl';
 
 type Props = {
-  filter: string;
+  dashboardType: DashboardType;
+  filter: TxHistoryFilterType;
 };
 
-const TxnHistoryTable = ({ filter }: Props) => {
+type StrategyType = 'TlcStrategy' | 'DsrBaseStrategy' | 'RamosStrategy' | 'TempleBaseStrategy' | 'All';
+
+type Transactions = {
+  hash: string;
+  amount: string;
+  amountUsd: string;
+  id: string;
+  from: string;
+  to: string;
+  kind: 'Repay' | 'Borrow';
+  timestamp: string;
+}[];
+
+type StrategyTxns = {
+  name: StrategyType;
+  id: string;
+  transactionCount: number;
+  transactions: Transactions;
+}[];
+
+type Meta = {
+  block: {
+    number: number;
+  };
+};
+
+type FetchTxnsResponse = SubGraphResponse<{ strategies: StrategyTxns; _meta: Meta }>;
+
+const TxnHistoryTable = ({ dashboardType, filter }: Props) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [data, setData] = useState<TableRow[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [blockNumber, setBlockNumber] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const lastRow = currentPage * rowsPerPage;
-  const firstRow = lastRow - rowsPerPage;
-  const totalPages = Math.ceil(data.length / rowsPerPage);
-  const currentData = data.slice(firstRow, lastRow);
-
-  // Fetch data
+  const restartTable = useCallback(async()=> {
+    setData([]);
+    setCurrentPage(1);
+    setBlockNumber(0);
+    setRowsPerPage(10);
+    setTotalPages(0);
+  }, []);
+  // Set default values TotalPages & BlockNumber on start
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Replace with your API endpoint
-        // const response = await fetch('https://api.yourdomain.com/data-endpoint');
-        // const result = await response.json();
+    (async ()=> {
+      await restartTable();
+      const strategyType = dashboardTypeToStrategyType(dashboardType);
+      const whereQuery = strategyType === 'All' ? `` : `( where: {name: "${strategyType}"} )`;
+      const { data: res } = await fetchGenericSubgraph<FetchTxnsResponse>(
+        'https://api.studio.thegraph.com/query/520/v2-sepolia/version/latest',
+        `{
+            strategies${whereQuery} {
+              transactionCount
+            }
+            _meta {
+              block {
+                number
+              }
+            }
+          }`
+      );
+      if (!res) return;
+      let txCountTotal = 0;
+      res.strategies.map(s=> txCountTotal += s.transactionCount);
+      setTotalPages(Math.ceil(txCountTotal / rowsPerPage));
+      setBlockNumber(res._meta.block.number);
+    })();
+  }, [dashboardType, rowsPerPage, restartTable]);
 
-        // setData(result.data);
+  const fetchTransactions = useCallback(
+    async (strategyType: StrategyType): Promise<Transactions> => {
+      let whereQuery = ``;
+      whereQuery = strategyType === 'All' ? `` : `( where: {name: "${strategyType}"} )`;
+      if (blockNumber > 0) {
+        whereQuery =
+          strategyType === 'All'
+            ? `( block: { number: ${blockNumber} } )`
+            : `( block: { number: ${blockNumber} } where: {name: "${strategyType}"})`;
+      }
+      const paginationQuery = `skip: ${(currentPage - 1) * rowsPerPage} first: ${rowsPerPage}`;
+      const { data: res } = await fetchGenericSubgraph<FetchTxnsResponse>(
+        'https://api.studio.thegraph.com/query/520/v2-sepolia/version/latest',
+        `{
+          strategies${whereQuery} {
+            name
+            id
+            transactions(orderBy: timestamp, orderDirection: desc ${paginationQuery}) {
+              hash
+              amount
+              amountUSD
+              id
+              from
+              kind
+              timestamp
+            }
+          }
+        }`
+      );
+      if (!res) return [];
+      const txns: Transactions = [];
+      for (const strategy of res.strategies) {
+        strategy.transactions.map((t) => txns.push(t));
+      }
+      return txns;
+    },
+    [blockNumber, currentPage, rowsPerPage]
+  );
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      try {
+        const txns = await fetchTransactions(dashboardTypeToStrategyType(dashboardType));
         // TODO: USE filter
-        console.log('filter', filter);
         setData(
-          new Array(100).fill(100).map((e, i) => {
+          txns.map((tx) => {
+            const amount = Number(Number(tx.amount).toFixed(2));
             return {
-              date: '2021-09-01',
-              debit: i,
-              credit: 100,
+              date: format(new Date(Number(tx.timestamp) * 1000), 'yyyy-MM-dd'),
+              dToken: 'dUsd', // TODO: update dynamically from strategy
+              borrow: tx.kind === 'Borrow' ? amount : 0,
+              repay: tx.kind === 'Repay' ? amount : 0,
+              txHash: tx.id,
             };
           })
         );
@@ -45,62 +144,37 @@ const TxnHistoryTable = ({ filter }: Props) => {
         console.error('Error fetching data: ', error);
         // Handle error appropriately
       }
-    };
-    fetchData();
-  }, [filter]);
+      setIsLoading(false);
+    })();
+  }, [filter, dashboardType, fetchTransactions]);
 
   return (
     <TableContainer>
-      <PaginationControls>
-        {[...Array(totalPages)].map((_, index) => (
-          <PageLink
-            selected={currentPage === index + 1}
-            key={index}
-            onClick={() => setCurrentPage(index + 1)}
-            disabled={currentPage === index + 1}
-          >
-            {index + 1}
-          </PageLink>
-        ))}
-
-        <PageLink selected onClick={() => setCurrentPage(1)} disabled={currentPage === 1}>
-          First
-        </PageLink>
-        <PageLink selected onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} disabled={currentPage === 1}>
-          Prev
-        </PageLink>
-        <PageLink
-          selected
-          onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </PageLink>
-        <PageLink selected onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages}>
-          Last
-        </PageLink>
-      </PaginationControls>
-
-      <DataTable>
-        <thead>
-          <tr>
-            <TableHeader>Date</TableHeader>
-            <TableHeader>Debit</TableHeader>
-            <TableHeader>Credit</TableHeader>
-          </tr>
-        </thead>
-        <tbody>
-          {currentData.map((row, index) => (
-            <DataRow key={index}>
-              <DataCell>{row.date}</DataCell>
-              <DataCell>{row.debit}</DataCell>
-              <DataCell>{row.credit}</DataCell>
-            </DataRow>
-          ))}
-        </tbody>
-      </DataTable>
+      <PaginationControls totalPages={totalPages} rowsPerPage={rowsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : data.length === 0 ? (
+        <p>No data available</p>
+      ) : (
+        <TxnDataTable dataSubset={data} />
+      )}
     </TableContainer>
   );
+};
+
+const dashboardTypeToStrategyType = (dType: DashboardType) => {
+  switch (dType) {
+    case DashboardType.TLC:
+      return 'TlcStrategy';
+    case DashboardType.RAMOS:
+      return 'RamosStrategy';
+    case DashboardType.DSR_BASE:
+      return 'DsrBaseStrategy';
+    case DashboardType.TEMPLE_BASE:
+      return 'TempleBaseStrategy';
+    default:
+      return 'All';
+  }
 };
 
 export default TxnHistoryTable;
@@ -109,45 +183,4 @@ const TableContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 70vw;
-`;
-
-type PageLinkProps = {
-  selected?: boolean;
-};
-
-const PageLink = styled.button<PageLinkProps>`
-  margin: 0 5px;
-  color: ${({ selected, theme }) => (selected ? theme.palette.brand : theme.palette.brandLight)};
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  background: none;
-  box-shadow: none;
-  border-radius: 0px;
-`;
-
-const TableHeader = styled.th`
-  text-align: left;
-  padding: 8px;
-`;
-
-const DataTable = styled.table`
-  border-collapse: collapse;
-  color: ${({ theme }) => theme.palette.brand};
-`;
-
-const DataRow = styled.tr`
-  border-top: 1px solid ${({ theme }) => theme.palette.brand};
-  border-bottom: 1px solid ${({ theme }) => theme.palette.brand};
-`;
-
-const DataCell = styled.td`
-  padding: 8px;
-  text-align: left;
-  color: ${({ theme }) => theme.palette.brandLight};
-`;
-
-const PaginationControls = styled.div`
-  text-align: right;
-  margin: 10px 0;
 `;
