93418603a2b8334ff9d3e81a67943fd33444fd95
pocin <8642344+pocin@users.noreply.github.com>
2023-11-30 10:12:47 +0100

    fix: show $ on new line

--- apps/dapp/src/components/Pages/Core/DappPages/Dashboard/Chart/V2StrategyMetricsChart.tsx
+++ apps/dapp/src/components/Pages/Core/DappPages/Dashboard/Chart/V2StrategyMetricsChart.tsx
@@ -63,7 +63,7 @@ const metricFormatters: { [k in V2SnapshotMetric]: MetricFormatter } = {
   totalMarketValueUSD: parseFloat,
 };
 
-const numberFormatter = new Intl.NumberFormat('en', { maximumFractionDigits: 0 });
+const numberFormatter = new Intl.NumberFormat('en', { maximumFractionDigits: 0, style: 'currency', currency: 'USD' });
 
 const V2StrategyMetricsChart: React.FC<{
   dashboardType: DashboardType;
@@ -71,13 +71,13 @@ const V2StrategyMetricsChart: React.FC<{
   selectedMetric: V2SnapshotMetric;
   selectedInterval: ChartSupportedTimeInterval;
 }> = ({ dashboardType, selectedMetric, selectedInterval, strategyNames }) => {
-  const formatMetricName = (name: string) => `${
-     name
-    // insert a space before all caps
-    .replace(/([A-Z][a-z])/g, ' $1')
-    // uppercase the first character
-    .replace(/^./, str=> str.toUpperCase())
-    .replace(/USD/, '')} ($)`;
+  const formatMetricName = (name: string) =>
+    `${name
+      // insert a space before all caps
+      .replace(/([A-Z][a-z])/g, ' $1')
+      // uppercase the first character
+      .replace(/^./, (str) => str.toUpperCase())
+      .replace(/USD/, '')}`;
 
   const tooltipValuesFormatter = (value: number, name: string) => [
     numberFormatter.format(value),
