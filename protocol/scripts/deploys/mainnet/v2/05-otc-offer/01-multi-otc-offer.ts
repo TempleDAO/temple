44f9dc630ee04ceb4c9420cc33268e81b1a0bdf0
princetonbishop <princetonbishop@protonmail.com>
2024-02-25 22:27:37 +0100

    mainnet deploy script

---
+++ protocol/scripts/deploys/mainnet/v2/05-otc-offer/01-multi-otc-offer.ts
@@ -0,0 +1,34 @@
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+import { MultiOtcOffer__factory } from '../../../../../typechain';
+import {
+  deployAndMine,
+  ensureExpectedEnvvars,
+} from '../../../helpers';
+import { getDeployedContracts } from '../contract-addresses';
+
+
+async function main() {
+    ensureExpectedEnvvars();
+    const [owner] = await ethers.getSigners();
+    const CORE_ADDRESSES = getDeployedContracts();
+    const initialRescuer = CORE_ADDRESSES.CORE.RESCUER_MSIG;
+    const initialExecutor = CORE_ADDRESSES.CORE.EXECUTOR_MSIG;
+    const factory = new MultiOtcOffer__factory(owner);
+    await deployAndMine(
+        'MULTI_OTC_MARKET_OFFER',
+        factory,
+        factory.deploy,
+        initialRescuer,
+        initialExecutor
+    );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
