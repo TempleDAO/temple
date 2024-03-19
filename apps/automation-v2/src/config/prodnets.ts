7bc7239c481eef7ad9b4a41c729c2889d4d076c6
frost ostrich <frost_ostrich@proton.me>
2024-03-19 10:33:14 -0500

    fix: update subgraph alchemy url

--- apps/automation-v2/src/config/prodnets.ts
+++ apps/automation-v2/src/config/prodnets.ts
@@ -10,7 +10,7 @@ const TLC_BATCH_LIQUIDATE_CONFIG: TlcBatchLiquidateConfig = {
   MIN_ETH_BALANCE_WARNING: parseEther('0.1'),
   GAS_LIMIT: 1_000_000n,
   SUBGRAPH_URL: 'https://api.thegraph.com/subgraphs/name/templedao/templedao-tlc-liquidations',
-  SUBGRAPH_ALCHEMY_URL: 'https://subgraph.satsuma-prod.com/a912521dd162/templedao/temple-v2-mainnet/api',
+  SUBGRAPH_ALCHEMY_URL: 'https://subgraph.satsuma-prod.com/a912521dd162/templedao/tlc-liquidations-mainnet/api',
   SUBGRAPH_RETRY_LIMIT: 3,
 };
 
