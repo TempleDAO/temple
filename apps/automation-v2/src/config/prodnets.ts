84f413f29552dc1edbb5184ad97f05ae79b98bc8
pocin <8642344+pocin@users.noreply.github.com>
2024-08-09 14:54:11 +0200

    fix: tlc liquidations subgraph url

--- apps/automation-v2/src/config/prodnets.ts
+++ apps/automation-v2/src/config/prodnets.ts
@@ -9,8 +9,8 @@ const TLC_BATCH_LIQUIDATE_CONFIG: TlcBatchLiquidateConfig = {
   ACC_LIQ_MAX_CHUNK_NO: 100,
   MIN_ETH_BALANCE_WARNING: parseEther('0.1'),
   GAS_LIMIT: 1_000_000n,
-  SUBGRAPH_URL: 'https://api.thegraph.com/subgraphs/name/templedao/templedao-tlc-liquidations',
-  SUBGRAPH_ALCHEMY_URL: 'https://subgraph.satsuma-prod.com/a912521dd162/templedao/tlc-liquidations-mainnet/api',
+  SUBGRAPH_URL:
+    'https://subgraph.satsuma-prod.com/a912521dd162/templedao/tlc-liquidations-mainnet/api',
   SUBGRAPH_RETRY_LIMIT: 3,
 };
 
