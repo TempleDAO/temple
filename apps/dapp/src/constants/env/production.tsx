4ba9e008794f39a1684709effbdf60cd0d5d1b83
Marshall <99344331+marshall2112@users.noreply.github.com>
2025-02-13 16:24:43 -0500

    Fix subgraph url and tweak countdown logic (#1153)

--- apps/dapp/src/constants/env/production.tsx
+++ apps/dapp/src/constants/env/production.tsx
@@ -304,7 +304,8 @@ const env: Environment = {
       'https://subgraph.satsuma-prod.com/a912521dd162/templedao/temple-v2-mainnet/api',
     templeV2Balances:
       'https://subgraph.satsuma-prod.com/a912521dd162/templedao/temple-v2-balances/api',
-    spiceBazaar: '',
+    spiceBazaar:
+      'https://subgraph.satsuma-prod.com/a912521dd162/templedao/spice-bazaar-mainnet/api',
   },
   featureFlags: {
     enableAscend: false,
