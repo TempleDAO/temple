bd0c84d1dbed586c5a977af1e9b54e661b8abcb9
princetonbishop <92975084+princetonbishop@users.noreply.github.com>
2025-05-26 15:05:29 +0200

    arb sepolia spice deploy scripts (#1187)

---
+++ protocol/scripts/deploys/arbitrumSepolia/deploymentArgs/0x8423ef7aFe88e2df9efa1529b89a88E1448f5F8F.js
@@ -0,0 +1,3 @@
+// arbitrumSepolia: SPICE_AUCTION_IMPLEMENTATION=0x8423ef7aFe88e2df9efa1529b89a88E1448f5F8F
+// yarn hardhat verify --network arbitrumSepolia 0x8423ef7aFe88e2df9efa1529b89a88E1448f5F8F --constructor-args scripts/deploys/arbitrumSepolia/deploymentArgs/0x8423ef7aFe88e2df9efa1529b89a88E1448f5F8F.js
+module.exports = [];
