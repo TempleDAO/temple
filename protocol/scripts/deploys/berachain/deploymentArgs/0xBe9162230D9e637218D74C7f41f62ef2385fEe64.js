c6bcafafefa500805040d3fc04b888c82397c5aa
princetonbishop <92975084+princetonbishop@users.noreply.github.com>
2025-07-08 00:00:46 +0200

    berachain contract deploys (#1200)

---
+++ protocol/scripts/deploys/berachain/deploymentArgs/0xBe9162230D9e637218D74C7f41f62ef2385fEe64.js
@@ -0,0 +1,3 @@
+// berachain: SPICE_AUCTION_IMPLEMENTATION=0xBe9162230D9e637218D74C7f41f62ef2385fEe64
+// yarn hardhat verify --network berachain 0xBe9162230D9e637218D74C7f41f62ef2385fEe64 --constructor-args scripts/deploys/berachain/deploymentArgs/0xBe9162230D9e637218D74C7f41f62ef2385fEe64.js
+module.exports = [];
