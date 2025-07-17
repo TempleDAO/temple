f591ac8d44835b6262936798da3550fa3cac2b17
princetonbishop <92975084+princetonbishop@users.noreply.github.com>
2025-07-17 09:41:00 +0200

    organize folder and update docker workflow (#1205)
    * organize folder and update docker workflow
    
    * config and package dependencies
    
    * yarn lock file
    
    * add abi install script and ts files
    
    * temple gold tasks
    
    * add tgld workflow
    
    * update Dockerfile
    
    * update build script

---
+++ apps/automation/templegold/src/utils/ethers-exception.ts
@@ -0,0 +1,35 @@
+import { CallExceptionError, EthersError, Interface } from "ethers";
+
+/// Given evm custom errors as per the specified interfaces, attempt to parse
+/// an ethers CallException, if successful throw a new error that decodes the custom
+/// error.
+///
+/// Otherwise, just rethrow the original exception
+///
+/// TODO: once stablised, bake this into the overlord TransactionManager
+
+export async function throwParsedEthersError(e: unknown, errorInterfaces: Interface[]): Promise<never> {
+  throw mapParsedEthersException(e, errorInterfaces);
+}
+
+export function mapParsedEthersException(e: unknown, errorInterfaces: Interface[]): unknown {
+  if ((e as EthersError).code !== "CALL_EXCEPTION") {
+    // Rethrow anything that is not an ethers call exception
+    return e;
+  }
+  const callExceptionError = e as CallExceptionError;
+  const data = callExceptionError.data;
+  if (data === null || data.length === 0 || data === '0x') {
+    return e;
+  }
+
+  for (const ei of errorInterfaces) {
+    const err = ei.parseError(data);
+    if (err) {
+      const args = err.args.toArray().map(v => v.toString());
+      return new Error(`CALL_EXCEPTION: ${err.signature} ${args}`, { cause: e });
+    }
+  }
+  return e;
+}
+
