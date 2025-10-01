ca9b52d62b1cfaa8a3673e8311465798ff2d1c1a
frontier159 <103474701+frontier159@users.noreply.github.com>
2025-10-01 10:33:50 +0800

    feat: epoch-29c

--- protocol/scripts/deploys/mainnet/team-payments/deploy.ts
+++ protocol/scripts/deploys/mainnet/team-payments/deploy.ts
@@ -6,7 +6,7 @@ import {
   expectAddressWithPrivateKey,
   toAtto,
 } from '../../helpers';
-import snapshot from './json/epoch29b.json';
+import snapshot from './json/epoch29c.json';
 
 // TODO: Add command line arguments for json allocations file
 async function main() {
