9a3c762a57f0fb123fd3dc336a49cb5658da4eb4
princetonbishop <92975084+princetonbishop@users.noreply.github.com>
2026-01-14 15:14:27 +0200

    Spice deploy scripts (#1244)
    * remove min and max auction duration restrictions
    
    * slither
    
    * revert minimum duration, use 1 day
    
    * cleanup
    
    * update readme
    
    * typo fix
    
    * readd max duration
    
    * cleanup
    
    * spice implementation, sENA auction

--- protocol/scripts/deploys/mainnet/deploymentArgs/0xBb860920F493ba86a3122110260fd70980e0B2e5.js
+++
@@ -1,3 +0,0 @@
-// mainnet: TEMPLE_GOLD.SPICE_AUCTION_IMPLEMENTATION=0xBb860920F493ba86a3122110260fd70980e0B2e5
-// yarn hardhat verify --network mainnet 0xBb860920F493ba86a3122110260fd70980e0B2e5 --constructor-args scripts/deploys/mainnet/deploymentArgs/0xBb860920F493ba86a3122110260fd70980e0B2e5.js
-module.exports = [];
