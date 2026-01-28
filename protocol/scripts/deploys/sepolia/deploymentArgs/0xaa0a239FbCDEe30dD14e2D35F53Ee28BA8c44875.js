f5dbb77c70ecf8af85e3b744359552d01451a416
princetonbishop <92975084+princetonbishop@users.noreply.github.com>
2026-01-28 15:27:34 +0200

    deploy scripts (#1249)

---
+++ protocol/scripts/deploys/sepolia/deploymentArgs/0xaa0a239FbCDEe30dD14e2D35F53Ee28BA8c44875.js
@@ -0,0 +1,3 @@
+// sepolia: SPICE_AUCTION_IMPLEMENTATION=0xaa0a239FbCDEe30dD14e2D35F53Ee28BA8c44875
+// yarn hardhat verify --network sepolia 0xaa0a239FbCDEe30dD14e2D35F53Ee28BA8c44875 --constructor-args scripts/deploys/sepolia/deploymentArgs/0xaa0a239FbCDEe30dD14e2D35F53Ee28BA8c44875.js
+module.exports = [];
