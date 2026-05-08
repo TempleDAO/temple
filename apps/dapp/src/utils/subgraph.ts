95e3366642c93d9951ff229710be0376bb260100
Marshall <99344331+marshall2112@users.noreply.github.com>
2026-05-08 10:56:19 -0400

    Merge pull request #1272 from TempleDAO/fix-spice-graph
    fix: Fix chart and auction details

--- apps/dapp/src/utils/subgraph.ts
+++ apps/dapp/src/utils/subgraph.ts
@@ -800,7 +800,7 @@ export type BidsHistoryGoldAuctionResp = z.infer<
 //----------------------------------------------------------------------------------------------------
 
 export function spiceBidHistoryQuery(
-  auctionToken: string,
+  auctionAddress: string,
   first = 1000,
   skip = 0
 ): SubGraphQuery<SpiceBidHistoryResp> {
@@ -815,7 +815,6 @@ export function spiceBidHistoryQuery(
       where: {
         auctionInstance_: {
           auctionType: SpiceAuction
-          auctionToken: "${auctionToken.toLowerCase()}"
         }
       }
     ) {
