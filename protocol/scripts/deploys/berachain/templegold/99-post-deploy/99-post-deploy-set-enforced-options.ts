713bd85365b87f399cecad4cbba7c29e4b29e0ee
princetonbishop <princetonbishop@protonmail.com>
2026-05-27 15:18:37 +0200

    berachain scripts boilerplate

--- protocol/scripts/deploys/berachain/templegold/99-post-deploy/99-post-deploy-set-enforced-options.ts
+++ protocol/scripts/deploys/berachain/templegold/99-post-deploy/99-post-deploy-set-enforced-options.ts
@@ -1,17 +1,14 @@
-import { ethers } from 'hardhat';
 import {
-    ensureExpectedEnvvars,
     mine,
+    runAsyncMain,
 } from '../../../helpers';
-import { connectToContracts } from '../contract-addresses';
 import { EnforcedOptionParamStruct } from '../../../../../typechain/@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OAppOptionsType3';
 import { TempleGold } from '../../../../../typechain';
 import { DEFAULT_SETTINGS } from '../default-settings';
+import { getDeployContext } from '../deploy-context';
 
 async function main() {
-    ensureExpectedEnvvars();
-    const [owner] = await ethers.getSigners();
-    const INSTANCES = connectToContracts(owner);
+    const { INSTANCES } = await getDeployContext(__dirname);
     // set enforced options
     await setEnforcedOptionsMainnet(INSTANCES.TEMPLE_GOLD.TEMPLE_GOLD);
 }
@@ -26,11 +23,4 @@ async function setEnforcedOptionsMainnet(templeGold: TempleGold) {
     await mine(templeGold.setEnforcedOptions(options));
 }
   
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+runAsyncMain(main);
