4a30e0172a131ded2def5ea7b3e9ed61ae770bf3
Dario <medariox@users.noreply.github.com>
2026-06-02 18:18:52 +0200

    rm subgraphs

--- apps/core-subgraph/scripts/graph-node-setup.sh
+++
@@ -1,4 +0,0 @@
-git clone git@github.com:graphprotocol/graph-node.git
-docker rmi graphprotocol/graph-node:latest
-cd graph-node && ./docker/build.sh
-docker tag graph-node graphprotocol/graph-node:latest
