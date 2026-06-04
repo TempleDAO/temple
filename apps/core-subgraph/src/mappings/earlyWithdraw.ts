68db735a3535ef2c57b9b0e21aedb6e1f2e15365
Marshall <99344331+marshall2112@users.noreply.github.com>
2026-06-04 06:38:46 -0400

    More dependabot fixes (#1286)
    * More fixes
    
    * rm subgraphs
    
    ---------
    
    Co-authored-by: Dario <medariox@users.noreply.github.com>

--- apps/core-subgraph/src/mappings/earlyWithdraw.ts
+++
@@ -1,8 +0,0 @@
-import { WithdrawCall } from '../../generated/VaultEarlyWithdraw/VaultEarlyWithdraw'
-
-import { createEarlyWithdraw } from '../entities/earlyWithdraw'
-
-
-export function onEarlyWithdraw(call: WithdrawCall): void {
-    createEarlyWithdraw(call)
-}
